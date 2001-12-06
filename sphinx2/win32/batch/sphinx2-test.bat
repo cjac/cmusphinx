7dc507b2cf3c5b4f0567afac7e2b0e1a54f17c23
(no author) <(no author)@c60273ff-4a12-0410-8d58-82ceea6d4170>
2001-12-06 06:15:22 +0000

    This commit was manufactured by cvs2svn to create branch 'CMU'.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/branches/CMU@207 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ sphinx2/win32/batch/sphinx2-test.bat
@@ -0,0 +1,13 @@
+echo off
+
+set S2BATCH=..\msdev\SphinxExamples\sphinx2_continuous\Debug\sphinx2_continuous.exe
+set HMM=..\..\model\hmm\6k
+set TASK=..\..\model\lm\turtle
+set CTLFILE=..\..\model\lm\turtle\turtle.ctl
+
+echo " "
+echo "sphinx2_test"
+echo "Run CMU Sphinx2 in Batch mode to decode an example utterance."
+echo " "
+
+%S2BATCH% -verbose 9 -adcin TRUE -adcext 16k -ctlfn %CTLFILE% -ctloffset 0 -ctlcount 100000000 -datadir %TASK% -agcmax FALSE -langwt 6.5 -fwdflatlw 8.5 -rescorelw 9.5 -ugwt 0.5 -fillpen 1e-10 -silpen 0.005 -inspen 0.65 -top 1 -topsenfrm 3 -topsenthresh -70000 -beam 2e-06 -npbeam 2e-06 -lpbeam 2e-05 -lponlybeam 0.0005 -nwbeam 0.0005 -fwdflat FALSE -fwdflatbeam 1e-08 -fwdflatnwbeam 0.0003 -bestpath TRUE -kbdumpdir %TASK% -lmfn %TASK%\turtle.lm -dictfn %TASK%\turtle.dic -noisedict %HMM%\noisedict -phnfn %HMM%\phone -mapfn %HMM%\map -hmmdir %HMM% -hmmdirlist %HMM% -8bsen TRUE -sendumpfn %HMM%\sendump -cbdir %HMM%
