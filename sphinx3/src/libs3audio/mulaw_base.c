35402cc905778ad2de5665793d80357315dceb66
lenzo <lenzo@c60273ff-4a12-0410-8d58-82ceea6d4170>
2001-12-11 04:10:16 +0000

    License.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@265 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3audio/mulaw_base.c
+++ sphinx3/src/libs3audio/mulaw_base.c
@@ -1,58 +1,15 @@
-/* ====================================================================
- * Copyright (c) 1997-2000 Carnegie Mellon University.  All rights 
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The names "Sphinx" and "Carnegie Mellon" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. To obtain permission, contact 
- *    sphinx@cs.cmu.edu.
- *
- * 4. Products derived from this software may not be called "Sphinx"
- *    nor may "Sphinx" appear in their names without prior written
- *    permission of Carnegie Mellon University. To obtain permission,
- *    contact sphinx@cs.cmu.edu.
- *
- * 5. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by Carnegie
- *    Mellon University (http://www.speech.cs.cmu.edu/)."
- *
- * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
- * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
- * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * ====================================================================
- *
- */
 /*
  * mulaw.c -- u-Law audio routines.
  * 
  * HISTORY
  * 
  * $Log$
- * Revision 1.1  2001/12/03  16:01:45  egouvea
- * Initial revision
+ * Revision 1.2  2001/12/11  04:10:16  lenzo
+ * License.
  * 
+ * Revision 1.1.1.1  2001/12/03 16:01:45  egouvea
+ * Initial import of sphinx3
+ *
  * Revision 1.1  2000/12/12 22:55:55  lenzo
  * Separate out a/d lib.
  *
