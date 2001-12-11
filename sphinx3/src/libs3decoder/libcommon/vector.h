7b29d57a9bf2974d37b1a4a2eb3bbe351b496065
lenzo <lenzo@c60273ff-4a12-0410-8d58-82ceea6d4170>
2001-12-11 04:40:56 +0000

    License cleanup.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@267 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/libcommon/vector.h
+++ sphinx3/src/libs3decoder/libcommon/vector.h
@@ -1,3 +1,38 @@
+/* ====================================================================
+ * Copyright (c) 1999-2001 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
 /*
  * vector.h -- vector routines.
  * 
@@ -257,9 +292,12 @@ vector_gautbl_eval_logs3 (vector_gautbl_t *gau,	/* In: Table of Gaussians */
  * Log record.  Maintained by RCS.
  *
  * $Log$
- * Revision 1.2  2001/12/04  02:57:47  egouvea
- * Merged memory leak fixes from RAH  (egouvea@cs.cmu.edu)
+ * Revision 1.3  2001/12/11  04:40:56  lenzo
+ * License cleanup.
  * 
+ * Revision 1.2  2001/12/04 02:57:47  egouvea
+ * Merged memory leak fixes from RAH  (egouvea@cs.cmu.edu)
+ *
  * Revision 1.1.1.1  2001/01/17 05:17:15  ricky
  * Initial Import of the s3.3 decoder, has working decodeaudiofile, s3.3_live
  *
