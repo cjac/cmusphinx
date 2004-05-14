fc2c4fc9225bbcb7eadf82be7b0dc649199b8995
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-05-14 19:58:34 +0000

    Removed system dependencies from ad.h, and added an ad_conf.h with the system dependent part
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@3206 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx2/include/ad.h
+++ sphinx2/include/ad.h
@@ -1,3 +1,35 @@
+/* ====================================================================
+ * Copyright (c) 1999-2001 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
 /*
  * ad.h -- generic live audio interface for recording and playback
  * 
@@ -11,11 +43,14 @@
  * HISTORY
  * 
  * $Log$
- * Revision 1.8  2004/03/01  00:00:48  egouvea
- * Fixed refs to win32, and clean up files used by autoconf/automake
+ * Revision 1.9  2004/05/14  19:58:33  egouvea
+ * Removed system dependencies from ad.h, and added an ad_conf.h with the system dependent part
  * 
- * Revision 1.7  2004/02/17 22:34:15  egouvea
- * First steps to get rid of the @ad_backend@, used by autoconf.
+ * Revision 1.3  2004/03/01 00:00:48  egouvea
+ * Fixed refs to win32, and clean up files used by autoconf/automake
+ *
+ * Revision 1.2  2004/02/17 22:34:15  egouvea
+ * First steps to get rid of the AD_BACKEND_SUNOS, used by autoconf.
  *
  * Revision 1.1  2000/12/12 23:01:41  lenzo
  * Rationalizing libs and names some more.  Split a/d and fe libs out.
@@ -49,6 +84,10 @@
 
 #ifndef _AD_H_
 #define _AD_H_
+
+#if !defined(WIN32)
+#include "ad_conf.h"
+#endif
 
 #if defined(WIN32)
 
