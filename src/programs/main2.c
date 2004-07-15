17a8b92a59adb64d3565393d86999248d1d3c580
yitao <yitao@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-07-15 19:24:43 +0000

    moved
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@3456 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/programs/main2.c
+++ src/programs/main2.c
@@ -2,81 +2,10 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "live2.h"
+#include "args.h"
 #include "ad.h"
 
 #define BUFSIZE 4096
-
-#if 0
-int
-main(int argc, char **argv)
-{
-  live_decoder_t decoder;
-  FILE *infile;
-  ad_rec_t *ad;
-  char *hypstr;
-  int16 samples[BUFSIZE];
-  int32 num_samples;
-  int32 tot_samples = 0;
-
-  if (argc < 2) {
-    printf("Usage:  livedecoder2 [ARGS] INPUT_FILE\n");
-    return -1;
-  }
-
-  if (ld_init(&decoder, argc - ((argc - 1) % 2), argv)) {
-    printf("ld_init() failed.\n");
-    return -1;
-  }
-
-  if (ld_utt_begin(&decoder, 0)) {
-    printf("ld_utt_begin() failed.\n");
-    return -1;
-  }
-
-  if (argc % 2) {
-    /* record for 5 seconds */
-    tot_samples = cmd_ln_int32 ("-samprate") * 5;
-    ad = ad_open_sps(cmd_ln_int32 ("-samprate"));
-    ad_start_rec(ad);
-    while (tot_samples > 0) {
-      num_samples = ad_read(ad, samples,
-			    tot_samples > BUFSIZE ? BUFSIZE : tot_samples);
-      if (num_samples > 0) {
-	if (ld_utt_proc_raw(&decoder, samples, num_samples) < 0) {
-	  printf("ld_utt_proc_raw() returned unexpectedly.\n");
-	  return -1;
-	}
-	tot_samples -= num_samples;
-      }
-    }
-    ad_stop_rec(ad);
-    ad_close(ad);
-  }
-  else {
-    infile = fopen(argv[1], "rb");
-    while (!feof(infile)) {
-      num_samples = fread(samples, sizeof(int16), BUFSIZE, infile);
-      if (num_samples > 0) {
-	if (ld_utt_proc_raw(&decoder, samples, num_samples) < 0) {
-	  printf("ld_utt_proc_raw() returned unexpectedly.\n");
-	  return -1;
-	}
-      }
-    }
-    fclose(infile);
-  }
-	
-  if (ld_utt_end(&decoder)) {
-    printf("ld_utt_end() failed.\n");
-    return -1;
-  }
-
-  ld_utt_hyps(&decoder, &hypstr, 0);
-  printf("decoder returned:\n%s\n", hypstr);
-
-  return 0;
-}
-#endif
 
 HANDLE startEvent;
 HANDLE finishEvent;
@@ -125,12 +54,17 @@ main(int argc, char **argv)
   /////////////////////////////////////////////////////////////////////////////
   // Initializing
   //
-  if (argc < 2) {
-    printf("Usage:  livedecoder2 [ARGS] \n");
+  if (argc != 2) {
+    printf("Usage:  livedecoder2 config_file \n");
     return -1;
   }
 
-  if (ld_init(&decoder, argc, argv)) {
+  if (cmd_ln_parse_file(arg_def, argv[1])) {
+    printf(">>>> cmd_ln_parse_file failed.\n");
+    return -1;
+  }
+
+  if (ld_init(&decoder)) {
     printf(">>>>> ld_init() failed.\n");
     return -1;
   }
@@ -187,6 +121,8 @@ main(int argc, char **argv)
   else {
     printf(">>>>> decoder returned:\n%s\n", hypstr);
   }
+
+  ld_finish(&decoder);
 
   return 0;
 }
