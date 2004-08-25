bc48dd5aba208cdb54960d219ac78463829b9b11
yitao <yitao@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-08-25 16:15:34 +0000

    more changes, but still not compiling yet.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@3761 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/remote_decode.c
+++ src/libs3decoder/remote_decode.c
@@ -61,6 +61,7 @@ enum {
   RD_CTRL_FINISH,
   RD_CTRL_UTT_BEGIN,
   RD_CTRL_UTT_END,
+  RD_CTRL_UTT_HYP,
   RD_CTRL_PROC_RAW,
   RD_CTRL_PROC_FRAME,
   RD_CTRL_PROC_FEAT,
@@ -111,27 +112,35 @@ int
 rd_init(remote_decoder_t *decoder)
 {
   memset(decoder, 0, sizeof(remote_decoder));
-  decoder->internal_cmd_ln = 0;
+  if ((decoder->mutex = CreateMutex(NULL, FALSE, NULL)) == NULL) {
+    return -1;
+  }
+
   return rd_queue_control_block(decoder, RD_CTRL_INIT, 0, 0);
 }
 
 int
 rd_init_with_args(remote_decoder_t *decoder, int argc, char **argv)
 {
-  int rv;
   cmd_ln_parse(arg_def, argc, argv);
-  rv = rd_init(decoder);
+  memset(decoder, 0, sizeof(remote_decoder));
   decoder->internal_cmd_ln = 1;
-  return rv;
+  if ((decoder->mutex = CreateMutex(NULL, FALSE, NULL)) == NULL) {
+    return -1;
+  }
+
+  return rd_queue_control_block(decoder, RD_CTRL_INIT, 0, 0);
 }
 
 int
 rd_finish(remote_decoder_t *decoder)
 {
   int rv;
+
   rd_lock_internal(decoder);
   rv = rd_queue_control_block(decoder, RD_CTRL_FINISH, 0, 0);
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
@@ -139,9 +148,17 @@ int
 rd_utt_begin(remote_decoder_t *decoder, char *uttid)
 {
   int rv;
+  char *local_uttid;
+
+  if (uttid && (local_uttid = ckd_calloc(1, strlen(uttid) + 1)) == 0) {
+    return -1;
+  }
+  strcpy(local_uttid, uttid);
+
   rd_lock_internal(decoder);
-  rv = rd_queue_control_block(decoder, RD_CTRL_UTT_BEGIN, 0, uttid);
+  rv = rd_queue_control_block(decoder, RD_CTRL_UTT_BEGIN, 0, local_uttid);
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
@@ -149,9 +166,11 @@ int
 rd_utt_end(remote_decoder_t *decoder)
 {
   int rv;
+
   rd_lock_internal(decoder);
   rv = rd_queue_control_block(decoder, RD_CTRL_UTT_END, 0, 0);
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
@@ -159,12 +178,14 @@ int
 rd_utt_abort(remote_decoder_t *decoder)
 {
   int rv = 0;
+
   rd_lock_internal(decoder);
   if (rd->state == RD_STATE_UTT) {
     while (rd_dequeue_control_block(decoder, 0, 0, 0) == 0);
     rv = rd_queue_control_block(decoder, RD_CTRL_UTT_END, 0, 0);
   }
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
@@ -172,9 +193,22 @@ int
 rd_utt_proc_raw(remote_decoder_t *decoder, int16 *samples, int32 num_samples)
 {
   int rv;
+  int16 local_samples;
+
+  if (samples == 0) {
+    return -1;
+  }
+
+  local_samples = ckd_calloc(sizeof(int16), num_samples);
+  if (local_samples == 0) {
+    return -1;
+  }
+
   rd_lock_internal(decoder);
-  rv = rd_queue_control_block(decoder, RD_CTRL_PROC_RAW, num_samples, samples);
+  rv = rd_queue_control_block(decoder, RD_CTRL_PROC_RAW, num_samples,
+			      local_samples);
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
@@ -184,12 +218,28 @@ rd_utt_proc_frame(remote_decoder_t *decoder,
 		  int32 num_frames)
 {
   int rv;
+  float32 **local_frames;
+
+  if (frames == 0) {
+    return -1;
+  }
+
+  local_frames = ckd_calloc_2d(num_frames, decoder->ld.fe->NUM_CEPSTRA,
+			       sizeof(float32));
+  if (local_frames == 0) {
+    return -1;
+  }
+
   rd_lock_internal(decoder);
-  rv = rd_queue_control_block(decoder, RD_CTRL_PROC_FRAME, num_frames, frames);
+  rv = rd_queue_control_block(decoder, RD_CTRL_PROC_FRAME, num_frames,
+			      local_frames);
   rd_unlock_internal(decoder);
+
   return rv;
 }
 
+/** let's not implement this just yet */
+#if 0
 int
 rd_utt_proc_feat(remote_decoder_t *decoder,
 		 float32 ***features,
@@ -202,16 +252,11 @@ rd_utt_proc_feat(remote_decoder_t *decoder,
   rd_unlock_internal(decoder);
   return rv;
 }
+#endif
 
 int
-rd_utt_hyps(remote_decoder_t *decoder, char **uttid, char **hyp_str, 
-	    hyp_t ***hyp_segs)
+rd_utt_record_hyps(remote_decoder_t *decoder)
 {
-  int rv;
-  rd_lock_internal(decoder);
-  rv = rd_dequeue_result_block(decoder, uttid, hyp_str, hyp_segs);
-  rd_unlock_internal(decoder);
-  return rv;
 }
 
 int
@@ -233,6 +278,17 @@ rd_interrupt(remote_decoder_t *decoder)
     while (rd_dequeue_control_block(decoder, 0, 0, 0) == 0);
     rv = rd_queue_control_block(decoder, RD_CTRL_JOIN, 0, 0);
   }
+  rd_unlock_internal(decoder);
+  return rv;
+}
+
+int
+rd_retrieve_hyps(remote_decoder_t *decoder, char **uttid, char **hyp_str, 
+		 hyp_t ***hyp_segs)
+{
+  int rv;
+  rd_lock_internal(decoder);
+  rv = rd_dequeue_result_block(decoder, uttid, hyp_str, hyp_segs);
   rd_unlock_internal(decoder);
   return rv;
 }
@@ -316,6 +372,12 @@ rd_run(remote_decoder_t *d)
       }
     }
     break;
+
+    case RD_CTRL_UTT_HYP: {
+      if (d->state == RD_STATE UTT || d->state == RD_STATE_IDLE) {
+	ld_utt_hyps
+      }
+    }
     }
 
     if (data) {
@@ -327,6 +389,131 @@ rd_run(remote_decoder_t *d)
   while (rd_dequeue_control_block(d, 0, 0, 0) == 0);
   rd_unlock_internal(d);
 
+  /** Win32 specific code */
+  CloseHandle(d->mutex);
+
   return rv;
 }
 
+/*****************************************************************************/
+/*****************************************************************************/
+/*****************************************************************************/
+
+int
+rd_lock_internal(remote_decoder_t *decoder)
+{
+  /** Win32 specific code */
+  if (decoder->mutex) {
+    WaitForSingleObject(decoder->mutex, INFINITE);
+  }
+
+  return 0;
+}
+
+int
+rd_unlock_internal(remote_decoder_t *decoder)
+{
+  /** Win32 specific code */
+  if (decoder->mutex) {
+    ReleaseMutex(decoder->mutex);
+  }
+
+  return 0;
+}
+
+int
+rd_queue_control(remote_decoder_t *decoder, int32 cmd, int32 param, void *data)
+{
+  int rv;
+  control_block *cb;
+
+  if ((cb = ckd_calloc(1, sizeof(control_block))) == 0) {
+    rv = -1;
+  }
+  
+  *cb = { cmd, param, data, decoder->control_queue == 0 ?
+	  cb : (struct _control_block *)decoder->control_queue };
+  decoder->control_queue = cb;
+  return 0;
+}
+
+int
+rd_dequeue_control(remote_decoder_t *decoder, int32 cmd, int32 *param,
+		   void **data)
+{
+}
+
+int
+rd_queue_result(remote_decoder_t *decoder, char *uttid, char *hyp_str,
+		hyp_t **hyp_segs)
+{
+  int rv = 0;
+  result_block *rb = 0;
+  char *local_uttid = 0;
+  char *local_hyp_str = 0;
+  hyp_t **local_hyp_segs = 0;
+  hyp_t *local_hyp_seg_buffer = 0;
+  int hyp_seglen = 0;
+  hyp_t *h;
+
+  if (rv == 0) {
+    if (uttid && (local_uttid = ckd_calloc(1, strlen(uttid) + 1)) == 0) {
+      rv = -1;
+    }
+    else {
+      strcpy(local_uttid, uttid);
+    }
+  }
+
+  if (rv == 0) {
+    if (hyp_str && (local_hyp_str = ckd_calloc(1, strlen(hyp_str) + 1)) == 0) {
+      rv == -1;
+    }
+    else {
+      strcpy(local_hyp_str, hyp_str);
+    }
+  }
+
+  if (rv == 0) {
+    for (h = hyp_segs; h; h++, hyp_seglen++);
+    if (hyp_segs && hyp_seglen > 0) {
+      local_hyp_segs = ckd_calloc(hyp_seglen + 1, sizeof(hyp_t *));
+      if (local_hyp_segs == 0) {
+	rv = -1;
+      }
+      else {
+	*local_hyp_segs = ckd_calloc(
+  }
+
+      
+  if ((rb = ckd_calloc(1, sizeof(result_block))) == 0) {
+    rv = -1;
+  }
+  *rb = { local_uttid, local_hyp_str, local_hyp_segs,
+	  decoder->result_queue == 0 ?
+	  cb : (struct _result_block *)decoder->result_queue };
+  decoder->result_queue = rb;
+
+  if (rv != 0) {
+    if (rb) {
+      ckd_free(rb);
+    }
+    if (local_uttid) {
+      ckd_free(local_uttid);
+    }
+    if (local_hyp_str) {
+      ckd_free(local_hyp_str);
+    }
+    if (local_hyp_segs) {
+      ckd_free(local_hyp_segs);
+    }
+  }
+
+  return rv;
+}
+
+int
+rd_dequeue_result(remote_decoder_t *decoder, char **uttid, char **hyp_str,
+		  hyp_t ***hyp_segs)
+{
+}
