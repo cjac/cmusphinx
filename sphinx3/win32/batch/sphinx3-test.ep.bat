72320201a113d7c450e0cfd65940a6902098ebfc
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-10-22 23:24:36 +0000

    following Evandro's suggestions, moving all files in ep_model/ to model/ep, tests in linux and windows are fixed. Evandro, please also send an adminrequest to delete ep_model
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4078 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/win32/batch/sphinx3-test.ep.bat
+++ sphinx3/win32/batch/sphinx3-test.ep.bat
@@ -14,5 +14,5 @@ echo This batch script assumes all files are relative to the main
 echo directory (S3ROOT).
 echo .
 
-%S3BATCH% -i ./ep_model/chan3.raw -srate 11025 -frate 105 -wlen 0.024 -alpha 0.97 -ncep 13 -nfft 512 -nfilt 36 -upperf 5400 -lowerf 130 -blocksize 262500 -mean ./ep_model/means -var ./ep_model/variances -mixw ./ep_model/mixture_weights 
+%S3BATCH% -i ./model/ep/chan3.raw -srate 11025 -frate 105 -wlen 0.024 -alpha 0.97 -ncep 13 -nfft 512 -nfilt 36 -upperf 5400 -lowerf 130 -blocksize 262500 -mean ./model/ep/means -var ./model/ep/variances -mixw ./model/ep/mixture_weights 
 
