49b2acf6a3a1fd3698e94505cdd301e256f50324
yitao <yitao@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-10-25 17:17:08 +0000

    ziad's classifier and endpointer, reimplemented and cleaned up.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4080 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/classifier.h
+++ sphinx3/src/libs3decoder/classifier.h
@@ -2,11 +2,72 @@
 #ifndef __SPHINX3_CLASSIFIER_H
 #define __SPHINX3_CLASSIFIER_H
 
+#include "libutil/libutil.h"
+#include "cont_mgau.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define CEP_LEN			13
+#define VOTING_LEN		5
+#define CLASS_LATENCY		2
+
+enum {
+  CLASS_SILENCE = 0,
+  CLASS_OWNER,
+  CLASS_SECONDARY,
+  CLASS_NOISE,
+  NUM_CLASSES
+};
+
+enum {
+  EP_MAYBE = 0,
+  EP_SILENCE,
+  EP_SPEECH
+};
+
+/* static char class_labels[NUM_CLASSES] = "OSN."; */
+static float32 class_prior_prob[NUM_CLASSES] = { 0.4, 0.1, 0.1, 0.4 };
+
 typedef struct {
+  mgau_model_t *gmm;
+  float32 frame_gmm[NUM_CLASSES];
+  int *classed_frames;
+  int num_frames;
+  int max_frames;
+  int voting_frames[VOTING_LEN];
+  int post_classify;
 } classifier_t;
 
-int cl_init(classifier_t *);
-int cl_classify_ceps(classifier_t *, float32 **, char **);
-int cl_finish(classifier_t *);
+typedef struct {
+  int *endpts;
+  int num_endpts;
+  int max_endpts;
+  int state;
+  int start_counter;
+  int cancel_counter;
+  int end_counter;
+  int pad_cancel;
+  int pad_leader;
+  int pad_trailer;
+} endptr_t;
+
+int cl_init(classifier_t *_cl, char *_means_file,  char *_vars_file,
+	    float64 _var_floor,  char *_mix_weights_file,
+	    float64 _mix_weight_floor,  char *_gm_type, int _post_classify);
+void cl_finish(classifier_t *_cl);
+int cl_classify_frames(classifier_t *_cl, float32 **_frames, int _num_frames,
+		       int **_classes);
+
+void ep_init(endptr_t *_ep, int _pad_leader, int _pad_trailer,
+	     int _pad_cancel);
+void ep_finish(endptr_t *_ep);
+int ep_endpoint(endptr_t *_ep, int *_classes, int _num_frames,
+		int **_endpts);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
