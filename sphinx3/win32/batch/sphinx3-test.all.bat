9b001eb29e61d41649019a4ec7dffd32d56b5b83
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-11-02 23:37:37 +0000

    Added batch file that calls all test batch files in sequence
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4101 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ sphinx3/win32/batch/sphinx3-test.all.bat
@@ -0,0 +1,10 @@
+call .\sphinx3-test.align.bat
+call .\sphinx3-test.allphone.bat
+call .\sphinx3-test.astar.bat
+call .\sphinx3-test.bat
+call .\sphinx3-test.dag.bat
+call .\sphinx3-test.decode.bat
+call .\sphinx3-test.ep.bat
+call .\sphinx3-test.gausubvq.bat
+call .\sphinx3-test.livepretend.bat
+call .\sphinx3-test.simple.bat
