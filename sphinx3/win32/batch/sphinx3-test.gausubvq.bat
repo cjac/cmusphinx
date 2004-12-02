b2c577ebabc850ba499cf0b5cce2c4370d79de77
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-12-02 05:35:23 +0000

    Add -seed argument for gausubvq.  This make gausubvq has the same behavior across windows and linux.  (Don't know about other platforms yet.)
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4195 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/win32/batch/sphinx3-test.gausubvq.bat
+++ sphinx3/win32/batch/sphinx3-test.gausubvq.bat
@@ -15,7 +15,7 @@ echo directory (S3ROOT).
 echo .
 
 
-%S3BATCH%  -mean ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/means -var  ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/variances -mixw  ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/mixture_weights -svspec 0-38 -iter 20 -svqrows 16 -subvq subvq.out 
+%S3BATCH%  -mean ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/means -var  ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/variances -mixw  ./model/hmm/hub4_cd_continuous_8gau_1s_c_d_dd/mixture_weights -svspec 0-38 -iter 20 -svqrows 16 -seed 1111 -subvq subvq.out 
 
 
 
