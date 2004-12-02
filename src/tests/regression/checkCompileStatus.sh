a4cb3fdf78fc1bea5c4112fe39228bd9de66a460
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-12-02 16:25:24 +0000

    Automtatic Compilation scripts for sphinx2, sphinx3 and SphinxTrain. This is just a temporary place to put this script at here.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4197 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/tests/regression/checkCompileStatus.sh
+++ src/tests/regression/checkCompileStatus.sh
@@ -1,27 +1,60 @@
 #!/bin/bash -x
 
+username=$1
+
 export PATH="/usr/local/bin:/bin:/usr/bin"
 
-root=/tmp/sphinx3.$$
-LIST='egouvea archan yitao'
+root=/tmp/sphinxCompilation.$$
+S2LIST='archan'
+S3LIST='archan'
+S4LIST='archan'
+STLIST='archan'
 
 mkdir $root
 cd $root
 
 export CVS_RSH=/usr/local/bin/ssh
 
-/usr/local/bin/cvs -d:ext:cvs.sf.net:/cvsroot/cmusphinx co sphinx3
+/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co sphinx2 >> ./test.out
 
-cd sphinx3
+pushd sphinx2 
+./autogen.sh >> ./test.out 2>&1 
+./autogen.sh >> ./test.out 2>&1 
 
-./autogen.sh
-./autogen.sh
-
-if ! make all test > test.out 2>&1;
- then mhmail -s "Sphinx3 compilation failed" ${LIST} < test.out;
- elif ! grep 'FWDVIT: P I T T S B U R G H' test.out;
- then mhmail -s "Sphinx3 test failed" ${LIST} < test.out;
+if ! make all test >> test.out 2>&1 ;
+ then mhmail -s "sphinx2 compilation or test failed" ${S2LIST} <test.out
+ else mhmail -s "sphinx2 compilation or test succeeded" ${S2LIST} <test.out
  fi
+
+popd
+
+/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co sphinx3 >> ./test.out
+
+pushd sphinx3
+
+./autogen.sh >> ./test.out 2>&1 
+./autogen.sh >> ./test.out 2>&1 
+
+if ! make all test-full >> test.out 2>&1 ;
+ then mhmail -s "sphinx3 compilation failed" ${S3LIST} < test.out;
+ elif ! grep 'FWDVIT: P I T T S B U R G H ' test.out;
+ then mhmail -s "sphinx3 test succeeded" ${S3LIST} < test.out;
+ fi
+
+popd 
+
+/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co SphinxTrain >> ./test.out
+
+pushd SphinxTrain
+./configure >> ./test
+if ! make all >> test.out 2>&1 ;
+ then mhmail -s "SphinxTrain compilation failed" ${STLIST} < test.out;
+ else mhmail -s "SphinxTrain compilation succeeded" ${STLIST} < test.out;
+ fi
+popd
 
 cd /tmp
 /bin/rm -rf $root
+
+
+
