0134a2ef7fec21f5cb9bc70c7b0e69fb5851e54e
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-12-07 00:59:31 +0000

    Removed absolute paths, added choices to mail client, added test verification to sphinx2
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4251 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/tests/regression/checkCompileStatus.sh
+++ sphinx3/src/tests/regression/checkCompileStatus.sh
@@ -1,63 +1,106 @@
-#!/bin/bash -x
+#!/bin/bash
 
 username=$1
 
+# The repository address
+address=${username}@cvs.sf.net
+
+# Define a path, just in case
 export PATH="/usr/local/bin:/bin:/usr/bin"
 
+# Try to find an executable that can send mail
+# Default to sendmail
+MAILX=sendmail
+
+# Try to find mhmail
+TMPMAIL=`which mhmail 2> /dev/null`
+if test z${TMPMAIL} == z; then
+# If we failed, try mailx
+    TMPMAIL=`which mailx 2> /dev/null`
+    if test z${TMPMAIL} == z; then
+# If we failed again, try mail
+	TMPMAIL=`which mail 2> /dev/null`
+    fi
+fi
+
+# If we found one of the above, use it. Otherwise, keep sendmail
+if test z${TMPMAIL} != z; then MAILX=${TMPMAIL};fi
+
+
+# Define the variables for compilation root, mailing lists
 root=/tmp/sphinxCompilation.$$
 S2LIST='archan egouvea yitao dhuggins'
 S3LIST='archan egouvea yitao dhuggins'
-S4LIST='archan egouvea yitao dhuggins'
+S4LIST='cmusphinx-commits@lists.sourceforge.net'
 STLIST='archan egouvea yitao dhuggins'
 
+# Create root and move there
 mkdir $root
 cd $root
 
-export CVS_RSH=/usr/local/bin/ssh
+# Output file that will get sent in case of failure
+outfile=$root/test.out
 
-/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co sphinx2 >> ./test.out
+# CVS requires us to define this
+export CVS_RSH=ssh
 
+# Fresh download of sphinx2
+cvs -d:ext:${address}:/cvsroot/cmusphinx co sphinx2 > $outfile 2>&1
+
+# Configure it
 pushd sphinx2 
-./autogen.sh >> ./test.out 2>&1 
-./autogen.sh >> ./test.out 2>&1 
 
-if ! make all test >> test.out 2>&1 ;
- then mhmail -s "sphinx2 compilation or test failed" ${S2LIST} <test.out
- else mhmail -s "sphinx2 compilation or test succeeded" ${S2LIST} <test.out
+./autogen.sh >> $outfile 2>&1 
+./autogen.sh >> $outfile 2>&1 
+
+# Compile and run test, and verify if both were successful
+if ! make all test >> $outfile 2>&1 ;
+ then ${MAILX} -s "sphinx2 compilation failed" ${S2LIST} < $outfile
+ elif ! (grep BESTPATH $outfile | grep -q 'GO FORWARD TEN METERS');
+ then ${MAILX} -s "Sphinx2 test failed" ${S2LIST} < $outfile;
+ else ${MAILX} -s "sphinx2 compilation or test succeeded" ${S2LIST} < $outfile
  fi
 
 popd
 
-/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co sphinx3 >> ./test.out
+# Fresh download of sphinx3
+cvs -d:ext:${address}:/cvsroot/cmusphinx co sphinx3 > $outfile 2>&1
 
+# Configure it
 pushd sphinx3
 
-./autogen.sh >> ./test.out 2>&1 
-./autogen.sh >> ./test.out 2>&1 
+./autogen.sh >> $outfile 2>&1 
+./autogen.sh >> $outfile 2>&1 
 
-if ! make all test-full >> test.out 2>&1 ;
- then mhmail -s "sphinx3 compilation failed" ${S3LIST} < test.out;
- elif ! (grep FWDVIT ../test.out | grep -q 'P I T T S B U R G H');
- then mhmail -s "Sphinx3 test failed" ${S3LIST} < ../test.out;
- elif ! (grep BSTPTH ../test.out | grep -q 'P I T T S B U R G H');
- then mhmail -s "Sphinx3 test failed" ${S3LIST} < ../test.out;
- elif grep -q FAILED ../test.out;
- then mhmail -s "Sphinx3 test failed" ${S3LIST} < ../test.out;
- else mhmail -s "sphinx3 test succeeded" ${S3LIST} < test.out;
+# Compile and run test, and verify that all tests ran successfully
+if ! make all test-full >> $outfile 2>&1 ;
+ then ${MAILX} -s "sphinx3 compilation failed" ${S3LIST} < $outfile;
+ elif ! (grep FWDVIT $outfile | grep -q 'P I T T S B U R G H');
+ then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
+ elif ! (grep BSTPTH $outfile | grep -q 'P I T T S B U R G H');
+ then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
+ elif grep -q FAILED $outfile;
+ then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
+ else ${MAILX} -s "sphinx3 test succeeded" ${S3LIST} < $outfile;
  fi
 
 popd 
 
-/usr/local/bin/cvs -d:ext:${username}@cvs.sf.net:/cvsroot/cmusphinx co SphinxTrain >> ./test.out
+# Fresh download of SphinxTrain
+cvs -d:ext:${address}:/cvsroot/cmusphinx co SphinxTrain > $outfile
 
+# Configure it
 pushd SphinxTrain
-./configure >> ./test
-if ! make all >> test.out 2>&1 ;
- then mhmail -s "SphinxTrain compilation failed" ${STLIST} < test.out;
- else mhmail -s "SphinxTrain compilation succeeded" ${STLIST} < test.out;
+./configure >> $outfile 2>&1
+
+# Coompile and make sure it's successful
+if ! make all >> $outfile 2>&1 ;
+ then ${MAILX} -s "SphinxTrain compilation failed" ${STLIST} < $outfile;
+ else ${MAILX} -s "SphinxTrain compilation succeeded" ${STLIST} < $outfile;
  fi
 popd
 
+# Remove what we created
 cd /tmp
 /bin/rm -rf $root
 
