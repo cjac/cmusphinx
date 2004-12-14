d4caf67679c82d72b6e4cfd347ddc53f382840f9
(no author) <(no author)@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-12-14 19:15:29 +0000

    This commit was manufactured by cvs2svn to create tag 'SPHINX3_5_RCV_IRII'.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/tags/SPHINX3_5_RCV_IRII@4301 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx4/scripts/deployZipCity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PUSH_DEST=/home/groups/c/cm/cmusphinx/htdocs/sphinx4/
-export PUSH_DEST
-cd bld
-tar cvf zipcity.tar zipcity
-sscp zipcity.tar  
-sshh "cd $PUSH_DEST; tar xvf ~/zipcity.tar; rm ~/zipcity.tar;"
-rm zipcity.tar
