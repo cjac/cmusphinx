ceb87595f4be7e3666ce757ce8b8f7537b26202c
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2004-12-14 00:56:57 +0000

    1, Adding test_align.sh.in. Notice that there are wo tests in the same test script, 2, update of sendCFjob.sh - an automatic check out code. make distcheck, send to CF, call compileFarmRunTest.sh in CF side and get back the report.  Currently, no analysis is done to the log. 3, set unlimit in runTest.sh.  This will bypass the problem of CF compilation in netbsd.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4274 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/tests/regression/compileFarmRunTest.sh
+++ src/tests/regression/compileFarmRunTest.sh
@@ -3,13 +3,16 @@
 # Clean thoroughly, just in case
 (cd sphinx3; make -k distclean)
 
-mkdir logCompile
+mkdir ./logCompile
 # Cycle through all machines
 for file in `grep ^1 /etc/compilefarm-hosts  | grep - | awk '{print $NF}'`;
 do
 # Run test for each machine
-ssh $file ~/runTest.sh > $file.log 2>&1
-mv $file.log logCompile
+ssh $file ./runTest.sh > $file.log 2>&1
+mv $file.log ./logCompile 
 done
+
+
+
 
 
