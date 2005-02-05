878cd4e1af358ac5080557915f8639a9763953f6
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-02-05 15:30:07 +0000

    Changed default cepdir to null, so a user can choose not to specify it, and making it consistent with decode.c. Removed assert checking if cepdir was null
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4352 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/programs/main_align.c
+++ src/programs/main_align.c
@@ -196,7 +196,7 @@ static arg_t defn[] = {
       "No. of utterances in -ctl file to be processed (after -ctloffset).  Default: Until EOF" },
     { "-cepdir",
       ARG_STRING,
-      ".",
+      NULL,
       "Directory for utterances in -ctl file (if relative paths specified)." },
     { "-cepext",
       ARG_STRING,
@@ -944,7 +944,7 @@ static void process_ctlfile ( void )
     
     cepdir = (char *) cmd_ln_access("-cepdir");
     cepext = (char *) cmd_ln_access("-cepext");
-    assert ((cepdir != NULL) && (cepext != NULL));
+    assert (cepext != NULL);
     
     ctloffset = *((int32 *) cmd_ln_access("-ctloffset"));
     if (! cmd_ln_access("-ctlcount"))
@@ -1073,7 +1073,13 @@ static void process_ctlfile ( void )
 	nfr = feat_s2mfc2feat(fcb, ctlspec, cepdir, cepext, sf, ef, feat, S3_MAX_FRAMES);
 	
 	if (nfr <= 0){
-	  E_ERROR("Utt %s: Input file read (%s) with dir (%s) and extension (%s) failed \n", uttid, ctlspec,cepdir, cepext);
+	  if (cepdir != NULL) {
+	    E_ERROR("Utt %s: Input file read (%s) with dir (%s) and extension (%s) failed \n", 
+		    uttid, ctlspec, cepdir, cepext);
+	  } else {
+	    E_ERROR("Utt %s: Input file read (%s) with extension (%s) failed \n", 
+		    uttid, ctlspec, cepext);
+	  }
 	}
 	else {
 	    E_INFO ("%s: %d input frames\n", uttid, nfr);
