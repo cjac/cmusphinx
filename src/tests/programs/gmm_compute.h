83578d501a3c1268e33d5e63417551b927631f6b
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-22 22:20:58 +0000

    Adding analysis.c and gmm_compute.[ch] into test/programs.  analysis.c is a program that can genarlly analyse some behavior of the recognizer. This program is currently using gmm_compute as the analysis function. gmm_compute.c analyse the GMM computation and create statistics for a particular database. The program analysis is compiled in the standard compilation procedure now.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4375 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ src/tests/programs/gmm_compute.h
@@ -0,0 +1,67 @@
+/* ====================================================================
+ * Copyright (c) 1999-2004 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
+/************************************************
+ * CMU ARPA Speech Project
+ *
+ * Copyright (c) 2000 Carnegie Mellon University.
+ * ALL RIGHTS RESERVED.
+ ************************************************
+ * 
+ * HISTORY
+ *  
+ * Jan-24-2005  A Chan (archan@cs.cmu.edu) at Carnegie Mellon University
+ *              Started
+ */
+
+#define NBEST_STEP 50
+
+typedef struct stats_s{
+  float32 total_hit;
+  float32 total_senone;
+  float32 total_lastidx_distortion;
+  float32 total_curidx_distortion;
+  float32 total_ci_distortion;
+  int32 total_fr;
+  char* description[1024];
+
+} stats_t;
+
+void gmm_compute (void *data, char *uttfile, int32 sf, int32 ef, char *uttid);
+void init_stat(stats_t *st, char* _desc);
+void add_stat(stats_t *a, stats_t *b);
+void print_stat(stats_t *st);
+void increment_stat(stats_t *a,int32 lst_idx_distort,int32 cur_idx_distort,int32 ci_distort, int32 hit);
+
