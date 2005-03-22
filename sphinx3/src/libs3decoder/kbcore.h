ce12c38aa6496aa00c8662a01d9a2a4301bcc889
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-22 23:31:23 +0000

    Fixed doxygen-style comments
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4382 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/kbcore.h
+++ sphinx3/src/libs3decoder/kbcore.h
@@ -81,23 +81,24 @@ extern "C" {
    */
 
 typedef struct {
-  feat_t *fcb; /** feature end structure */
-  mdef_t *mdef; /** Model definition  */
-  dict_t *dict; /** Dictionary structure */
-  dict2pid_t *dict2pid; /** Conversion of dictionary to Phoneme ID */
-  lm_t *lm; /** LM */
-  lmset_t *lmset; /** LM Set, if it is used, LM is not used */
-  fillpen_t *fillpen; /** Filler penalty */
-  s3lmwid_t *dict2lmwid; /** Dictionary ID to LM ID mapping */
-  mgau_model_t *mgau; /** Model */
-  subvq_t *svq; /** SVQ */
-  gs_t *gs; /** Gaussian Selector */
-  tmat_t *tmat; /** Transition Matrix. */
-  int32 n_lm; /** number of language model */
-  int32 n_alloclm; /* Number of allocated language model */
+  feat_t *fcb; /**< feature end structure */
+  mdef_t *mdef; /**< Model definition  */
+  dict_t *dict; /**< Dictionary structure */
+  dict2pid_t *dict2pid; /**< Conversion of dictionary to Phoneme ID */
+  lm_t *lm; /**< The current LM */
+  lmset_t *lmset; /**< LM Set, if it is used, LM is not used */
+  fillpen_t *fillpen; /**< Filler penalty */
+  s3lmwid_t *dict2lmwid; /**< Dictionary ID to LM ID mapping */
+  mgau_model_t *mgau; /**< Model */
+  subvq_t *svq; /**< SVQ */
+  gs_t *gs; /*<* Gaussian Selector */
+  tmat_t *tmat; /**< Transition Matrix. */
+  int32 n_lm; /**< number of language model */
+  int32 n_alloclm; /**< Number of allocated language model */
 
-  int32 maxNewHeurScore; /* Temporary variables for phoneme lookahead. This stores the heuristic score */
-  int32 lastfrm; /* Temporary variables, should be removed */
+  int32 maxNewHeurScore; /**< Temporary variables for phoneme
+                            lookahead. This stores the heuristic score */
+  int32 lastfrm; /**< Temporary variables, should be removed */
 
 } kbcore_t;
 
@@ -111,40 +112,40 @@ typedef struct {
  * Return value: (obvious) pointer to structure encapsulating the individual data bases.
  * NOTE: If any model fails to initialize, the call fails with a FATAL ERROR.
  */
-kbcore_t *kbcore_init (float64 logbase,		/** Must be specified */
-		       char *feattype,          /** feature type*/
-		       char *cmn,               /** Type of CMN */
-		       char *varnorm,           /** Type of variance normalization */
-		       char *agc,               /** Type of AGC */
-		       char *mdeffile,          /** Model definition file */
-		       char *dictfile,          /** Dictionary file */
-		       char *fdictfile,         /** filler dictionary file */
-		       char *compsep,		/** Must be valid if dictfile specified */
-		       char *lmfile,            /** LM file */
-		       char *lmctlfile,         /** LM control file, mutually exclusive with lmfile */
-		       char *lmdumpdir,         /** Dump LM  */
-		       char *fillpenfile,       /** Filler penality file,*/
-		       char *senmgau,           /** NOT USED */
-		       float64 silprob,		/** Silence probablity Must be valid if lmfile/fillpenfile is
+kbcore_t *kbcore_init (float64 logbase,		/**< log bases used in logs3.c Must be specified */
+		       char *feattype,          /**< feature type*/
+		       char *cmn,               /**< Type of CMN */
+		       char *varnorm,           /**< Type of variance normalization */
+		       char *agc,               /**< Type of AGC */
+		       char *mdeffile,          /**< Model definition file */
+		       char *dictfile,          /**< Dictionary file */
+		       char *fdictfile,         /**< filler dictionary file */
+		       char *compsep,		/**< Must be valid if dictfile specified */
+		       char *lmfile,            /**< LM file */
+		       char *lmctlfile,         /**< LM control file, mutually exclusive with lmfile */
+		       char *lmdumpdir,         /**< Dump LM  */
+		       char *fillpenfile,       /**< Filler penality file,*/
+		       char *senmgau,           /**< NOT USED */
+		       float64 silprob,		/**< Silence probablity Must be valid if lmfile/fillpenfile is
 						   specified */
-		       float64 fillprob,	/** Filler penalty Must be valid if lmfile/fillpenfile is
+		       float64 fillprob,	/**< Filler penalty Must be valid if lmfile/fillpenfile is
 						   specified */
-		       float64 langwt,		/** Language model weight, Must be valid if lmfile/fillpenfile is
+		       float64 langwt,		/**< Language model weight, Must be valid if lmfile/fillpenfile is
 						   specified. */
-		       float64 inspen,		/** Insertion penalty. Must be valid if lmfile/fillpenfile is
+		       float64 inspen,		/**< Insertion penalty. Must be valid if lmfile/fillpenfile is
 						   specified. */
-		       float64 uw,		/** Unigram weight Must be valid if lmfile/fillpenfile is
+		       float64 uw,		/**< Unigram weight Must be valid if lmfile/fillpenfile is
 						   specified. */
-		       char *meanfile,		/** Means Acoustic model... */
-		       char *varfile,		/** Variance file, must be specified if meanfile specified */
-		       float64 varfloor,	/** Variance floowr, must be valid if varfile specified */
-		       char *mixwfile,		/** Mixture weight file, must be specified if meanfile specified */
-		       float64 mixwfloor,	/** Mixture weight floor, must be valid if mixwfile specified */
-		       char *subvqfile,		/** Subvector quantized acoustic model
+		       char *meanfile,		/**< Means Acoustic model... */
+		       char *varfile,		/**< Variance file, must be specified if meanfile specified */
+		       float64 varfloor,	/**< Variance floowr, must be valid if varfile specified */
+		       char *mixwfile,		/**< Mixture weight file, must be specified if meanfile specified */
+		       float64 mixwfloor,	/**< Mixture weight floor, must be valid if mixwfile specified */
+		       char *subvqfile,		/**< Subvector quantized acoustic model
 						   (quantized mean/var values), optional */
-		       char *gsfile,		/** Gaussian Selection Mapping*/
-		       char *tmatfile,          /** Transition matrix */
-		       float64 tmatfloor);	/** Transition probability floors, must be valid if tmatfile specified */
+		       char *gsfile,		/**< Gaussian Selection Mapping*/
+		       char *tmatfile,          /**< Transition matrix */
+		       float64 tmatfloor);	/**< Transition probability floors, must be valid if tmatfile specified */
 
   /** free the kbcore */
   void kbcore_free (kbcore_t *kbcore);
