c893a1f15c8edefcf2f76fd83167fb144ecbcb88
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-22 22:48:18 +0000

    changes in approx_cont_mgau.[ch] since last update, 1, as frame down-sampling (FDS) always cause a lot of degradation, instead of ignore the whole frame when frame is skipped, we just used a tightened beam.  This beam is controlled by tighten_factor specified in the command-line. 2, In 3.x (x<5), CIGMMS was causing a lot of trouble on those CD senones which are not computed in detail.  We use the so called best index tricks in this problem.  If the best index of the last frame for a particular senone, we assume the previous frame best index to be the best index for this frame as well. (literature said this is true for 70% for 8 mixture. I tested it and it is quite true.)  As the best Gaussian usually dominates the GMM scores, we just need to compute the score using 1 Gaussian index. 3, Cosmetic changes, I moved most of the detail comment from the .c file to the .h file.  Also doxygen style's comments are mostly fixed.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4376 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/libam/approx_cont_mgau.h
+++ sphinx3/src/libs3decoder/libam/approx_cont_mgau.h
@@ -81,37 +81,151 @@ extern "C" {
       Arthur Chan's web site at www.cs.cmu.edu/~archan/ .  
   */
 
-  /**
- * Evaluate the approximation gaussian score for one frame. 
- */
-int32 approx_cont_mgau_frame_eval (kbcore_t * kbc,  /** Input, kbcore, for mdef, svq and gs*/
-				   fast_gmm_t *fastgmm,	 /** Input/Output: wrapper for
+/** 
+   approx_con_mgau_frame_eval encapsulates all approximations in the
+   Gaussian computation.  This assumes programmers NOT to initialize
+   the senone scores at every frame before using this function. This
+   modularize this routine but complicated issues such as
+   frame-dropping which can also be done in the front-end
+
+   This layer of code controls the optimization performance in Frame
+   Leval and GMM Level.
+
+   Frame Level:
+
+   ^^^^^^^^^^^^
+
+   We select to compute the scores only if it is not similar to the
+   most recently computed frames.  There are multiple ways to
+   configures this.
+
+   Naive down-sampling : Skip the computation one every other n-frames
+
+   Conditional down-sampling : Skip the computation only if the
+   current frame doesn't belong to the same neighborhood of the same
+   frame.  This neighborhood corresponds to the codeword which the
+   feature vector found to be the closest.
+   
+   No matter which down-sampling was used, the following problem will
+   appear in the computation.  Active senones of frame which supposed
+   to be skipped in computation could be not computed in the most
+   recently computed frame.  In those cases, we chose to compute those
+   senones completely.
+
+   GMM Level:
+
+   ^^^^^^^^^^
+
+   In the implementation of CI-based GMM selection makes use of the
+   fact that in s3.3 , CI models are always placed before all CD
+   models. Hence the following logic is implemented:
+
+   if(it is CI senone)
+      compute score
+   else if (it is CD senone)
+      if the ci-phone beam was not set 
+          compute score
+      else
+          if the CD senone's parent has a score within the beam
+	     compute_score
+	  else CD senone's parent has a score out of the beam
+	     back-off using the parent senone score. 
+
+   During s3.5, the idea of bestidx in a GMM has been changed and 
+   the above logic becomes
+
+   if(it is CI senone)
+      compute score
+   else if (it is CD senone)
+      if the ci-phone beam was not set 
+          compute score
+      else
+          if the CD senone's parent has a score within the beam
+	     compute_score
+	  else CD senone's parent has a score out of the beam
+	     if the bestindex of the last frame exists
+	        compute score using the bestidx
+	     then
+  	        back-off using the parent senone score. 
+	  
+   About renormalization
+
+   ^^^^^^^^^^^^^^^^^^^^^
+
+   Sphinx 3.4 generally renormalize the score using the best
+   score. Notice that this introduce extra complication to the
+   implementation.  I have separated the logic of computing or not
+   computing the scores.  This will clarify the code a bit.
+   
+   Accounting of senone and gaussian computation
+
+   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+   This function assumes approx_cont_mgau_ci_eval was run before it,
+   hence at the end the score was added on top of the it.
+   
+   Design
+
+   ^^^^^^ 
+
+   The whole idea of this function is based on my paper on "4-level
+   categorization of GMM computation " which basically describe how
+   different techniques of fast GMM computation should interact with
+   each others.  The current implementation was there to make the
+   code to be as short as possible. I hope that no one will try to make
+   the code to be longer than 500 lines. 
+
+   Imperfection
+   ^^^^^^^^^^^^
+
+   Imperfections of the code can be easily seen by experts so I want
+   to point out before they freak out.  There are synchronization
+   mechanism in the bestindex and rec_sen_active. That can easily be a
+   source of error.  I didn't do it because somehow when you trust
+   just the best matching index of the previous frame is slightly
+   different from if you trust the score of the previous frame
+
+   The sen_active, rec_sen_active and senscr should be inside the GMM
+   structure rather than just a separate array. I didn't fix it
+   because this change will also touch other data structures as well. 
+
+   @see approx_mgau_eval 
+   @return the best senone score
+*/
+
+int32 approx_cont_mgau_frame_eval (kbcore_t * kbc,  /**< Input, kbcore, for mdef, svq and gs*/
+				   fast_gmm_t *fastgmm,	 /**< Input/Output: wrapper for
 							    parameters for Fast GMM , for
 							    all beams and parameters, during
 							    the computation, the */
-				   float32 *feat,	/**Input: the current feature vector */
-				   int32 frame,         /**Input: the current frame number */
-				   int32 *sen_active,	/**Input: the current active senones */
-				   int32 *rec_sen_active, /**Input: the most recent active senones */
-				   int32 *senscr,         /**Output: the output senone scores */
-				   int32 *cache_ci_senscr, /**Input: the CI senone scores for CI GMMS */
-				   ptmr_t *tm_ovrhd        /**Output: the timer used for computing overhead */
+				   float32 *feat,	/**< Input: the current feature vector */
+				   int32 frame,         /**< Input: the current frame number */
+				   int32 *sen_active,	/**< Input: the current active senones */
+				   int32 *rec_sen_active, /**< Input: the most recent active senones */
+				   int32 *senscr,         /**< Output: the output senone scores */
+				   int32 *cache_ci_senscr, /**< Input: the CI senone scores for CI GMMS */
+				   ptmr_t *tm_ovrhd        /**< Output: the timer used for computing overhead */
 				   );
 
 
   /**
-   * Evaluate the approximation gaussian score for CI senone for one frame. 
+   * Evaluate the approximate gaussian score for CI senone for one
+   * frame.  In Sphinx 3.X (X=4,5), this routine is used to precompute
+   * CI senone score as a kind of approximate match of the CD score.
    */
 
+
 void approx_cont_mgau_ci_eval (
-			       kbcore_t *kbc, /** Input, kbcore, for mdef, svq and gs*/
-			       fast_gmm_t *fg, /** Input/Output: wrapper for
+			       kbcore_t *kbc, /**< Input, kbcore, for mdef, svq and gs*/
+			       fast_gmm_t *fg, /**< Input/Output: wrapper for
 							    parameters for Fast GMM , for
 							    all beams and parameters, during
 							    the computation, the */
-			       mdef_t *mdef,  /** Input : model definition */
-			       float32 *feat, /* Input : the current frame of feature */
-			       int32 *ci_senscr /* Output : the ci senscore for this frame */
+			       mdef_t *mdef,  /**< Input : model definition */
+			       float32 *feat, /**< Input : the current frame of feature */
+			       int32 *ci_senscr, /**< Output : the ci senscore for this frame */
+			       int32 fr /**< Input : The frame number */
+
 			       );
 
 #ifdef __cplusplus
