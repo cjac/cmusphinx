49615740fb1b4ac09291cf80609a4d3515f1be49
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-22 23:28:38 +0000

    Detail documentation of kb.[ch] and add tighten_factor in this layter.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4381 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/libsearch/kb.h
+++ sphinx3/src/libs3decoder/libsearch/kb.h
@@ -92,10 +92,10 @@ extern "C" {
    *
    */
 typedef struct {
-  kbcore_t *kbcore;		/** Core model structures */    
+  kbcore_t *kbcore;		/**< Core model structures */    
 
   /*Feature related variables*/
-  float32 ***feat;		/** Feature frames */
+  float32 ***feat;		/**< Feature frames */
 
   /*Search related variables, e.g lexical tree and Viterbi history*/
 
@@ -105,106 +105,114 @@ typedef struct {
    * alleviates the problem of excessive Viterbi pruning in lextrees.
    */
 
-  int32 n_lextree;		/** See above comment about n_lextree */
+  int32 n_lextree;		/**< See above comment about n_lextree */
   lextree_t **ugtree;
   lextree_t **fillertree;
-  int32 n_lextrans;		/** #Transitions to lextree (root) made so far */
-  lextree_t **ugtreeMulti;  /** This data structure allocate all trees for all LMs specified by the users */
-  vithist_t *vithist;		/** Viterbi history, built during search */
+  int32 n_lextrans;		/**< #Transitions to lextree (root) made so far */
+  lextree_t **ugtreeMulti;  /**< This data structure allocate all trees for all LMs specified by the users */
+  vithist_t *vithist;		/**< Viterbi history, built during search */
 
-  char *uttid;                  /** Utterance ID */
-  int32 nfr;			/** #Frames in feat in current utterance */
-  int32 tot_fr;                 /** The total number of frames that the
+  char *uttid;                  /**< Utterance ID */
+  int32 nfr;			/**< #Frames in feat in current utterance */
+  int32 tot_fr;                 /**< The total number of frames that the
                                    recognizer has been
                                    recognized. Mainly for bookeeping.  */
     
-  cmn_t *cmn;                   /** The structure for cepstral mean normalization. */
+  cmn_t *cmn;                   /**< The structure for cepstral mean normalization. */
 
   /** Thing that are used by multiple parts of the recognizer.  Pretty
      hard to wrap them into one data structure. Just leave it there
      for now. */
 
-  int32 *ssid_active;		/** For determining the active senones in any frame */
-  int32 *comssid_active;        /** Composite senone active */
-  int32 *sen_active;            /** Structure that record whether the current state is active. */
-  int32 *rec_sen_active;        /** Most recent senone active state */
+  int32 *ssid_active;		/**< For determining the active senones in any frame */
+  int32 *comssid_active;        /**< Composite senone active */
+  int32 *sen_active;            /**< Structure that record whether the current state is active. */
+  int32 *rec_sen_active;        /**< Most recent senone active state */
 
-  int32 **cache_ci_senscr;     /** Cache of ci senscr in the next pl_windows frames, include this frame.*/
-  int32 *cache_best_list;        /** Cache of best the ci sensr the next pl_windows, include this frame*/
+  int32 **cache_ci_senscr;     /**< Cache of ci senscr in the next pl_windows frames, include this frame.*/
+  int32 *cache_best_list;        /**< Cache of best the ci sensr the next pl_windows, include this frame*/
 
-  int32 bestscore;	/** Best HMM state score in current frame */
-  int32 bestwordscore;	/** Best wordexit HMM state score in current frame */
+  int32 bestscore;	/**< Best HMM state score in current frame */
+  int32 bestwordscore;	/**< Best wordexit HMM state score in current frame */
     
-  ascr_t *ascr;		  /** Senone and composite senone scores for one frame */
-  beam_t *beam;		  /** Structure that wraps up parameters related to beam pruning */
-  histprune_t *histprune; /** Structure that wraps up parameters related to histogram pruning */
-  fast_gmm_t *fastgmm;         /** Structure that wraps up fast GMM computation */
+  ascr_t *ascr;		  /**< Senone and composite senone scores for one frame */
+  beam_t *beam;		  /**< Structure that wraps up parameters related to beam pruning */
+  histprune_t *histprune; /**< Structure that wraps up parameters related to histogram pruning */
+  fast_gmm_t *fastgmm;         /**< Structure that wraps up fast GMM computation */
 
-  int32 *hmm_hist;		/** Histogram: #frames in which a given no. of HMMs are active */
-  int32 hmm_hist_bins;	/** #Bins in above histogram */
-  int32 hmm_hist_binsize;	/** Binsize in above histogram (#HMMs/bin) */
+  int32 *hmm_hist;		/**< Histogram: #frames in which a given no. of HMMs are active */
+  int32 hmm_hist_bins;	/**< #Bins in above histogram */
+  int32 hmm_hist_binsize;	/**< Binsize in above histogram (#HMMs/bin) */
     
   /* All structure that measure the time and stuffs we computed */
-  ptmr_t tm_sen;    /** timer for senone computation */
-  ptmr_t tm_srch;   /** timer for search */
-  ptmr_t tm_ovrhd; /** timer for GMM computation overhead */
+  ptmr_t tm_sen;    /**< timer for senone computation */
+  ptmr_t tm_srch;   /**< timer for search */
+  ptmr_t tm_ovrhd; /**< timer for GMM computation overhead */
   
-  int32 utt_hmm_eval; /** HMM evaluated for utterance */
-  int32 utt_sen_eval; /** CD Senones evaluated for utterance */
-  int32 utt_gau_eval; /** CD Gaussians evaluated for utterance */
-  int32 utt_cisen_eval; /** CI Senones evaluated for utterance */
-  int32 utt_cigau_eval; /** CI Gaussians evaluated for utterance */
+  int32 utt_hmm_eval; /**< HMM evaluated for utterance */
+  int32 utt_sen_eval; /**< CD Senones evaluated for utterance */
+  int32 utt_gau_eval; /**< CD Gaussians evaluated for utterance */
+  int32 utt_cisen_eval; /**< CI Senones evaluated for utterance */
+  int32 utt_cigau_eval; /**< CI Gaussians evaluated for utterance */
   
-  float64 tot_sen_eval;	/* Total CD Senones evaluated over the entire session */
-  float64 tot_gau_eval;	/* Total CD Gaussian densities evaluated over the entire session */
-  float64 tot_ci_sen_eval;	/* Total CI Senones evaluated over the entire session */
-  float64 tot_ci_gau_eval;	/* Total CI Senones evaluated over the entire session */
+  float64 tot_sen_eval;	/**< Total CD Senones evaluated over the entire session */
+  float64 tot_gau_eval;	/**< Total CD Gaussian densities evaluated over the entire session */
+  float64 tot_ci_sen_eval;	/**< Total CI Senones evaluated over the entire session */
+  float64 tot_ci_gau_eval;	/**< Total CI Senones evaluated over the entire session */
 
-  float64 tot_hmm_eval;	/* Total HMMs evaluated over the entire session */
-  float64 tot_wd_exit;	/* Total Words hypothesized over the entire session */
+  float64 tot_hmm_eval;	/**< Total HMMs evaluated over the entire session */
+  float64 tot_wd_exit;	/**< Total Words hypothesized over the entire session */
   
-  FILE *matchfp; /* File handle for the match file */
-  FILE *matchsegfp; /* File handle for the match segmentation file */
+  FILE *matchfp; /**< File handle for the match file */
+  FILE *matchsegfp; /**< File handle for the match segmentation file */
 
 
   /** ARCHAN: Phoneme lookahead stuffs I want to move to somewhere
       else. */
-  int32 *phn_heur_list;          /** Cache of best the ci phoneme scores in the next pl_windows, include this frame*/
-  int32 pl_win;            /** The window size of phoneme look-ahead */
-  int32 pl_win_strt;      /** The start index of the window near the end of a block */
-  int32 pl_win_efv;  /** Effective window size in livemode */
-  int32 pl_beam;              /** Beam for phoneme look-ahead */
+  int32 *phn_heur_list;          /**< Cache of best the ci phoneme scores in the next pl_windows, include this frame*/
+  int32 pl_win;            /**< The window size of phoneme look-ahead */
+  int32 pl_win_strt;      /**< The start index of the window near the end of a block */
+  int32 pl_win_efv;  /**< Effective window size in livemode */
+  int32 pl_beam;              /**< Beam for phoneme look-ahead */
 
-  int32 *wordbestscore; /** The word best score */
-  int32 *wordbestexit; /** The word best exit */
-  int32 epl;  /** End of word penalty */
+  int32 *wordbestscore; /**< The word best score */
+  int32 *wordbestexit; /**< The word best exit */
+  int32 epl;  /**< End of word penalty */
 
   /*variables for mllrmatrix */
-  char* prevmllrfn; /** Last MLLR matrix files */
-  float32** regA; /** Regression matrix : the multiplcation term */
-  float32* regB; /** Regression matrix : the bias term */
+  char* prevmllrfn; /**< Last MLLR matrix files */
+  float32** regA; /**< Regression matrix : the multiplcation term */
+  float32* regB; /**< Regression matrix : the bias term */
 
 } kb_t;
 
 
   /** Initialize the kb */
-void kb_init (kb_t *kb);
+  void kb_init (kb_t *kb /**< In/Out: A empty kb_t */
+	      );
   /** Swap lexical trees */
 
-void kb_lextree_active_swap (kb_t *kb);
+void kb_lextree_active_swap (kb_t *kb /**< In/Out: A empty kb_t */
+			     );
   
   /** Deallocate the kb structure */
-
-void kb_free (kb_t *kb);	/* RAH 4.16.01 */
+void kb_free (kb_t *kb /**< In/Out: A empty kb_t */
+	      );	/* RAH 4.16.01 */
   
-  /** Set LM */
-void kb_setlm(char* lmname,kb_t *kb);    /* ARCHAN 20040228 */
+  /** Use LM with lmname */
+void kb_setlm(char* lmname, /**< In: The name of the language model */
+	      kb_t *kb /**< In/Out: A empty kb_t */
+	      );    /* ARCHAN 20040228 */
 
   /** Set MLLR */
-void kb_setmllr(char* mllrname,kb_t *kb);    /* ARCHAN 20040724 */
+void kb_setmllr(char* mllrname, /**< In: The name of the mllr model */
+		kb_t *kb /**< In/Out: A empty kb_t */
+		);    /* ARCHAN 20040724 */
 
-  /** Set Utterance ID */
-void kb_set_uttid(char *uttid,kb_t *kb);   /* ARCHAN 20041111 */
+  /** Set Utterance ID to uttid */
+  void kb_set_uttid(char *uttid, /**< In: the new utt id */
+		  kb_t *kb    /**< In/Out: A empty kb_t */
+		  );   /* ARCHAN 20041111 */
 
 
 #ifdef __cplusplus
