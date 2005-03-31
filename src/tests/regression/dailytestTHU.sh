443da93e020c5610a436af8f1da588935d032f9e
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-31 19:25:09 +0000

    Changed format of daily test file
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4400 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/tests/regression/dailytestTHU.sh
+++ src/tests/regression/dailytestTHU.sh
@@ -1,1 +1,13 @@
-make -C $(top_srcdir)/src/tests/performance/Communicator base fast
+# The file format is:
+#
+# /path/to/test/location  target
+#
+# where the path is to the location where the test is located
+# (e.g. ti46, rm1 etc) and target is a target for a single test
+# (as opposed to a target that points to other targets)
+#
+./src/tests/performance/Communicator com-fast-lp
+./src/tests/performance/Communicator com-fast-decode
+./src/tests/performance/Communicator com-base-lp
+./src/tests/performance/Communicator com-base-decode
+./src/tests/performance/Communicator com-base-decany
--- src/tests/regression/dailytestTUE.sh
+++ src/tests/regression/dailytestTUE.sh
@@ -1,3 +1,14 @@
-make -C $(top_srcdir)/src/tests/performance/tidigits quick slow
-
-
+# The file format is:
+#
+# /path/to/test/location  target
+#
+# where the path is to the location where the test is located
+# (e.g. ti46, rm1 etc) and target is a target for a single test
+# (as opposed to a target that points to other targets)
+#
+./src/tests/performance/tidigits tidigits-quick-lp
+./src/tests/performance/tidigits tidigits-quick-decode
+./src/tests/performance/tidigits tidigits-quick-decany
+./src/tests/performance/tidigits tidigits-lp
+./src/tests/performance/tidigits tidigits-decode
+./src/tests/performance/tidigits tidigits-decany
