cc0e99196c3b4d0b12a16d448bb2152f52874785
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-03-31 19:28:53 +0000

    Added scripts for regression test. Not tested yet, still debugging
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4401 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ src/tests/regression/regression_launch.pl
@@ -0,0 +1,83 @@
+#!/usr/local/bin/perl
+
+use strict;
+use Cwd qw(abs_path cwd);
+
+die "Usage: $0 <test name> <test type>\n" if ($#ARGV < 1);
+
+my $test_name = shift;
+my $test_type = shift;
+
+my $regression_dir = cwd;
+
+my $dependence = "";
+
+my $local_regression_log = "$regression_dir/local_regression.log";
+
+open (REGRESSION, ">$local_regression_log") or exit -1;
+
+my $s3_root = abs_path("$regression_dir/../../..");
+
+# Compile
+my $compilation = system("cd $s3root; autogen.sh && autogen.sh && make");
+
+if ($compilation) {
+  # Error in compilation
+  print REGRESSION "compile|failed";
+} else {
+  print REGRESSION "compile|OK";
+
+  # For each line in the test file, run it
+  my $qsub;
+  if ($test_type =~ m/^speed$/i) {
+    $qsub = "qsub -q s4";
+  } elsif ($test_type =~ m/^accuracy$/i) {
+    $qsub = "qsub";
+  } else {
+    $qsub = "";
+    warn "No test type defined. Assuming local test";
+  }
+  if (open (CTL, "<$test_file")) {
+    while (<CTL>) {
+      my ($test_dir, $test_target) = split /\s+/;
+      chdir "$s3_root/$test_dir";
+      my $current_time = time();
+      my $temp_script = "tmp_script$current_time";
+      open (TEMP, ">$temp_script") 
+	or (
+	    warn "Could not create temp file at $test_dir\n";
+	    next;
+	   );
+      print TEMP "#\n";
+      print TEMP "cd $s3_root/$test_dir\n";
+      print TEMP make "$test_target\n";
+      close(TEMP);
+      my $job = `$qsub $temp_script`;
+      if ($qsub ne "") {
+	$job =~ s/\..*//;
+	$depedence .= ":" . "$job";
+      }
+      unlink $temp_script;
+    }
+    close(CTL);
+  }
+}
+
+close(REGRESSION);
+my job_id = "";
+if ($dependence eq "") {
+  system("perl regression_report.pl $localregression_log $test_file");
+} else {
+  my $regression_script = "$regression_dir/regression.pl";
+  open (REPORT, ">$regression_script") or exit (-1);
+  print REGRESSION, "#\n";
+  print REGRESSION "cd $regression_dir\n";
+  print REGRESSION "perl regression_report.pl $localregression_log $test_file");
+  close(REGRESSION);
+  $job_id = `$qsub -W:afterany$dependence $regression_script`;
+}
+
+print "$job_id";
+
+exit (0);
+
