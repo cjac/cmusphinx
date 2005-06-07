cf37c10ec5d22866b44181b2ffa311d10e5635f6
dhdfu <dhdfu@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-07 19:32:04 +0000

    Update ms_mllr to support multiple regression classes.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4470 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/libam/ms_mllr.h
+++ src/libs3decoder/libam/ms_mllr.h
@@ -33,19 +33,19 @@ extern "C" {
  * Return value: 0 if successful, -1 otherwise.
  */
 int32 ms_mllr_read_regmat (const char *regmatfile,	/** In: File to be read */
-			float32 ****A,		/** Out: [*A][stream][streamlen][streamlen] */
-			float32 ***B,		/** Out: [*B][stream][streamlen] */
-			int32 *streamlen,	/** In: Dimension of each feature stream */
-			int32 n_stream);	/** In: #Feature streams */
+			   float32 *****A,		/** Out: [*A][stream][nclass][streamlen][streamlen] */
+			   float32 ****B,		/** Out: [*B][stream][nclass][streamlen] */
+			   int32 *streamlen,	/** In: Dimension of each feature stream */
+			   int32 n_stream,	/** In: #Feature streams */
+			   int32 *nclass);	/** Out: number of classes */
 
   /**
  * Free a regression matrix previously read in by mllr_read_regmat.
  * Return value: 0 if successful, -1 otherwise.
  */
-int32 ms_mllr_free_regmat (float32 ***A,		/** In: A[stream][streamlen][streamlen] */
-			float32 **B,		/** In: B[stream][streamlen] */
-			int32 *streamlen,	/** In: Dimension of each feature stream */
-			int32 n_stream);	/** In: #Feature streams */
+int32 ms_mllr_free_regmat (float32 ****A,	/** In: A[stream][nclass][streamlen][streamlen] */
+			   float32 ***B,	/** In: B[stream][nclass][streamlen] */
+			   int32 n_stream);	/** In: #Feature streams */
 
   /**
  * Transform a mixture density mean matrix according to the given regression matrix.
@@ -54,10 +54,11 @@ int32 ms_mllr_free_regmat (float32 ***A,		/** In: A[stream][streamlen][streamlen
 int32 ms_mllr_norm_mgau (float32 ***mean,	/** In/Out: Set of vectors to be transformed:
 					   mean[stream][density] */
 		      int32 n_density,	/** In: #densities in above mean matrix */
-		      float32 ***A,	/** In: "matrix" portion of regression matrix */
-		      float32 **B,	/** In: "vector" portion of regression matrix */
+		      float32 ****A,	/** In: "matrix" portion of regression matrix */
+		      float32 ***B,	/** In: "vector" portion of regression matrix */
 		      int32 *streamlen,	/** In: Dimension of each feature stream */
-		      int32 n_stream);	/** In: #Feature streams */
+		      int32 n_stream,	/** In: #Feature streams */
+		      int32 class);	/** In: number of classes */
 
 #ifdef __cplusplus
 }
