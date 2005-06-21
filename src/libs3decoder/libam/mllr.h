fc47c2de735e4c881b01ae04a5658da001f82f32
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-21 18:50:45 +0000

    Log. 1, Fixed doxygen documentation, 2, Added $Log$ keyword.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4501 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/libam/mllr.h
+++ src/libs3decoder/libam/mllr.h
@@ -44,6 +44,19 @@
  * **********************************************
  * 
  * HISTORY
+ * $Log$
+ * Revision 1.6  2005/06/21  18:50:45  arthchan2003
+ * Log. 1, Fixed doxygen documentation, 2, Added $Log$ keyword.
+ * 
+ * Revision 1.5  2005/06/19 19:41:22  archan
+ * Sphinx3 to s3.generic: Added multiple regression class for single stream MLLR. Enabled MLLR for livepretend and decode.
+ *
+ * Revision 1.4  2005/06/13 04:02:55  archan
+ * Fixed most doxygen-style documentation under libs3decoder.
+ *
+ * Revision 1.3  2005/03/30 01:22:47  archan
+ * Fixed mistakes in last updates. Add
+ *
  * 
  * 24-Jul-04    ARCHAN (archan@cs.cmu.edu) at Carnegie Mellon University
  *              First checked in from sphinx 3.0 to sphinx 3.5
@@ -70,37 +83,41 @@ extern "C" {
 
 
   /** Dump the regression matrix from a given file */
-void mllr_dump(float32 ***A, float32 **B,int32 veclen, int32 nclass);
+  void mllr_dump(float32 ***A, /**< In: [nclass][streamlen][streamlen]*/
+		 float32 **B,    /**< In: [nclass][streamlen]*/
+		 int32 veclen,  /**< In: vector length*/
+		 int32 nclass   /**< In: Number of classes */
+		 );
 /*
  * Load a regression matrix from the given file.  Space for the matrix is allocated
  * by this routine.  (The regression "matrix" is actually a matrix A and a vector B.)
- * Return value: 0 if successful, -1 otherwise.
+ * @return 0 if successful, -1 otherwise.
  */
-int32 mllr_read_regmat (const char *regmatfile,	/* In: File to be read */
-			float32 ****A,		/* Out: [*A][nclass][streamlen][streamlen] */
-			float32 ***B,		/* Out: [*B][nclass][streamlen] */
-			int32 *nclass,		/* Out: number of classes */
-			int32 ceplen);          /* In: vector length */
+int32 mllr_read_regmat (const char *regmatfile,	/**< In: File to be read */
+			float32 ****A,		/**< Out: [*A][nclass][streamlen][streamlen] */
+			float32 ***B,		/**< Out: [*B][nclass][streamlen] */
+			int32 *nclass,		/**< Out: number of classes */
+			int32 ceplen);          /**< In: vector length */
 
   /**
  * Free a regression matrix previously read in by mllr_read_regmat.
- * Return value: 0 if successful, -1 otherwise.
+ * @return 0 if successful, -1 otherwise.
  */
-int32 mllr_free_regmat (float32 ***A,		/* In: A[streamlen][streamlen] */
-			float32 **B		/* In: B[streamlen] */
+int32 mllr_free_regmat (float32 ***A,		/**< In: A[streamlen][streamlen] */
+			float32 **B		/**< In: B[streamlen] */
 			);
 
 
   /**
  * Transform a mixture density mean matrix according to the given regression matrix.
- * Return value: 0 if successful, -1 otherwise.
+ * @return 0 if successful, -1 otherwise.
  */
 
-int32 mllr_norm_mgau (mgau_model_t *mgauset, /** In/Out: The gaussian distribution needs to be transformed */
-		      float32 ***A,	/** In: "matrix" portion of regression matrix */
-		      float32 **B,	/** In: "vector" portion of regression matrix */
-		      int32 nclass,	/** In: number of classes */
-		      int32 *cb2mllr	/** In: class to senone mapping */
+int32 mllr_norm_mgau (mgau_model_t *mgauset, /**< In/Out: The gaussian distribution needs to be transformed */
+		      float32 ***A,	/**< In: "matrix" portion of regression matrix */
+		      float32 **B,	/**< In: "vector" portion of regression matrix */
+		      int32 nclass,	/**< In: number of classes */
+		      int32 *cb2mllr	/**< In: class to senone mapping */
 		      );
 
 #ifdef __cplusplus
