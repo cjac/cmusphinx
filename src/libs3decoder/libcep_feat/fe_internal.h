1cc88606dec467b71a36ea8b1da13e028d177133
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-21 20:34:59 +0000

    Add doxygen comments to all functions to fe.h. Due to my limitation, I don't believe the documentation is correct enough.  The material is just to allow other develepors to have something to base on.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4508 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/libcep_feat/fe_internal.h
+++ src/libs3decoder/libcep_feat/fe_internal.h
@@ -56,53 +56,124 @@ typedef struct { float64 r, i; } complex;
 
   /** Build mel filters */
 
-int32 fe_build_melfilters(melfb_t *MEL_FB);
+  int32 fe_build_melfilters(melfb_t *MEL_FB /**< A mel-frequency banks data structure */
+			  );
 
-  /** Compute mel cosine */
-int32 fe_compute_melcosine(melfb_t *MEL_FB);
-float32 fe_mel(float32 x);
-float32 fe_melinv(float32 x);
+  /** Compute mel cosine. Effectively, applied discrete cosine transform on the filter banks
+      output. 
+   */
+  int32 fe_compute_melcosine(melfb_t *MEL_FB /**< A mel-frequency banks data structure */
+			   );
 
-  /** Perform pre-emphasis */
-void fe_pre_emphasis(int16 const *in, float64 *out, int32 len, float32 factor, int16 prior);
+  /** Convert a number to mel scale */
+  float32 fe_mel(float32 x /**< A number in real-domain*/
+	       );
+
+  /** Convert a number from mel scale to real domain */
+  float32 fe_melinv(float32 x /**< A number in mel-scale */
+		  );
+
+  /** Perform pre-emphasis 
+      The output will be produced based on the following formula:
+      
+      o[0]=in[0]-factor * prior
+      o[i]=in[i]-factor * in[i-1]
+   */
+  void fe_pre_emphasis(int16 const *in, /**<Input: The input vector */
+		       float64 *out,  /**<Output: The output vector*/
+		       int32 len,     /**<Input: The length of the input vector in*/
+		       float32 factor,  /**<Input: The preemphasis factor */
+		       int16 prior      /**< Input: The value that is assumed to be before the first sample */
+		       );
 
   /** Create a hamming window */
 void fe_create_hamming(float64 *in, int32 in_len);
 
-  /** Apply hamming window */
-void fe_hamming_window(float64 *in, float64 *window, int32 in_len);
+  /** Apply a hamming window */
+  void fe_hamming_window(float64 *in, /**< Input: The input vector */
+			 float64 *window,  /**< Input: The type of window */
+			 int32 in_len /**<Input: the length of the input evector in */
+		       );
 
-  /** Initialize hamming window */
-void fe_init_hamming(float64 *win, int32 len);
+  /** Initialize hamming window 
+      The hamming window is initialized using the following formula.
+      in[i] = 0.54  - 0.46 * cos (2 * PI * i / (length -1))
+   */
+  void fe_init_hamming(float64 *win, /**< Input/Output: The input/output vector */
+		       int32 len       /**< Input: The length of the vector */
+		     );
 
-  /** Compute the magnitude of a spectrum */
-void fe_spec_magnitude(float64 const *data, int32 data_len, float64 *spec, int32 fftsize);
+  /** Compute the magnitude of a spectrum 
+   */
+  void fe_spec_magnitude(float64 const *data, /**< Input : The input data */
+			 int32 data_len,  /**< Input : The data length*/
+			 float64 *spec, /**<Input : The output spectrum*/
+			 int32 fftsize /**<Input: The size of FFT */
+			 );
   
-  /** Compute the feature from frame */
-int32 fe_frame_to_fea(fe_t *FE, float64 *in, float64 *fea); 
+  /** Compute the feature from frame 
+   */
+  int32 fe_frame_to_fea(fe_t *FE,  /**< Input: A FE structure */
+			float64 *in,  /**< Input: The input data */
+			float64 *fea  /**< Output: The output feature vector */
+			); 
 
   /** Mel spectrum */
-void fe_mel_spec(fe_t *FE, float64 const *spec, float64 *mfspec);
+  void fe_mel_spec(fe_t *FE,  /**< Input: A FE structure */
+		   float64 const *spec,  /**< Input: The spectrum vector */
+		   float64 *mfspec  /**< Output: A mel spectral vector after passing through the filter banks*/
+		   );
 
   /** Mel cepstrum */
 
-int32 fe_mel_cep(fe_t *FE, float64 *mfspec, float64 *mfcep);
+  int32 fe_mel_cep(fe_t *FE,  /**< Input: A FE structure */
+		   float64 *mfspec, /**< Input:  mel spectral vector after passing through the filter banks */
+		   float64 *mfcep   /**< Output: mel cepstral vector after */
+		   );
 
-  /** FFT */
+  /** FFT Implementation of both fast Fourier transform (FFT) and
+      inverse fast Fourier transform (IFFT)
+  */
 
-int32 fe_fft(complex const *in, complex *out, int32 N, int32 invert);
-void fe_short_to_double(int16 const *in, float64 *out, int32 len);
+  int32 fe_fft(complex const *in, /**< Input: The input vector in complex numbers */
+	       complex *out,      /**< Output: The output vector in complex numbers */
+	       int32 N,           /**< The size of the FFT */
+	       int32 invert       /**< invert=-1, IFFT, invert=1, FFT*/
+	       );
 
-  /** Front end specific memory allocation routine */
-char **fe_create_2d(int32 d1, int32 d2, int32 elem_size);
+  /** Convert short to double
+   */
+  void fe_short_to_double(int16 const *in, /**< Input: vector in short */
+			  float64 *out,    /**< Output: vector in double */
+			  int32 len        /**< Input: Length of the vector */
+			  );
 
-  /** Front end specific memory delallocation routine */
-void fe_free_2d(void **arr);
+  /** DUPLICATION! Front end specific memory allocation routine 
+      Create a 2D array. 
+      @return: a d1 x d2 array will be returned
+   */
+  char **fe_create_2d(int32 d1, /** Input: first dimension*/
+		      int32 d2,  /** Input: second dimension*/
+		      int32 elem_size /** Input : the size of element */
+		      );
+
+  /** DUPLICATION! Front end specific memory delallocation routine */
+  void fe_free_2d(void **arr /**Input: a 2d vector */
+		);
 
   /** Print the fe structure.  */
-void fe_print_current(fe_t *FE);
-void fe_parse_general_params(param_t const *P, fe_t *FE);
-void fe_parse_melfb_params(param_t const *P, melfb_t *MEL);
+  void fe_print_current(fe_t *FE /** Input: an FE structure */
+		      );
+  
+  /**Parse general parameters from a param_t */
+  void fe_parse_general_params(param_t const *P, /**Input: a param_t structure*/
+			       fe_t *FE /**Output: a FE structure */
+			       );
+
+  /**Parse mel frequency parameters from a param_t */
+  void fe_parse_melfb_params(param_t const *P, /**Input: a param_t structure*/
+			     melfb_t *MEL /**Output: a FE structure */
+			     );
 
 #ifdef __cplusplus
 }
