28d7100f1566db9ca1296d0ba1064240beec8e69
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-21 23:34:39 +0000

    Remove all dag functions. Eventually I may just want to delete the whole file as well.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4534 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/libsearch/search.h
+++ sphinx3/src/libs3decoder/libsearch/search.h
@@ -46,9 +46,48 @@
  * HISTORY
  * 
  * $Log$
- * Revision 1.3  2004/12/06  10:52:01  arthchan2003
- * Enable doxygen documentation in libs3decoder
+ * Revision 1.4  2005/06/21  23:34:39  arthchan2003
+ * Remove all dag functions. Eventually I may just want to delete the whole file as well.
  * 
+ * Revision 1.2  2005/06/03 05:46:19  archan
+ * Refactoring across dag/astar/decode_anytopo.  Code is not fully tested.
+ * There are several changes I have done to refactor the code across
+ * dag/astar/decode_anyptop.  A new library called dag.c is now created
+ * to include all routines that are shared by the three applications that
+ * required graph operations.
+ * 1, dag_link is now shared between dag and decode_anytopo. Unfortunately, astar was using a slightly different version of dag_link.  At this point, I could only rename astar'dag_link to be astar_dag_link.
+ * 2, dag_update_link is shared by both dag and decode_anytopo.
+ * 3, hyp_free is now shared by misc.c, dag and decode_anytopo
+ * 4, filler_word will not exist anymore, dict_filler_word was used instead.
+ * 5, dag_param_read were shared by both dag and astar.
+ * 6, dag_destroy are now shared by dag/astar/decode_anytopo.  Though for some reasons, even the function was not called properly, it is still compiled in linux.  There must be something wrong at this point.
+ * 7, dag_bestpath and dag_backtrack are now shared by dag and decode_anytopo. One important thing to notice here is that decode_anytopo's version of the two functions actually multiply the LM score or filler penalty by the language weight.  At this point, s3_dag is always using lwf=1.
+ * 8, dag_chk_linkscr is shared by dag and decode_anytopo.
+ * 9, decode_anytopo nows supports another three options -maxedge, -maxlmop and -maxlpf.  Their usage is similar to what one could find dag.
+ *
+ * Notice that the code of the best path search in dag and that of 2-nd
+ * stage of decode_anytopo could still have some differences.  It could
+ * be the subtle difference of handling of the option -fudge.  I am yet
+ * to know what the true cause is.
+ *
+ * Some other small changes include
+ * -removal of startwid and finishwid asstatic variables in s3_dag.c.  dict.c now hide these two variables.
+ *
+ * There are functions I want to merge but I couldn't and it will be
+ * important to say the reasons.
+ * i, dag_remove_filler_nodes.  The version in dag and decode_anytopo
+ * work slightly differently. The decode_anytopo's one attached a dummy
+ * predecessor after removal of the filler nodes.
+ * ii, dag_search.(s3dag_dag_search and s3flat_fwd_dag_search)  The handling of fudge is differetn. Also, decode_anytopo's one  now depend on variable lattice.
+ * iii, dag_load, (s3dag_dag_load and s3astar_dag_load) astar and dag seems to work in a slightly different, one required removal of arcs, one required bypass the arcs.  Don't understand them yet.
+ * iv, dag_dump, it depends on the variable lattice.
+ *
+ * Revision 1.1.1.1  2005/03/24 15:24:00  archan
+ * I found Evandro's suggestion is quite right after yelling at him 2 days later. So I decide to check this in again without any binaries. (I have done make distcheck. ) . Again, this is a candidate for s3.6 and I believe I need to work out 4-5 intermediate steps before I can complete the first prototype.  That's why I keep local copies. 
+ *
+ * Revision 1.3  2004/12/06 10:52:01  arthchan2003
+ * Enable doxygen documentation in libs3decoder
+ *
  * Revision 1.2  2004/08/31 08:43:47  arthchan2003
  * Fixing _cpluscplus directive
  *
@@ -104,21 +143,6 @@ typedef struct srch_hyp_s {
     int32     pscr;
     struct srch_hyp_s *next;
 } srch_hyp_t;
-
-/* ---------------- DAG related functions ---------------- */
-
-
-  /** DAG built from forward pass lattice */
-int32 dag_build ( void );
-
-  /** Dump DAG to file for postprocessing */
-int32 dag_dump (char *dir, int32 onlynodes, char *uttid);
-
-  /** Best path search through DAG from fwdvit word lattice */
-srch_hyp_t *dag_search (char *uttid);
-
-  /** Destroy DAG */
-int32 dag_destroy ( void );
 
 #ifdef __cplusplus
 }
