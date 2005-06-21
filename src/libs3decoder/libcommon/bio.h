b1d1d389d67a28c0af5b9aa1d33927cc28ffb3c0
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-21 20:44:34 +0000

    1, Fixed doxygen documentation, 2, Add the $ keyword.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@4511 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/libcommon/bio.h
+++ src/libs3decoder/libcommon/bio.h
@@ -44,6 +44,19 @@
  * **********************************************
  * 
  * HISTORY
+ * $Log$
+ * Revision 1.8  2005/06/21  20:40:46  arthchan2003
+ * 1, Fixed doxygen documentation, 2, Add the $ keyword.
+ * 
+ * Revision 1.5  2005/06/13 04:02:57  archan
+ * Fixed most doxygen-style documentation under libs3decoder.
+ *
+ * Revision 1.4  2005/05/10 21:21:52  archan
+ * Three functionalities added but not tested. Code on 1) addition/deletion of LM in mode 4. 2) reading text-based LM 3) Converting txt-based LM to dmp-based LM.
+ *
+ * Revision 1.3  2005/03/30 01:22:46  archan
+ * Fixed mistakes in last updates. Add
+ *
  * 
  * 28-Apr-1999	M K Ravishankar (rkm@cs.cmu.edu) at Carnegie Mellon University
  * 		Created.
@@ -80,6 +93,24 @@ extern "C" {
 #define SWAP_FLOAT32(x)	SWAP_INT32((int32 *) x)
 
 
+
+  /** "reversed senses" SWAP, ARCHAN: This is still incorporated in
+   Sphinx 3 because lm3g2dmp used it.  Don't think that I am very
+   happy with it. */
+
+#if (__BIG_ENDIAN__)
+#define REVERSE_SENSE_SWAP_INT16(x)  x = ( (((x)<<8)&0x0000ff00) | (((x)>>8)&0x00ff) )
+#define REVERSE_SENSE_SWAP_INT32(x)  x = ( (((x)<<24)&0xff000000) | (((x)<<8)&0x00ff0000) | \
+                         (((x)>>8)&0x0000ff00) | (((x)>>24)&0x000000ff) )
+#else
+#define REVERSE_SENSE_SWAP_INT16(x)
+#define REVERSE_SENSE_SWAP_INT32(x)
+
+#endif
+
+
+
+
   /**
  * Read binary file format header: has the following format
  *     s3
@@ -90,26 +121,29 @@ extern "C" {
  *     4-byte byte-order word used to find file byte ordering relative to host machine.
  * Lines beginning with # are ignored.
  * Memory for name and val allocated by this function; use bio_hdrarg_free to free them.
- * Return value: 0 if successful, -1 otherwise.
+ * @return: 0 if successful, -1 otherwise.
  */
-int32 bio_readhdr (FILE *fp,		/** In: File to read */
-		   char ***name,	/** Out: array of argument name strings read */
-		   char ***val,		/** Out: corresponding value strings read */
-		   int32 *swap);	/** Out: file needs byteswapping iff (*swap) */
-
+int32 bio_readhdr (FILE *fp,		/**< In: File to read */
+		   char ***name,	/**< Out: array of argument name strings read */
+		   char ***val,		/**< Out: corresponding value strings read */
+		   int32 *swap	/**< Out: file needs byteswapping iff (*swap) */
+		   );
   /**
  * Write a simple binary file header, containing only the version string.  Also write
  * the byte order magic word.
- * Return value: 0 if successful, -1 otherwise.
+ * @return: 0 if successful, -1 otherwise.
  */
-int32 bio_writehdr_version (FILE *fp, char *version);
+  int32 bio_writehdr_version (FILE *fp,  /**< Output: File to write */
+			      char *version /**< Input: A string of version */
+			      );
 
 
   /**
  * Free name and value strings previously allocated and returned by bio_readhdr.
  */
-void bio_hdrarg_free (char **name,	/** In: Array previously returned by bio_readhdr */
-		      char **val);	/** In: Array previously returned by bio_readhdr */
+void bio_hdrarg_free (char **name,	/**< In: Array previously returned by bio_readhdr */
+		      char **val	/**< In: Array previously returned by bio_readhdr */
+		      );
 
   /**
  * Like fread but perform byteswapping and accumulate checksum (the 2 extra arguments).
@@ -119,9 +153,10 @@ void bio_hdrarg_free (char **name,	/** In: Array previously returned by bio_read
 int32 bio_fread (void *buf,
 		 int32 el_sz,
 		 int32 n_el,
-		 FILE *fp,
-		 int32 swap,		/** In: Byteswap iff (swap != 0) */
-		 uint32 *chksum);	/** In/Out: Accumulated checksum */
+		 FILE *fp,              /**< In: An input file pointer */
+		 int32 swap,		/**< In: Byteswap iff (swap != 0) */
+		 uint32 *chksum	/**< In/Out: Accumulated checksum */
+		 );
 
   /**
  * Read a 1-d array (fashioned after fread):
@@ -131,21 +166,23 @@ int32 bio_fread (void *buf,
  * Fails fatally if expected data not read.
  * Return value: #array elements allocated and read; -1 if error.
  */
-int32 bio_fread_1d (void **buf,		/** Out: contains array data; allocated by this
+int32 bio_fread_1d (void **buf,		/**< Out: contains array data; allocated by this
 					   function; can be freed using ckd_free */
-		    int32 el_sz,	/** In: Array element size */
-		    int32 *n_el,	/** Out: #array elements allocated/read */
-		    FILE *fp,		/** In: File to read */
-		    int32 sw,		/** In: Byteswap iff (swap != 0) */
-		    uint32 *ck);	/** In/Out: Accumulated checksum */
+		    int32 el_sz,	/**< In: Array element size */
+		    int32 *n_el,	/**< Out: #array elements allocated/read */
+		    FILE *fp,		/**< In: File to read */
+		    int32 sw,		/**< In: Byteswap iff (swap != 0) */
+		    uint32 *ck	/**< In/Out: Accumulated checksum */
+		    );
 
   /**
  * Read and verify checksum at the end of binary file.  Fails fatally if there is
  * a mismatch.
  */
-void bio_verify_chksum (FILE *fp,	/** In: File to read */
-			int32 byteswap,	/** In: Byteswap iff (swap != 0) */
-			uint32 chksum);	/** In: Value to compare with checksum in file */
+void bio_verify_chksum (FILE *fp,	/**< In: File to read */
+			int32 byteswap,	/**< In: Byteswap iff (swap != 0) */
+			uint32 chksum	/**< In: Value to compare with checksum in file */
+			);
 
 #ifdef __cplusplus
 }
