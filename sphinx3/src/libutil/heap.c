920caa6c1f7999cd34eee20151ab082cb11ec81c
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-06-22 03:05:49 +0000

    1, Fixed doxygen documentation, 2, Add  keyword.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@4548 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libutil/heap.c
+++ sphinx3/src/libutil/heap.c
@@ -45,6 +45,13 @@
  * **********************************************
  * 
  * HISTORY
+ * $Log$
+ * Revision 1.4  2005/06/22  03:05:49  arthchan2003
+ * 1, Fixed doxygen documentation, 2, Add  keyword.
+ * 
+ * Revision 1.3  2005/03/30 01:22:48  archan
+ * Fixed mistakes in last updates. Add
+ *
  * 
  * 05-Mar-99	M K Ravishankar (rkm@cs.cmu.edu) at Carnegie Mellon University
  * 		Fixed bug in heap_destroy() (in while loop exit condition).
