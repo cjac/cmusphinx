a94823f29614ba4f99f1c232eb0ae13c378ee228
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2005-11-28 19:01:34 +0000

    Temporary test program for confidence annotation.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/branches/SPHINX3_5_2_RCI_IRII_BRANCH@5120 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ sphinx3/src/tests/regression/test-confidence.sh
@@ -0,0 +1,15 @@
+./src/programs/conf \
+-mdef /net/elroy/usr1/archan/experiment.training/Communicator/cont.all/model_architecture/CFS3.2000.mdef \
+-dict /net/wilma/usr1/archan/Communicator/lists/communicator.dict.reduced \
+-fdict /net/wilma/usr1/archan/Communicator/lists/noise.dict.reduced.cmu.1 \
+-lm /net/wilma/usr1/archan/Communicator/lists/training.trans.proc.arpa.DMP \
+-ctl /net/elroy/usr1/archan/experiment/Communicator/res/nist.ctl.no_comment.no_id \
+-inlatdir /net/elroy/usr1/archan/experiment/Communicator/dag/Nist/ \
+-inhypseg /net/elroy/usr1/archan/experiment/Communicator/log/Comm.all.64mix.DE.BL.20051122/Comm.all.64mix.DE.BL.20051122.matchseg \
+-fillprob 0.1 \
+-lw 7.5 \
+-silprob 0.5 \
+-output conf.out
+
+
+
