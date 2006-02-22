7d76dff0ee71733e4fb1b4cb36f2d60d68c10232
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-02-22 17:27:39 +0000

    Merged from SPHINX3_5_2_RCI_IRII_BRANCH: 1, NOT doing truncation in the multi-stream GMM computation \n. 2, Added .s3cont. to be the alias of the old multi-stream GMM computation routine \n. 3, Added license \n.  4, Fixed dox-doc. \n
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@5264 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libs3decoder/libam/ms_senone.h
+++ src/libs3decoder/libam/ms_senone.h
@@ -1,3 +1,38 @@
+/* ====================================================================
+ * Copyright (c) 1999-2004 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
 /*
  * senone.h -- Mixture density weights associated with each tied state.
  *
@@ -11,9 +46,26 @@
  * HISTORY
  * 
  * $Log$
- * Revision 1.6  2005/06/21  19:00:19  arthchan2003
- * Add more detail comments  to ms_senone.h
+ * Revision 1.7  2006/02/22  17:27:39  arthchan2003
+ * Merged from SPHINX3_5_2_RCI_IRII_BRANCH: 1, NOT doing truncation in the multi-stream GMM computation \n. 2, Added .s3cont. to be the alias of the old multi-stream GMM computation routine \n. 3, Added license \n.  4, Fixed dox-doc. \n
  * 
+ * Revision 1.6.4.4  2006/01/16 19:47:05  arthchan2003
+ * Removed the truncation of senone probability code.
+ *
+ * Revision 1.6.4.3  2005/08/03 18:53:43  dhdfu
+ * Add memory deallocation functions.  Also move all the initialization
+ * of ms_mgau_model_t into ms_mgau_init (duh!), which entails removing it
+ * from decode_anytopo and friends.
+ *
+ * Revision 1.6.4.2  2005/07/20 19:39:01  arthchan2003
+ * Added licences in ms_* series of code.
+ *
+ * Revision 1.6.4.1  2005/07/05 05:47:59  arthchan2003
+ * Fixed dox-doc. struct level of documentation are included.
+ *
+ * Revision 1.6  2005/06/21 19:00:19  arthchan2003
+ * Add more detail comments  to ms_senone.h
+ *
  * Revision 1.5  2005/06/21 18:57:31  arthchan2003
  * 1, Fixed doxygen documentation. 2, Added $ keyword.
  *
@@ -69,28 +121,31 @@
 extern "C" {
 #endif
 
-typedef uint8 senprob_t;	/** Senone logs3-probs, truncated to 8 bits */
+typedef uint32 senprob_t;	/**< Senone logs3-probs, truncated to 8 bits */
 
   /**
- * 8-bit senone PDF structure.  Senone pdf values are normalized, floored, converted to
- * logs3 domain, and finally truncated to 8 bits precision to conserve memory space.
- */
+   * \struct senone_t
+   * \brief 8-bit senone PDF structure. 
+   * 
+   * 8-bit senone PDF structure.  Senone pdf values are normalized, floored, converted to
+   * logs3 domain, and finally truncated to 8 bits precision to conserve memory space.
+   */
 typedef struct {
-    senprob_t ***pdf;		/** gaussian density mixture weights, organized two possible
+    senprob_t ***pdf;		/**< gaussian density mixture weights, organized two possible
 				   ways depending on n_gauden:
 				   if (n_gauden > 1): pdf[sen][feat][codeword].  Not an
 				       efficient representation--memory access-wise--but
 				       evaluating the many codebooks will be more costly.
 				   if (n_gauden == 1): pdf[feat][codeword][sen].  Optimized
 				       for the shared-distribution semi-continuous case. */
-    int32 n_sen;		/** #senones in this set */
-    int32 n_feat;		/** #feature streams */ 
-    int32 n_cw;			/** #codewords per codebook,stream */
-    int32 n_gauden;		/** #gaussian density codebooks referred to by senones */
-    float32 mixwfloor;		/** floor applied to each PDF entry */
-    int32 shift;		/** LSB bits truncated from original logs3 value */
-    s3mgauid_t *mgau;		/** senone-id -> mgau-id mapping for senones in this set */
-    int32* featscr;              /** The feature score for every senone, will be initialized inside senone_eval_all */
+    int32 n_sen;		/**< #senones in this set */
+    int32 n_feat;		/**< #feature streams */ 
+    int32 n_cw;			/**< #codewords per codebook,stream */
+    int32 n_gauden;		/**< #gaussian density codebooks referred to by senones */
+    float32 mixwfloor;		/**< floor applied to each PDF entry */
+    int32 shift;		/**< LSB bits truncated from original logs3 value */
+    s3mgauid_t *mgau;		/**< senone-id -> mgau-id mapping for senones in this set */
+    int32* featscr;              /**< The feature score for every senone, will be initialized inside senone_eval_all */
 } senone_t;
 
 
@@ -106,6 +161,9 @@ senone_t *senone_init (char *mixwfile,		/**< In: mixing weights file */
 						   If NULL all senones map to codebook 0 */
 		       float32 mixwfloor	/**< In: Floor value for senone weights */
 		       );
+
+/** Release memory allocated by senone_init. */
+void senone_free(senone_t *s); /**< In: The senone_t to free */
 
   /**
  * Evaluate the score for the given senone wrt to the given top N gaussian codewords.
