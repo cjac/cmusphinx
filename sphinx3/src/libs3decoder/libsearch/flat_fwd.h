a19749f5b168a19c038b038fc9da7fdd91374a03
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-02-23 05:25:32 +0000

    Merged from branch SPHINX3_5_2_RCI_IRII_BRANCH: Most of the functions are removed and now resided in srch_flat_fwd.h
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@5316 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/libs3decoder/libsearch/flat_fwd.h
+++ sphinx3/src/libs3decoder/libsearch/flat_fwd.h
@@ -32,6 +32,7 @@
  *
  * ====================================================================
  *
+
  */
 /*
  * fwd.c -- Forward Viterbi beam search
@@ -44,6 +45,46 @@
  * **********************************************
  * 28-Jul-04    ARCHAN (archan@cs.cmu.edu) at Carnegie Mellon Unversity 
  *              First created it. 
+ *
+ * $Log$
+ * Revision 1.12  2006/02/23  05:25:32  arthchan2003
+ * Merged from branch SPHINX3_5_2_RCI_IRII_BRANCH: Most of the functions are removed and now resided in srch_flat_fwd.h
+ * 
+ * Revision 1.10.4.7  2005/11/17 06:27:48  arthchan2003
+ * 1, Clean up. 2, removed fwg in dag_build.
+ *
+ * Revision 1.10.4.6  2005/10/26 03:53:12  arthchan2003
+ * Put add_fudge and remove_filler_nodes into srch_flat_fwd.c . This conformed to s3.0 behavior.
+ *
+ * Revision 1.10.4.5  2005/09/18 01:18:24  arthchan2003
+ * Only retain processing of the array whmm_t in flat_fwd.[ch]
+ *
+ * Revision 1.10.4.4  2005/09/11 02:58:10  arthchan2003
+ * remove most dag-related functions except dag_build. Use latticehist_t insteads of loosed arrays.
+ *
+ * Revision 1.10.4.3  2005/09/07 23:40:06  arthchan2003
+ * Several Bug Fixes and Enhancements to the flat-lexicon
+ * 1, Fixed Dox-doc.
+ * 2, Add -worddumpef and -hmmdumpef in parrallel to -worddumpsf and
+ * -hmmdumpsf. Usage is trivial. a structure called fwd_dbg_t now wrapped
+ * up all these loose parameters.  Methods of fwd_dbg are implemented.
+ * 3, word_ugprob is now initialized by init_word_ugprob
+ * 4, Full-triphone expansion is implemented. User can change this
+ * behavior by specifying -multiplex_multi and -multiplex_single. The
+ * former turn on multiplex triphone for word-begin for multi-phone word.
+ * The latter do that for single-phone word. Turning off both could
+ * tremendously increase computation.
+ * 5, Word expansions of possible right contexts now records independent
+ * history.  The behavior in the past was to use only one history for a
+ * word.
+ *
+ * Revision 1.10.4.2  2005/08/02 21:12:45  arthchan2003
+ * Changed senlist from 8-bit to 32-bit. It will be compatible to the setting of ascr's sen_active.
+ *
+ * Revision 1.10.4.1  2005/07/15 07:50:33  arthchan2003
+ * Remove hmm computation and context building code from flat_fwd.c.
+ *
+ *
  */
 
 #ifndef _LIBFBS_FWD_H_
@@ -52,7 +93,8 @@
 /* Added by BHIKSHA; Fix for 3 state hmms? 
 #define ANYHMMTOPO	1
  End modification by BHIKSHA */
-
+#include "dag.h"
+#include "vithist.h"
 
 /** \file flat_fwd.h
    \brief (Currently not opened to public) Header for forward search for flat lexicon
@@ -63,96 +105,6 @@
  *   - Final state is a non-emitting state with no arcs out of it.
  *   - Some form of Bakis topology (ie, no cycles, except for self-transitions).
  */
-
-
-
-/**
- * Triphone information for all word hmm modelling broken up into 4 cases:
- * 	within-word triphones
- * 	left-context cross-word triphones (multi-phone words)
- * 	right-context cross-word triphones (multi-phone words)
- * 	left- and right-cross-word triphones (single-phone words)
- * These 4 cases captured by the following data structures.
- */
-
-/**
- * First, the within word triphone models.  wwpid[w] = list of triphone pronunciations
- * for word w.
- * Since left and right extremes require cross-word modelling (see below), wwpid[w][0]
- * and wwpid[w][pronlen-1] contain no information and shouldn't be touched.
- */
-
-typedef struct {
-    s3pid_t   *pid;	/**< Pid list for all context ciphones; compressed, unique */
-    s3cipid_t *cimap;	/**< Index into pid[] above for each ci phone */
-    int32    n_pid;	/**< #Unique pid in above, compressed pid list */
-} xwdpid_t;
-
-/**
- * Word HMM instance: the basic structure searched during recognition.
- * 
- * whmm[w] = head of list of all active HMM for word w:
- * 	Will only contain active HMM in current frame.
- * 	List ordered by pronunciation position within word.
- * 	If last phone is active, right context instances ordered as in rcpid or lrcpid
- * 
- * The triphone modelled by a given whmm_t is normally obtained by looking up wwpid or
- * rcpid above, using whmm_t.pos and whmm_t.rc.  However, left context modelling, unlike
- * right context, is done not by explicit fanout but by multiplexing a single whmm_t
- * structure among all possible instantiations (for all possible left context ciphones).
- * Each state can be from a different triphone instantiation.  whmm_t.pid[] used for
- * these triphone ids.
- * (This is probably worse than no explanation.)
- */
-typedef struct whmm_s {
-    struct whmm_s *next;	/**< Next active whmm_t for this word */
-    int32     *score;		/**< Per state path score */
-    s3latid_t *history;		/**< Per state predecessor lattice entry index */
-    s3pid_t   *pid;		/**< Triphone id: 1 per state if 1st phone in word,
-				   otherwise single pid for entire phone */
-    int32      bestscore;	/**< Best among this whmm.score[] in current frame */
-    int16       pos;		/**< Word pronunciation position index */
-    s3cipid_t  rc;		/**< Right context position (only for last phone in word);
-				   index into rcpid[][].pid or lrcpid[][].pid */
-    int32      active;		/**< Whether active in current frame */
-} whmm_t;
-
-
-/**
- * Initialization of flat forward search 
- */
-void fwd_init (mdef_t* _mdef,  /**< A model definition */
-	       tmat_t* _tmat,  /**< A transition matrix */
-	       dict_t* _dict,  /**< A dictionary */
-	       lm_t *_lm       /**< An LM */
-	       );
-
-/**
- * Start of flat foward search 
- */ 
-void fwd_start_utt (char *id /**< ID of an utterance */
-		    );
-
-/** 
- * Make the search to go forward for one frame. 
- * @return best score of this frame. 
- */
-int32 fwd_frame (int32 *senscr /**< An array of senone score */
-		 ); 
-
-/**
- * Find the active senone list. 
- */
-void fwd_sen_active (uint8 *senlist, int32 n_sen);
-
-/**
- * End of flat foward search 
- * @return searching hypothesis. 
- */ 
-
-srch_hyp_t *fwd_end_utt ( void );
-
-void fwd_timing_dump (float64 tot);
 
 /**
  * The flat forward search version of DAG search. Very similar to what
@@ -175,15 +127,14 @@ srch_hyp_t *s3flat_fwd_dag_search (char *utt /**< utterance id */
  * absurdum.
  */
 
-int32 dag_build ( void );
+dag_t* dag_build (s3latid_t endid, latticehist_t * lathist, dict_t *dict, lm_t *lm, ctxt_table_t* ctxt, fillpen_t* fpen, int32 n_frm);
 
-/** Dump dag in s3.0 format
- * A function that can dump a dag given a lattice_t structure. 
- */
-int32 dag_dump (char *dir,  /**< The output directory */
-		int32 onlynodes, /**< Dump only nodes of the DAG*/
-		char *id    /**< Sentence ID*/
-		);
 
-#endif
+
+void flat_fwd_dag_remove_filler_nodes (dag_t* dag, 
+				       latticehist_t *lathist, 
+				       float64 lwf, 
+				       lm_t *lm, 
+				       dict_t* dict, ctxt_table_t *ct_table, fillpen_t *fpen);
+#endif /* _LIBFBS_FWD_H_*/
 
