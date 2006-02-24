5d5ee8fce78c76880761864172b50582f974dbe5
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-02-24 04:40:53 +0000

    Merged from the branch SPHINX3_5_2_RCI_IRII_BRANCH: Used ctl_process from now on.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@5358 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/programs/main_livepretend.c
+++ src/programs/main_livepretend.c
@@ -45,9 +45,21 @@
  *
  * HISTORY
  * $Log$
- * Revision 1.14  2005/06/22  05:39:56  arthchan2003
- * Synchronize argument with decode. Removed silwid, startwid and finishwid.  Wrapped up logs3_init, Wrapped up lmset. Refactor with functions in dag.
+ * Revision 1.15  2006/02/24  04:40:53  arthchan2003
+ * Merged from the branch SPHINX3_5_2_RCI_IRII_BRANCH: Used ctl_process from now on.
  * 
+ * Revision 1.14.4.3  2006/01/16 20:29:52  arthchan2003
+ * Changed -ltsoov to -lts_mismatch. Changed lm_rawscore interface. Change from cmd_ln_access to cmd_ln_str.
+ *
+ * Revision 1.14.4.2  2005/07/27 23:23:39  arthchan2003
+ * Removed process_ctl in allphone, dag, decode_anytopo and astar. They were duplicated with ctl_process and make Dave and my lives very miserable.  Now all application will provided their own utt_decode style function and will pass ctl_process.  In that way, the mechanism of reading would not be repeated. livepretend also follow the same mechanism now.  align is still not yet finished because it read yet another thing which has not been considered : transcription.
+ *
+ * Revision 1.14.4.1  2005/07/18 23:21:24  arthchan2003
+ * Tied command-line arguments with marcos
+ *
+ * Revision 1.14  2005/06/22 05:39:56  arthchan2003
+ * Synchronize argument with decode. Removed silwid, startwid and finishwid.  Wrapped up logs3_init, Wrapped up lmset. Refactor with functions in dag.
+ *
  * Revision 1.5  2005/06/15 21:41:56  archan
  * Sphinx3 to s3.generic: 1) added -ctloffset and -ctlcount support for livepretend. 2) Also allow lmname to be set correctly.  Now it is using ld_set_lm instead of kb_setlm.  That makes the code cleaner.
  *
@@ -55,16 +67,6 @@
  * Add comments on all mains for preparation of factoring the command-line.
  *
  * Revision 1.3  2005/03/30 00:43:41  archan
- * Add $Log$
- * Revision 1.14  2005/06/22  05:39:56  arthchan2003
- * Synchronize argument with decode. Removed silwid, startwid and finishwid.  Wrapped up logs3_init, Wrapped up lmset. Refactor with functions in dag.
- * 
- * Add Revision 1.5  2005/06/15 21:41:56  archan
- * Add Sphinx3 to s3.generic: 1) added -ctloffset and -ctlcount support for livepretend. 2) Also allow lmname to be set correctly.  Now it is using ld_set_lm instead of kb_setlm.  That makes the code cleaner.
- * Add
- * Add Revision 1.4  2005/04/20 03:50:36  archan
- * Add Add comments on all mains for preparation of factoring the command-line.
- * Add into most of the .[ch] files. It is easy to keep track changes.
  *
  */
 
@@ -78,29 +80,66 @@
 
 #include "kb.h"
 #include "s3types.h"
+#include "corpus.h"
 
 #define SAMPLE_BUFFER_LENGTH	4096
 #define FILENAME_LENGTH		512
 
+static live_decoder_t decoder;
+static char *rawdirfn;
+static stat_t* st;
+
+/**
+   
+ */
+static void utt_livepretend(void *data, utt_res_t *ur, int32 sf, int32 ef, char *uttid)
+{
+  char fullrawfn[FILENAME_LENGTH];
+  char *hypstr;
+
+  short samples[SAMPLE_BUFFER_LENGTH];
+  kb_t *kb;
+  FILE *rawfd;
+  int len;
+
+  kb = (kb_t *) data;
+
+  /*  report_utt_res(ur);*/
+  sprintf(fullrawfn, "%s/%s%s", rawdirfn, ur->uttfile,decoder.rawext);
+  if ((rawfd = fopen(fullrawfn, "rb")) == NULL) {
+    E_FATAL("Cannnot open raw file %s.\n", fullrawfn);
+  }
+  
+  if(ur->lmname!=NULL) srch_set_lm((srch_t*)kb->srch,ur->lmname);
+  if(ur->regmatname!=NULL) kb_setmllr(ur->regmatname,ur->cb2mllrname,kb);
+
+  if (ld_begin_utt(&decoder, ur->uttfile) != LD_SUCCESS) {
+    E_FATAL("Cannot begin utterance decoding.\n");
+  }
+  len = fread(samples, sizeof(short), SAMPLE_BUFFER_LENGTH, rawfd);
+  
+  while (len > 0) {
+    ptmr_start (&(st->tm));
+    ld_process_raw(&decoder, samples, len);
+    ptmr_stop (&(st->tm));
+    
+    if (ld_retrieve_hyps(&decoder, NULL, &hypstr, NULL) == LD_SUCCESS) {
+      if(decoder.phypdump){
+	E_INFO("PARTIAL_HYP: %s\n", hypstr);
+      }
+    }
+    len = fread(samples, sizeof(short), SAMPLE_BUFFER_LENGTH, rawfd);
+  }
+  fclose(rawfd);
+  ld_end_utt(&decoder);
+
+}
+
 int
 main(int _argc, char **_argv)
 {
-
-  live_decoder_t decoder;
-  short samples[SAMPLE_BUFFER_LENGTH];
-  int len;
-  char *hypstr;
   char *ctrlfn;
   char *cfgfn;
-  char *rawdirfn;
-  char rawfn[FILENAME_LENGTH];
-  char lmname[FILENAME_LENGTH];
-  char fullrawfn[FILENAME_LENGTH];
-  FILE *ctrlfd;
-  FILE *ctllmfd;
-  FILE *rawfd;
-  stat_t* st;
-  int32 nskip, count;
 
   print_appl_info(_argv[0]);
 
@@ -113,12 +152,6 @@ main(int _argc, char **_argv)
   rawdirfn = _argv[2];
   cfgfn = _argv[3];
 
-
-
-  if ((ctrlfd = fopen(ctrlfn, "r")) == NULL) {
-    E_FATAL("Cannot open control file %s.\n", ctrlfn);
-  }
-
   if (cmd_ln_parse_file(arg_def, cfgfn)) {
     E_FATAL("Bad configuration file %s.\n", cfgfn);
   }
@@ -130,73 +163,18 @@ main(int _argc, char **_argv)
   st = decoder.kb.stat;
   ptmr_init(&(st->tm));
 
-  nskip = cmd_ln_int32("-ctloffset");
-  count = cmd_ln_int32("-ctlcount");
-  if (cmd_ln_str("-ctl_lm")) {
-    if ((ctllmfd = fopen(cmd_ln_str("-ctl_lm"), "r")) == NULL)
-      E_FATAL("Cannot open LM control file %s.\n", cmd_ln_str("-ctl_lm"));
-  }
-  else
-    ctllmfd = NULL;
 
-  if (nskip > 0) {
-    E_INFO("Skipping %d entries at the beginning of %s\n", nskip, ctrlfn);
-    
-    for (; nskip > 0; --nskip) {
-      if (fscanf(ctrlfd, "%s", rawfn) == EOF)
-	E_FATAL("EOF while skipping initial lines\n");
-      if (ctllmfd && fscanf(ctllmfd, "%s", lmname) == EOF)
-	E_FATAL("EOF while skipping initial lines (in ctl_lm)\n");
-    }
-  }
-
-  while (fscanf(ctrlfd, "%s", rawfn) != EOF) {
-    if (count-- == 0)
-      break;
-
-    if (ctllmfd) {
-      if (fscanf(ctllmfd, "%s", lmname) == EOF)
-	E_FATAL("EOF while reading LM control file\n");
-
-      /* This is somewhat inelegant; the live-decode API should make
-         an allowance for dynamic LM switching - dhuggins@cs,
-         2005-04-19 */
-      /*kb_setlm(lmname, &decoder.kb);*/
-
-      /* 20050615 Dave's last comment is valid. Now a lm switching
-	 interface is available implemented and it is a two-layered
-	 wrapper of lm switching function in the search implementation
-	 modules.
-      */
-      ld_set_lm(&decoder,lmname);
-    }
-
-    sprintf(fullrawfn, "%s/%s%s", rawdirfn, rawfn,decoder.rawext);
-    if ((rawfd = fopen(fullrawfn, "rb")) == NULL) {
-      E_FATAL("Cannnot open raw file %s.\n", fullrawfn);
-    }
-
-    if (ld_begin_utt(&decoder, rawfn) != LD_SUCCESS) {
-      E_FATAL("Cannot begin utterance decoding.\n");
-    }
-    len = fread(samples, sizeof(short), SAMPLE_BUFFER_LENGTH, rawfd);
-
-    while (len > 0) {
-      ptmr_start (&(st->tm));
-      ld_process_raw(&decoder, samples, len);
-      ptmr_stop (&(st->tm));
-
-      if (ld_retrieve_hyps(&decoder, NULL, &hypstr, NULL) == LD_SUCCESS) {
-	if(decoder.phypdump){
-	  E_INFO("PARTIAL_HYP: %s\n", hypstr);
-	}
-      }
-      len = fread(samples, sizeof(short), SAMPLE_BUFFER_LENGTH, rawfd);
-    }
-    fclose(rawfd);
-    ld_end_utt(&decoder);
-    ptmr_reset(&(st->tm));
-
+  if (ctrlfn) {
+    /* When -ctlfile is speicified, corpus.c will look at -ctl_lm and -ctl_mllr to get
+       the corresponding LM and MLLR for the utterance */
+    st->tm = ctl_process (ctrlfn,
+			  cmd_ln_str("-ctl_lm"),
+			  cmd_ln_str("-ctl_mllr"),
+			  cmd_ln_int32("-ctloffset"),
+			  cmd_ln_int32("-ctlcount"),
+			  utt_livepretend, &(decoder.kb));
+  } else {
+    E_FATAL("control file is not specified.\n");
   }
 
   ld_finish(&decoder);
