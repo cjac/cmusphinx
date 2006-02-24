7aa7a68e2a691a7f902e9fb7781501dc3bd1520d
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-02-24 03:11:39 +0000

    Merged from branch SPHINX3_5_2_RCI_IRII_BRANCH: 1, changed ckd_calloc to my_malloc, 2, add functions to free nodes.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@5337 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/libutil/glist.h
+++ src/libutil/glist.h
@@ -45,9 +45,15 @@
  * 
  * HISTORY
  * $Log$
- * Revision 1.9  2005/06/22  03:02:51  arthchan2003
- * 1, Fixed doxygen documentation, 2, add  keyword.
+ * Revision 1.10  2006/02/24  03:11:39  arthchan2003
+ * Merged from branch SPHINX3_5_2_RCI_IRII_BRANCH: 1, changed ckd_calloc to my_malloc, 2, add functions to free nodes.
  * 
+ * Revision 1.9.4.1  2005/06/27 05:41:46  arthchan2003
+ * Added glist_delete in glist.h.  glist_delete only delete one node after a specified node. glist_delete and glist_add thus from a simple operation that allow the glist to act like a stack.
+ *
+ * Revision 1.9  2005/06/22 03:02:51  arthchan2003
+ * 1, Fixed doxygen documentation, 2, add  keyword.
+ *
  * Revision 1.4  2005/05/03 04:09:11  archan
  * Implemented the heart of word copy search. For every ci-phone, every word end, a tree will be allocated to preserve its pathscore.  This is different from 3.5 or below, only the best score for a particular ci-phone, regardless of the word-ends will be preserved at every frame.  The graph propagation will not collect unused word tree at this point. srch_WST_propagate_wd_lv2 is also as the most stupid in the century.  But well, after all, everything needs a start.  I will then really get the results from the search and see how it looks.
  *
@@ -67,10 +73,12 @@
  * \file glist.h
  * \brief Generic linked-lists maintenance.
  *
- * Only insert at the head of the list.
- * No selective deletions, except to destroy the entire list.
- * A convenient little linked-list package, but a double-edged sword: the user must
- * keep track of the data type within the linked list elements.
+ * Only insert at the head of the list.  A convenient little
+ * linked-list package, but a double-edged sword: the user must keep
+ * track of the data type within the linked list elements.  When it
+ * was first written, there was no selective deletions except to
+ * destroy the entire list.  This is modified in later version. 
+ * 
  * 
  * (C++ would be good for this, but that's a double-edged sword as well.)
  */
@@ -129,18 +137,38 @@ typedef gnode_t *glist_t;	/** Head of a list of gnodes */
 			     );
 
 
+
   /**
    * Create and insert a new list node, with the given user-defined data, after
    * the given generic node gn.  gn cannot be NULL.
    * Return ptr to the newly created gnode_t.
    */
 
-gnode_t *glist_insert_ptr (gnode_t *gn, void *ptr);
-gnode_t *glist_insert_int32 (gnode_t *gn, int32 val);
-gnode_t *glist_insert_uint32 (gnode_t *gn, uint32 val);
-gnode_t *glist_insert_float32 (gnode_t *gn, float32 val);
-gnode_t *glist_insert_float64 (gnode_t *gn, float64 val);
+  gnode_t *glist_insert_ptr (gnode_t *gn, /**< a generic node which ptr will be inserted after it*/
+			     void *ptr /**< pointer inserted */
+			   );
+gnode_t *glist_insert_int32 (gnode_t *gn, /**< a generic node which a value will be inserted after it*/
+			     int32 val /**< int32 inserted */
+			     );
+gnode_t *glist_insert_uint32 (gnode_t *gn, /**< a generic node which a value will be inserted after it*/
+			      uint32 val /**< uint32 inserted */
+			      );
+gnode_t *glist_insert_float32 (gnode_t *gn, /**< a generic node which a value will be inserted after it*/
+			       float32 val /**< float32 inserted */
+			       );
+gnode_t *glist_insert_float64 (gnode_t *gn, /**< a generic node which a value will be inserted after it*/
+			       float64 val /**< float64 inserted */
+			       );
 
+  /**
+   * Delete a list node, with the given user-defined data, after
+   * the given generic node gn.  gn cannot be NULL.
+   * Return ptr to the newly created gnode_t.
+   * It is more a mirror image of glist_add_* family of functions.
+   */
+
+  gnode_t *glist_delete (gnode_t *gn /**< a generic node which ptr will be deleted after it. */
+			 );
 
   /**
    * Check the given glist to see if it already contains the given value (of appropriate type).
@@ -157,16 +185,19 @@ int32 glist_chkdup_float64 (glist_t g, float64 val);
   /**
    * Reverse the order of the given glist.  (glist_add() adds to the head; one might
    * ultimately want the reverse of that.)
-   * Return value: The head of the new list.
    * NOTE: The list is reversed "in place"; i.e., no new memory is allocated.
+   * @return: The head of the new list.
    */
-glist_t glist_reverse (glist_t g);
+  glist_t glist_reverse (glist_t g /**< input link list */
+		       );
 
 
   /**
-     Return the number of elements in the given glist_t 
+     Count the number of element in a given link list 
+     @return the number of elements in the given glist_t 
   */
-int32 glist_count (glist_t g);
+int32 glist_count (glist_t g /**< input link list */
+		   );
 
 
   /**
@@ -194,6 +225,14 @@ void glist_free (glist_t g);
    */
 void glist_myfree (glist_t g, int32 datasize);
 
+
+  /**
+   * Free the given node, gn, of a glist, pred being its predecessor in the list.
+   * Return ptr to the next node in the list after the freed node.
+   */
+  gnode_t *gnode_free(gnode_t *gn, 
+		      gnode_t *pred
+		      );
 
   /**
    * Return the last node in the given list.
