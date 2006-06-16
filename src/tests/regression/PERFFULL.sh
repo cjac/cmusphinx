0dff6194c668ece3183964c6ebbbdbbae11868e8
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-06-16 22:48:26 +0000

    Removed dependency on PBS from the scripts
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@5637 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/tests/regression/PERFFULL.sh
+++ src/tests/regression/PERFFULL.sh
@@ -23,6 +23,7 @@ fi
 # If we found one of the above, use it. Otherwise, keep sendmail
 if test z${TMPMAIL} != z; then MAILX=${TMPMAIL};fi
 
+if [ -n "$PBS_ENVIRONMENT" ]; then
 echo "This job was submitted by user $PBS_O_LOGNAME"
 echo "This job was submitted to host $PBS_O_HOST"
 echo "This job was submitted to queue $PBS_O_QUEUE"
@@ -31,21 +32,22 @@ echo "PBS job id $PBS_JOBID"
 echo "PBS job name $PBS_JOBNAME"
 echo "PBS environment $PBS_ENVIRONMENT"
 echo "This script is running on `hostname`"
+fi
 
 S3REGTESTLIST='archan@cs.cmu.edu egouvea@cs.cmu.edu yitao@cs.cmu.edu dhuggins@cs.cmu.edu'
 
-#Run test. 
-cd $PBS_O_WORKDIR
+#Run test, assume we're at the top level (sphinx3 directory). 
+
+testdate=`date -I`
 
 if ! make perf-full > perf-full.log 2>&1 ;
  then
-    ${MAILX} -s "Full Performance Test failed at date:$testdate,machine:`hostname`,dir:$PBS_O_WORKDIR" ${S3REGTESTLIST} < perf-full.log
+    ${MAILX} -s "Full Performance Test failed on date:$testdate, machine:`hostname`, dir:`pwd`" ${S3REGTESTLIST} < perf-full.log
     exit
  fi
 
 
 #Store the results. 
-testdate=`date -I`
 logdir=log.$testdate
 resultfn=allresults
 mkdir $logdir
@@ -54,5 +56,5 @@ cp ./src/tests/performance/*/*.raw ./perf-full.log ./dailySTD.sh.[oe]* $logdir
 
 #Add analysis script at here. 
 cat $logdir/*.raw > $resultfn
-${MAILX} -s "Results of S3 FULL Regression Test at date:$testdate,machine:`hostname`,dir:$PBS_O_WORKDIR " ${S3REGTESTLIST} < $resultfn
+${MAILX} -s "Results of S3 FULL Regression Test on date:$testdate, machine:`hostname`, dir:`pwd` " ${S3REGTESTLIST} < $resultfn
 rm $resultfn
