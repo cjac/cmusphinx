7b5914847d1c7577219a9c486aef9ce991631ed8
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-06-30 20:56:55 +0000

    Replaced 'make all test-full' with 'make distcheck' in daily compilation test, and added coding style check to the test; currently, it only reports problems, it doesn't do anything else
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk/sphinx3@5741 c60273ff-4a12-0410-8d58-82ceea6d4170

--- src/tests/regression/checkCompileStatus.sh
+++ src/tests/regression/checkCompileStatus.sh
@@ -73,10 +73,10 @@ pushd sphinx2 >> $outfile 2>&1
 ./autogen.sh >> $outfile 2>&1 
 
 # Compile and run test, and verify if both were successful
-if ! ${GMAKE} all test >> $outfile 2>&1 ;
+if ! ${GMAKE} distcheck >> $outfile 2>&1 ;
  then ${MAILX} -s "sphinx2 compilation failed" ${S2LIST} < $outfile
  elif ! (grep BESTPATH $outfile | grep 'GO FORWARD TEN METERS' > /dev/null);
- then ${MAILX} -s "Sphinx2 test failed" ${S2LIST} < $outfile;
+ then ${MAILX} -s "sphinx2 test failed" ${S2LIST} < $outfile;
  else ${MAILX} -s "sphinx2 compilation and test succeeded" ${S2LIST} < $outfile
 fi
 
@@ -94,18 +94,16 @@ pushd sphinx3 >> $outfile 2>&1
 ./autogen.sh >> $outfile 2>&1 
 
 # Compile and run test, and verify that all tests ran successfully
-if ! ${GMAKE} all test-full >> $outfile 2>&1 ;
+if ! ${GMAKE} distcheck >> $outfile 2>&1 ;
  then ${MAILX} -s "sphinx3 compilation failed" ${S3LIST} < $outfile;
- elif ! (grep FWDVIT $outfile | grep 'P I T T S B U R G H' > /dev/null);
- then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
- elif ! (grep BSTPTH $outfile | grep 'P I T T S B U R G H' > /dev/null);
- then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
  elif grep FAILED $outfile > /dev/null;
  then ${MAILX} -s "Sphinx3 test failed" ${S3LIST} < $outfile;
  else ${MAILX} -s "sphinx3 compilation and test succeeded" ${S3LIST} < $outfile;
 fi
 
 popd >> $outfile 2>&1
+
+./src/tests/regression/checkStyle.sh 2>&1 | ${MAILX} -s "Coding style"
 
 /bin/rm -rf sphinx3
 
--- src/tests/regression/checkStyle.sh
+++ src/tests/regression/checkStyle.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-if which indent > /dev/null 2>&1 ; then
-    echo "Running style check";
-else
+# Define a path, just in case
+export PATH="/usr/local/bin:/bin:/usr/bin:/usr/ccs/bin"
+
+if ! which indent > /dev/null 2>&1 ; then
     echo "indent not found; exiting.";
     exit 1;
 fi
 
-outfile="`pwd`/style.out";
+pushd  `(cd src/tests/regression/../../.. && pwd)`  > /dev/null 2>&1
 
-pushd  ../../..  > /dev/null 2>&1
+# outfile="`pwd`/style.out";
 
-/bin/rm -rf $outfile
-touch $outfile
-for file in `find . -name '*.[ch]' -print`; do 
-    indent -kr $file -o /tmp/tmpfile >> $outfile 2>&1; 
+# /bin/rm -rf $outfile
+# touch $outfile
+for file in `find include -name '*.[ch]' -print && find src -name '*.[ch]' -print`; do 
+    indent -i8 -kr -psl -nce -nut $file -o /tmp/tmpfile 2>&1; 
     if ! diff $file /tmp/tmpfile > /dev/null 2>&1; then 
-	echo "$file does not conform to K&R" >> $outfile; 
+	echo "$file does not conform to style"; 
     fi; 
 done
 
 popd > /dev/null 2>&1
+
+echo "Report finished."
