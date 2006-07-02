ed753e51d2a384c6753c647517b2868dd8dd3f9a
arthchan2003 <arthchan2003@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-07-02 23:00:59 +0000

    check in a correct test script for 16 bit mode. At this point, there is one fail
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@5756 c60273ff-4a12-0410-8d58-82ceea6d4170

--- cmuclmtk/test/test.sh
+++ cmuclmtk/test/test.sh
@@ -5,13 +5,13 @@ if (diff English/emma11.txt.filtered.wfreq English/emma11.wfreq > /dev/null 2>&1
 then echo "V3 text2wfreq PASSED"; else \
 echo "V3 text2wfreq FAILED"; fi 
 
-echo "V2 text2wfreq"
-cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2wfreq > English/emma11.txt.filtered.wfreq
-diff English/emma11.txt.filtered.wfreq English/emma11.wfreq
-if (diff English/emma11.txt.filtered.wfreq English/emma11.wfreq > /dev/null 2>&1); \
-then echo "V2 text2wfreq PASSED"; else \
-echo "V2 text2wfreq FAILED"; fi 
-
+#echo "V2 text2wfreq"
+#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2wfreq > English/emma11.txt.filtered.wfreq
+#diff English/emma11.txt.filtered.wfreq English/emma11.wfreq
+#if (diff English/emma11.txt.filtered.wfreq English/emma11.wfreq > /dev/null 2>&1); \
+#then echo "V2 text2wfreq PASSED"; else \
+#echo "V2 text2wfreq FAILED"; fi 
+#
 #Testing of wfreq2vocab
 echo "V3 wfreq2vocab"
 cat English/emma11.txt.filtered.wfreq |../src/wfreq2vocab -top 20000  > English/emma11.txt.filtered.vocab
@@ -19,12 +19,12 @@ if (diff English/emma11.vocab English/emma11.txt.filtered.vocab > /dev/null 2>&1
 then echo "V3 wfreq2vocab PASSED"; else \
 echo "V3 wfreq2vocab FAILED"; fi 
 
-echo "V2 wfreq2vocab"
-cat English/emma11.txt.filtered.wfreq |../../CMU-Cam_Toolkit_v2/src/wfreq2vocab -top 20000  > English/emma11.txt.filtered.vocab
-if (diff English/emma11.vocab English/emma11.txt.filtered.vocab > /dev/null 2>&1); \
-then echo "V2 wfreq2vocab PASSED"; else \
-echo "V2 wfreq2vocab FAILED"; fi 
-
+#echo "V2 wfreq2vocab"
+#cat English/emma11.txt.filtered.wfreq |../../CMU-Cam_Toolkit_v2/src/wfreq2vocab -top 20000  > English/emma11.txt.filtered.vocab
+#if (diff English/emma11.vocab English/emma11.txt.filtered.vocab > /dev/null 2>&1); \
+#then echo "V2 wfreq2vocab PASSED"; else \
+#echo "V2 wfreq2vocab FAILED"; fi 
+#
 #Testing of text2idngram binary idngram
 echo "V3 text2idngram BIN IDNGRAM"
 cat English/emma11.txt.filtered | ../src/text2idngram -vocab English/emma11.vocab > ./English/emma11.txt.filtered.idngram  
@@ -32,12 +32,12 @@ if (cmp English/emma11.idngram ./English/emma11.txt.filtered.idngram > /dev/null
 then echo "V3 text2idngram BIN IDNGRAM PASSED"; else \
 echo "V3 text2idngram BIN IDNGRAM FAILED"; fi 
 
-echo "V2 text2idngram BIN IDNGRAM"
-cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab > English/emma11.txt.filtered.idngram  
-if (cmp English/emma11.idngram ./English/emma11.txt.filtered.idngram > /dev/null 2>&1); \
-then echo "V2 text2idngram BIN IDNGRAM PASSED"; else \
-echo "V2 text2idngram BIN IDNGRAM FAILED"; fi 
-
+#echo "V2 text2idngram BIN IDNGRAM"
+#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab > English/emma11.txt.filtered.idngram  
+#if (cmp English/emma11.idngram ./English/emma11.txt.filtered.idngram > /dev/null 2>&1); \
+#then echo "V2 text2idngram BIN IDNGRAM PASSED"; else \
+#echo "V2 text2idngram BIN IDNGRAM FAILED"; fi 
+#
 #Testing of text2idngram text idngram 
 echo "V3 text2idngram TXT IDNGRAM"
 cat English/emma11.txt.filtered | ../src/text2idngram -vocab English/emma11.vocab -write_ascii > ./English/emma11.txt.filtered.idngram.txt  
@@ -45,12 +45,12 @@ if (diff English/emma11.idngram.txt ./English/emma11.txt.filtered.idngram.txt > 
 then echo "V3 text2idngram TXT IDNGRAM PASSED"; else \
 echo "V3 text2idngram TXT IDNGRAM FAILED"; fi 
 
-echo "V2 text2idngram TXT IDNGRAM"
-cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab  -write_ascii > English/emma11.txt.filtered.idngram  
-if (diff English/emma11.idngram.txt ./English/emma11.txt.filtered.idngram.txt > /dev/null 2>&1); \
-then echo "V2 text2idngram TXT IDNGRAM PASSED"; else \
-echo "V2 text2idngram TXT IDNGRAM FAILED"; fi 
-
+#echo "V2 text2idngram TXT IDNGRAM"
+#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab  -write_ascii > English/emma11.txt.filtered.idngram  
+#if (diff English/emma11.idngram.txt ./English/emma11.txt.filtered.idngram.txt > /dev/null 2>&1); \
+#then echo "V2 text2idngram TXT IDNGRAM PASSED"; else \
+#echo "V2 text2idngram TXT IDNGRAM FAILED"; fi 
+#
 #Testing of idngram2lm
  echo "V3 idngram2lm ARPA"
 ../src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa
@@ -64,19 +64,19 @@ if (cmp English/emma11.bin English/emma11.filtered.bin  > /dev/null 2>&1); \
 then echo "V3 idngram2lm BIN PASSED"; else \
 echo "V3 idngram2lm BIN FAILED"; fi 
 
-#Testing of idngram2lm
-echo "V2 idngram2lm ARPA"
-../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa
-if (cmp English/emma11.arpa English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V2 idngram2lm ARPA PASSED"; else \
-echo "V2 idngram2lm ARPA FAILED"; fi 
-
-echo "V2 idngram2lm BIN"
-../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -binary English/emma11.filtered.bin
-if (cmp English/emma11.bin English/emma11.filtered.bin  > /dev/null 2>&1); \
-then echo "V2 idngram2lm BIN PASSED"; else \
-echo "V2 idngram2lm BIN FAILED"; fi 
-
+##Testing of idngram2lm
+#echo "V2 idngram2lm ARPA"
+#../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa
+#if (cmp English/emma11.arpa English/emma11.filtered.arpa  > /dev/null 2>&1); \
+#then echo "V2 idngram2lm ARPA PASSED"; else \
+#echo "V2 idngram2lm ARPA FAILED"; fi 
+#
+#echo "V2 idngram2lm BIN"
+#../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -binary English/emma11.filtered.bin
+#if (cmp English/emma11.bin English/emma11.filtered.bin  > /dev/null 2>&1); \
+#then echo "V2 idngram2lm BIN PASSED"; else \
+#echo "V2 idngram2lm BIN FAILED"; fi 
+#
 #Testing of idngram2lm
  echo "V3 idngram2lm ARPA ABSOLUTE TXT"
 ../src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -absolute -arpa English/emma11.filtered.arpa
@@ -114,13 +114,13 @@ then echo "V3 binlm2arpa  PASSED"; else \
 echo "V3 binlm2arpa FAILED"; fi 
 rm convert.arpa
 
-echo "V2 of binlm2arpa"
-../../CMU-Cam_Toolkit_v2/src/binlm2arpa -binary ./English/emma11.bin -arpa convert.arpa
-if (diff convert.arpa ./English/emma11.arpa > /dev/null 2>&1); \
-then echo "V2 binlm2arpa  PASSED"; else \
-echo "V2 binlm2arpa FAILED"; fi 
-rm convert.arpa
-
+#echo "V2 of binlm2arpa"
+#../../CMU-Cam_Toolkit_v2/src/binlm2arpa -binary ./English/emma11.bin -arpa convert.arpa
+#if (diff convert.arpa ./English/emma11.arpa > /dev/null 2>&1); \
+#then echo "V2 binlm2arpa  PASSED"; else \
+#echo "V2 binlm2arpa FAILED"; fi 
+#rm convert.arpa
+#
 #Testing of idngram2stats 
 echo "V3 of idngram2stats"
 cat ./English/emma11.idngram |  ../src/idngram2stats -n 3 > ./tmp.stats 2>&1
@@ -129,13 +129,13 @@ then echo "V3 idngram2stats PASSED"; else \
 echo "V3 idngram2stats FAILED"; fi
 rm ./tmp.stats
 
-echo "V2 of idngram2stats"
-cat ./English/emma11.idngram |  ../../CMU-Cam_Toolkit_v2/src/idngram2stats -n 3 > ./tmp.stats 2>&1 
-if (diff ./tmp.stats ./English/emma11.stats > /dev/null 2>&1); \
-then echo "V2 idngram2stats PASSED"; else \
-echo "V2 idngram2stats FAILED"; fi
-rm ./tmp.stats
-
+#echo "V2 of idngram2stats"
+#cat ./English/emma11.idngram |  ../../CMU-Cam_Toolkit_v2/src/idngram2stats -n 3 > ./tmp.stats 2>&1 
+#if (diff ./tmp.stats ./English/emma11.stats > /dev/null 2>&1); \
+#then echo "V2 idngram2stats PASSED"; else \
+#echo "V2 idngram2stats FAILED"; fi
+#rm ./tmp.stats
+#
 #Testing of evallm
 echo "V3 of evallm STATS"
 echo "stats" | ../src/evallm -binary ./English/emma11.bin > ./tmp.stats 2>&1
