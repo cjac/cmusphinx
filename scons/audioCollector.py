71a8fe7ef86657a1eae6a73f3d5f9e34668315d1
jkwoelfel <jkwoelfel@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-07-05 20:57:38 +0000

    Add plugin.jar to classpath
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@5781 c60273ff-4a12-0410-8d58-82ceea6d4170

--- scons/audioCollector.py
+++ scons/audioCollector.py
@@ -6,6 +6,7 @@ Import('common')
 
 srcDir = '../tools/audioCollector/src/java'
 classDir = os.path.normpath('../../scons_build/classes/audioCollector')
+javaHome = os.path.normpath(os.environ['JAVA_HOME'])
 
 classpath =  '../tools/common/lib/batch.jar:'
 classpath += '../tools/common/lib/dom4j-1.6.1.jar:'
@@ -13,6 +14,7 @@ classpath += '../tools/common/lib/forms_rt_license.jar:'
 classpath += '../tools/common/lib/forms_rt.jar:'
 classpath += '../tools/common/lib/javolution.jar:'
 classpath += '../tools/common/lib/sphinx4.jar:'
+classpath += javaHome + '/lib/plugin.jar:'
 classpath += str(common[0])
 
 env = Environment(ENV=os.environ, JAVACFLAGS='-source 1.5 -classpath ' + classpath, JARCHDIR=classDir)
