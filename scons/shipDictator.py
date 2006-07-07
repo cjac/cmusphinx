513e745a3590f17d11c734e0298be622d3a3ff8f
jkwoelfel <jkwoelfel@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-07-07 19:08:47 +0000

    Add a runDictator target
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@5808 c60273ff-4a12-0410-8d58-82ceea6d4170

--- scons/shipDictator.py
+++ scons/shipDictator.py
@@ -17,4 +17,26 @@ Install(dir, '../tools/dictator/run/run.sh')
 Install(dir, str(dictator[0]))
 
 shipDictator = Alias('foo', dir)
+
+
+classpath =  'WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz.jar:batch.jar:'
+classpath += 'common.jar:'
+classpath += 'dictator.jar:'
+classpath += 'dom4j-1.6.1.jar:'
+classpath += 'forms_rt.jar:'
+classpath += 'javolution.jar:'
+classpath += 'sphinx4.jar'
+classpath = classpath.replace('/',os.sep).replace(':',os.pathsep)
+
+def build(target, source, env):
+    os.chdir(dir)
+    os.system('java -Xmx256m -classpath ' + classpath + ' edu.cmu.sphinx.tools.dictator.DictatorView')
+    return None
+
+env = Environment(BUILDERS = {'RunDictator' : Builder(action = build)})
+runDictator = env.RunDictator('bar', shipDictator)
+Depends(runDictator, shipDictator)
+
+
 Export('shipDictator')
+Export('runDictator')
