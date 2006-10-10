79ad7a2b2a9abf796ebdcc7425d3068702308c97
dhdfu <dhdfu@c60273ff-4a12-0410-8d58-82ceea6d4170>
2006-10-10 03:21:53 +0000

    Make this consistent with sphinxbase (if this breaks the endpointer, the endpointer is broken)
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@6125 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/include/s3types.h
+++ sphinx3/include/s3types.h
@@ -193,8 +193,7 @@ typedef int16   	s3mgauid_t;	/** Mixture-gaussian codebook id */
 #define S3_LOGPROB_ZERO_F	((float32) -1e30)	/** Float version of log of zero Approx -infinity!! */
 
 /* RAH, I believe this is unreasonably large and not feasible */ /* Frame = 10msec */
-/* #define S3_MAX_FRAMES		30000   */ /* Frame = 10msec */  
-#define S3_MAX_FRAMES		1000000    /* RAH, I believe this is still too large, but better than before */
+#define S3_MAX_FRAMES		15000   /* Frame = 10msec */  
 
 #define RENORM_THRESH     ((int32) ((S3_LOGPROB_ZERO)>>1))       /** Bestscore getting close to 0 */
 
