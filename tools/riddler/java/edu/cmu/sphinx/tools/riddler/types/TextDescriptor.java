14a4978da5b81333b899b43194bdaa79489558e5
steely1 <steely1@c60273ff-4a12-0410-8d58-82ceea6d4170>
2007-02-07 04:51:07 +0000

    Tweaking the TextDescriptor's content to better support non-Western languages.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@6421 c60273ff-4a12-0410-8d58-82ceea6d4170

--- tools/riddler/java/edu/cmu/sphinx/tools/riddler/types/TextDescriptor.java
+++ tools/riddler/java/edu/cmu/sphinx/tools/riddler/types/TextDescriptor.java
@@ -16,23 +16,24 @@
 package edu.cmu.sphinx.tools.riddler.types;
 
 /**
- * The "meat" of a text entry. Contains the actual String contents. All words in the String
+ * The "meat" of a text entry. Contains the actual String contents in one long array (not assuming
+ * spaces as delimiters for better non-Western language compatibility). All tokens in the words array
  * should be present in the dictionary(ies) assigned to this Corpus.
  * @see DictionaryID
  * @see TextID
  */
 public class TextDescriptor {
-    String words;
+    String[] words;
 
-    public TextDescriptor(String words) {
+    public TextDescriptor(String[] words) {
         this.words = words;
     }
 
-    public String getWords() {
+    public String[] getWords() {
         return words;
     }
 
-    public void setWords(String words) {
+    public void setWords(String[] words) {
         this.words = words;
     }
 }
