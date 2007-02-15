056a4f09d8870db2ce80f87c8841d86e6f7d4792
steely1 <steely1@c60273ff-4a12-0410-8d58-82ceea6d4170>
2007-02-15 03:42:32 +0000

    tweaking build environment to enable Glassfish deployment and server startup/shutdown.  first test fixture written.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@6472 c60273ff-4a12-0410-8d58-82ceea6d4170

--- tools/riddler/dotNet/RiddlerTest.cs
+++ tools/riddler/dotNet/RiddlerTest.cs
@@ -8,36 +8,47 @@ namespace edu.cmu.sphinx.riddler
 	[TestFixture] 
 	public class RiddlerTest
 	{
-        /// <summary>
-        /// Riddler reference itself
-        /// </summary>
-        protected Riddler riddler;
-        /// <summary>
-		/// 
-		/// </summary>
-		protected string string1;
-		
+        RiddlerBeanService riddler;
+
 		/// <summary>
 		/// 
 		/// </summary>
 		[SetUp] public void Init() 
 		{
-            riddler = new Riddler();
-            string1 = "a predefined String";			
+            riddler = new RiddlerBeanService();
 		}
 
 		/// <summary>
 		/// 
 		/// </summary>
 		///
-		[Test] public void Echo() 
+		[Test] public void CreateDictionaryAndCorpus() 
 		{
-            string result = riddler.echo(string1);
-			Assert.AreEqual(string1, result, "Input and result are equal");
+            dictionaryDescriptor did = new dictionaryDescriptor();
+            dictionaryDescriptorEntry dictMetadatum = new dictionaryDescriptorEntry();
+            dictMetadatum.key = "SomeKey";
+            dictMetadatum.value = "SomeValue";
+            did.metadata = new dictionaryDescriptorEntry[] { dictMetadatum };
+            dictionaryID dictID = riddler.createDictionary(did);
+            
+            Assert.AreEqual(dictID, riddler.getDictionary(did));
+
+            corpusDescriptor cod = new corpusDescriptor();
+            cod.collectDate = DateTime.Today;
+            corpusDescriptorEntry corpMetadatum = new corpusDescriptorEntry();
+            corpMetadatum.key = "SomeKey";
+            corpMetadatum.value = "SomeValue";
+            cod.metadata = new corpusDescriptorEntry[] { corpMetadatum };
+            corpusID corpID = riddler.createCorpus(dictID, cod);
+
+            corpusDescriptor fetched = riddler.getCorpusDescriptor(corpID);
+            Assert.AreEqual(cod, fetched);
+            Assert.AreEqual(cod.metadata, fetched.metadata);
+            Assert.AreEqual(cod.collectDate, fetched.collectDate);
 		}
 
 		[Test]
-		[ExpectedException(typeof(InvalidOperationException))]
+        [ExpectedException(typeof(InvalidCastException))]
 		public void ExpectAnException()
 		{
 			throw new InvalidCastException();
