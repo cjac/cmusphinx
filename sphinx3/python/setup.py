dda6fb6b10262b5c0dd70da06c5c5d26a54534c5
dhdfu <dhdfu@c60273ff-4a12-0410-8d58-82ceea6d4170>
2007-06-04 14:03:10 +0000

    Make this build from a not-installed-yet sphinx3/sphinxbase setup (not really the right way to do this yet)
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@6674 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/python/setup.py
+++ sphinx3/python/setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup, Extension
 
 module = Extension('_sphinx3',
-                   include_dirs = ['../../../sphinxbase/include',
+                   include_dirs = ['../../sphinxbase/include',
                                    '../include',
                                    '/usr/local/include/sphinxbase/',
                                    '/usr/local/include/sphinx3',
                                    ],
                    libraries = ['sphinxutil', 'sphinxfe', 'sphinxfeat',
                                 'sphinxad', 's3decoder'],
+                   library_dirs = ['../../sphinxbase/src/libsphinxutil/.libs',
+                               '../../sphinxbase/src/libsphinxad/.libs',
+                               '../../sphinxbase/src/libsphinxfe/.libs',
+                               '../../sphinxbase/src/libsphinxfeat/.libs',
+                               '../src/libs3decoder/.libs',
+                               ],
                    sources = ['_sphinx3module.c'])
 
 setup(name = 'Sphinx3',
