32da4686e2512ecb3279a13b3d234dba83dd07ea
dhdfu <dhdfu@c60273ff-4a12-0410-8d58-82ceea6d4170>
2007-08-20 23:45:21 +0000

    discard comments entirely, hopefully this will work everywhere now
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@7167 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx3/src/tests/regression/test-lattice-htk.sh
+++ sphinx3/src/tests/regression/test-lattice-htk.sh
@@ -40,6 +40,6 @@ lmargs="-lm $an4lm/an4.ug.lm.DMP"
 
 run_program sphinx3_decode $margs $lmargs > $tmpout 2>&1
 bn=`head -1 $an4lm/an4.ctl`
-sed -e 's,^#.*,,' $bn.slf > tmp.slf && mv -f tmp.slf $bn.slf
-sed -e 's,^#.*,,' $an4lm/$bn.slf > tmp.slf
+sed -ne '!s,^#.*,,p' $bn.slf > tmp.slf && mv -f tmp.slf $bn.slf
+sed -ne '!s,^#.*,,p' $an4lm/$bn.slf > tmp.slf
 compare_table "HTK lattice test" tmp.slf $bn.slf
