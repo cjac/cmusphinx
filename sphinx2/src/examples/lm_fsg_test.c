19a4de45360dad633b9bf56f450a6e1ce0ea9068
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2007-10-31 19:19:17 +0000

    Removed hardwired control file and data dir from example code, replacing with command line args, and added the corresponding projects to the win32 side
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@7356 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinx2/src/examples/lm_fsg_test.c
+++ sphinx2/src/examples/lm_fsg_test.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "s2types.h"
 #include "err.h"
 #include "fbs.h"
-
-
-#define DATADIR		"/usr2/rkm/sphinxtest/ctl"
-#define CTLFILE		"/usr2/rkm/sphinxtest/ctl/h1_et_94-g.ctl"
 
 int
 main (int32 argc, char *argv[])
@@ -14,33 +11,55 @@ main (int32 argc, char *argv[])
   char uttfile[4096];	/* utterance file without filename extension */
   char mfcfile[4096];
   char fsgfile[4096];
+  char ctlfile[4096];
+  char datadir[4096];
   char *fsgname;
   char idspec[4096];
   search_hyp_t *hyp;
   FILE *fp;
   int32 n;
+  int32 new_argc;
+  char **new_argv;
+
+  new_argc = 0;
+  new_argv = (char **)calloc(argc, sizeof(char *));
+  ctlfile[0] = '\0';
+  for (n = 0; n < argc; n++) {
+    if (strcmpi (argv[n], "-ctlfn") == 0) {
+      /* Keep the control file name */
+      strcpy(ctlfile, argv[n + 1]);
+      /* Don't add the ctlfn argument to the new arg list, so that fbs_init
+       * doesn't take over full control. */
+    } else {
+      new_argv[new_argc++] = argv[n]; 
+      if(strcmpi (argv[n], "-datadir") == 0) {
+	strcpy(datadir, argv[n + 1]);
+      }
+    }
+  }
+
+  fbs_init (new_argc, new_argv);	/* Make sure -ctlfn is not in the args */
   
-  fbs_init (argc, argv);	/* Make sure -ctlfn is not in the args */
-  
-  if ((fp = fopen(CTLFILE, "r")) == NULL)
-    E_FATAL("fopen(%s,r) failed\n", CTLFILE);
+  if ((fp = fopen(ctlfile, "r")) == NULL)
+    E_FATAL("fopen(%s,r) failed\n", ctlfile);
   
   for (n = query_ctl_offset(); n > 0; --n)
     if (fscanf (fp, "%s", uttfile) != 1)
-      E_FATAL("EOF(%s) while skipping %d utts\n", CTLFILE, query_ctl_offset());
+      E_FATAL("EOF(%s) while skipping %d utts\n", ctlfile, query_ctl_offset());
   
   for (n = 0; n < query_ctl_count(); n++) {
     if (fscanf (fp, "%s", uttfile) != 1)
       break;
     
     strcpy (idspec, uttfile);
-    sprintf (fsgfile, "%s/%s.fsg", DATADIR, uttfile);
-    sprintf (mfcfile, "%s/%s", DATADIR, uttfile);
+    sprintf (fsgfile, "%s/%s.fsg", datadir, uttfile);
+    sprintf (mfcfile, "%s/%s", datadir, uttfile);
 
     /* Decode using N-gram LM */
-    uttproc_set_lm("");
-    hyp = run_sc_utterance (mfcfile, -1, -1, idspec);
-
+    /*
+      uttproc_set_lm("");
+      hyp = run_sc_utterance (mfcfile, -1, -1, idspec);
+    */
     /* Decode using FSG */
     if ((fsgname = uttproc_load_fsgfile (fsgfile)) == NULL)
       E_FATAL("Error loading FSG file '%s'\n", fsgfile);
--- sphinx2/src/examples/lm_fsg_test2.c
+++ sphinx2/src/examples/lm_fsg_test2.c
@@ -4,43 +4,47 @@
 #include "err.h"
 #include "fbs.h"
 
-
-#define DATADIR		"/usr2/rkm/sphinxtest/ctl"
-#define CTLFILE		"/usr2/rkm/sphinxtest/ctl/h1_et_94-g.ctl"
-
 int
 main (int32 argc, char *argv[])
 {
   char uttfile[4096];	/* utterance file without filename extension */
   char mfcfile[4096];
   char fsgfile[4096];
+  char datadir[4096];
   char fsgname[4096];
   char idspec[4096];
   search_hyp_t *hyp;
   FILE *fp;
   int32 n;
-  
+
+  for (n = 1; n < argc; n++) {
+    if(strcmpi (argv[n], "-datadir") == 0) {
+      strcpy(datadir, argv[++n]);
+      break;
+    }
+  }
   fbs_init (argc, argv);	/* Make sure -ctlfn is not in the args */
   
-  if ((fp = fopen(CTLFILE, "r")) == NULL)
-    E_FATAL("fopen(%s,r) failed\n", CTLFILE);
+  if ((fp = fopen(query_ctlfile_name(), "r")) == NULL)
+    E_FATAL("fopen(%s,r) failed\n", query_ctlfile_name());
   
   for (n = query_ctl_offset(); n > 0; --n)
     if (fscanf (fp, "%s", uttfile) != 1)
-      E_FATAL("EOF(%s) while skipping %d utts\n", CTLFILE, query_ctl_offset());
+      E_FATAL("EOF(%s) while skipping %d utts\n", query_ctlfile_name(), query_ctl_offset());
   
   for (n = 0; n < query_ctl_count(); n++) {
     if (fscanf (fp, "%s", uttfile) != 1)
       break;
     
     strcpy (idspec, uttfile);
-    sprintf (fsgfile, "%s/%s.fsg", DATADIR, uttfile);
-    sprintf (mfcfile, "%s/%s", DATADIR, uttfile);
+    sprintf (fsgfile, "%s/%s.fsg", datadir, uttfile);
+    sprintf (mfcfile, "%s/%s", datadir, uttfile);
 
     /* Decode using N-gram LM */
-    uttproc_set_lm("");
-    hyp = run_sc_utterance (mfcfile, -1, -1, idspec);
-
+    /*
+      uttproc_set_lm("");
+      hyp = run_sc_utterance (mfcfile, -1, -1, idspec);
+    */
     /* Decode using FSG (already loaded by -fsgctlfn argument) */
     sprintf (fsgname, "%s.fsg", uttfile);
     if (uttproc_set_fsg (fsgname) < 0)
