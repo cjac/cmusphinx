4a1a0963ca5b66e13436720823e7b8e6e6a4f66d
holgerbrandl <holgerbrandl@c60273ff-4a12-0410-8d58-82ceea6d4170>
2008-01-30 14:56:01 +0000

    cosmetics
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@7672 c60273ff-4a12-0410-8d58-82ceea6d4170

--- tools/confdesigner/src/edu/cmu/sphinx/tools/confdesigner/propedit/PropertyDescriptorAdapter.java
+++ tools/confdesigner/src/edu/cmu/sphinx/tools/confdesigner/propedit/PropertyDescriptorAdapter.java
@@ -25,103 +25,114 @@ import java.beans.PropertyVetoException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-/**
- * PropertyDescriptorAdapter.<br>
- *
- */
+/** PropertyDescriptorAdapter.<br> */
 class PropertyDescriptorAdapter extends AbstractProperty {
 
-  private PropertyDescriptor descriptor;
+    private PropertyDescriptor descriptor;
 
-  public PropertyDescriptorAdapter() {
-    super();
-  }
 
-  public PropertyDescriptorAdapter(PropertyDescriptor descriptor) {
-    this();
-    setDescriptor(descriptor);
-  }
-
-  public void setDescriptor(PropertyDescriptor descriptor) {
-    this.descriptor = descriptor;
-  }
-
-  public PropertyDescriptor getDescriptor() {
-    return descriptor;
-  }
-
-  public String getName() {
-    return descriptor.getName();
-  }
-
-  public String getDisplayName() {
-    return descriptor.getDisplayName();
-  }
-
-  public String getShortDescription() {
-    return descriptor.getShortDescription();
-  }
-
-  public Class getType() {
-    return descriptor.getPropertyType();
-  }
-
-  public Object clone() {
-    PropertyDescriptorAdapter clone = new PropertyDescriptorAdapter(descriptor);
-    clone.setValue(getValue());
-    return clone;
-  }
-
-  public void readFromObject(Object object) {
-    try {
-      Method method = descriptor.getReadMethod();
-      if (method != null) {
-        setValue(method.invoke(object, null));
-      }
-    } catch (Exception e) {
-      String message = "Got exception when reading property " + getName();
-      if (object == null) {
-        message += ", object was 'null'";
-      } else {
-        message += ", object was " + String.valueOf(object);
-      }
-      throw new RuntimeException(message, e);
+    public PropertyDescriptorAdapter() {
+        super();
     }
-  }
 
-  public void writeToObject(Object object) {
-    try {
-      Method method = descriptor.getWriteMethod();
-      if (method != null) {
-        method.invoke(object, new Object[]{getValue()});
-      }
-    } catch (Exception e) {
-      // let PropertyVetoException go to the upper level without logging
-      if (e instanceof InvocationTargetException &&
-        ((InvocationTargetException)e).getTargetException() instanceof PropertyVetoException) {
-        throw new RuntimeException(((InvocationTargetException)e).getTargetException());
-      }
 
-      String message = "Got exception when writing property " + getName();
-      if (object == null) {
-        message += ", object was 'null'";
-      } else {
-        message += ", object was " + String.valueOf(object);
-      }
-      throw new RuntimeException(message, e);
+    public PropertyDescriptorAdapter(PropertyDescriptor descriptor) {
+        this();
+        setDescriptor(descriptor);
     }
-  }
 
-  public boolean isEditable() {
-    return descriptor.getWriteMethod() != null;
-  }
 
-  public String getCategory() {
-    if (descriptor instanceof ExtendedPropertyDescriptor) {
-      return ((ExtendedPropertyDescriptor)descriptor).getCategory();
-    } else {
-      return null;
+    public void setDescriptor(PropertyDescriptor descriptor) {
+        this.descriptor = descriptor;
     }
-  }
+
+
+    public PropertyDescriptor getDescriptor() {
+        return descriptor;
+    }
+
+
+    public String getName() {
+        return descriptor.getName();
+    }
+
+
+    public String getDisplayName() {
+        return descriptor.getDisplayName();
+    }
+
+
+    public String getShortDescription() {
+        return descriptor.getShortDescription();
+    }
+
+
+    public Class getType() {
+        return descriptor.getPropertyType();
+    }
+
+
+    @Override
+    public Object clone() {
+        PropertyDescriptorAdapter clone = new PropertyDescriptorAdapter(descriptor);
+        clone.setValue(getValue());
+        return clone;
+    }
+
+
+    public void readFromObject(Object object) {
+        try {
+            Method method = descriptor.getReadMethod();
+            if (method != null) {
+                setValue(method.invoke(object, new Object[]{null}));
+            }
+        } catch (Exception e) {
+            String message = "Got exception when reading property " + getName();
+            if (object == null) {
+                message += ", object was 'null'";
+            } else {
+                message += ", object was " + String.valueOf(object);
+            }
+            throw new RuntimeException(message, e);
+        }
+    }
+
+
+    public void writeToObject(Object object) {
+        try {
+            Method method = descriptor.getWriteMethod();
+            if (method != null) {
+                method.invoke(object, getValue());
+            }
+        } catch (Exception e) {
+            // let PropertyVetoException go to the upper level without logging
+            if (e instanceof InvocationTargetException &&
+                    ((InvocationTargetException) e).getTargetException() instanceof PropertyVetoException) {
+                throw new RuntimeException(((InvocationTargetException) e).getTargetException());
+            }
+
+            String message = "Got exception when writing property " + getName();
+            if (object == null) {
+                message += ", object was 'null'";
+            } else {
+                message += ", object was " + String.valueOf(object);
+            }
+            throw new RuntimeException(message, e);
+        }
+    }
+
+
+    public boolean isEditable() {
+        return descriptor.getWriteMethod() != null;
+    }
+
+
+    public String getCategory() {
+        if (descriptor instanceof ExtendedPropertyDescriptor) {
+            return ((ExtendedPropertyDescriptor) descriptor).getCategory();
+        } else {
+            return null;
+        }
+    }
 
 }
