5be587f10a64a795b0ad6e9176d3c6f4d7f54eb9
air <air@c60273ff-4a12-0410-8d58-82ceea6d4170>
2008-04-04 21:31:44 +0000

    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@8012 c60273ff-4a12-0410-8d58-82ceea6d4170

--- logios/Tools/MakeDict/make_pronunciation.pl
+++ logios/Tools/MakeDict/make_pronunciation.pl
@@ -2,17 +2,17 @@
 # create a dictionary from a word list
 # [20080227] (air)
 
-BEGIN { push @INC, "./lib", "MakeDict/lib"; }
-use strict;
-use Pronounce;
-use File::Spec;
+# dir paths all need to be fully resolved; files are not
 
+use strict;
+use File::Spec;
+# use Pronounce;  # load dynamically (see below)
 use Getopt::Long;
 
 my $usage = "usage: $0 -tools <dir> -resources <dir> -words <file> -handdict <file> -dict <file>\n";
 
-my $toolsdir = "";  # where to find MakeDict/ tree
-my $resources = ""; # where to find word list and hand.dict
+my $toolsdir = "";
+my $resources = "";
 
 my $words = "";  # input word list file
 my $handdict = ""; # hand edited pronunciations
@@ -27,7 +27,9 @@ GetOptions( "words:s", \$words,
 	  );
 if (scalar @ARGV gt 0) { die "$usage\n"; }
 
-&Pronounce::make_dict('loc',$toolsdir,$resources,$words,$handdict,$dict,"pronunciation.log");
+my $lib = File::Spec->catfile($toolsdir,'MakeDict','lib','Pronounce.pm');
+require $lib;
+&Pronounce::make_dict($toolsdir,$resources,$words,$handdict,$dict,"pronunciation.log");
 
 exit 1;
 #
