23446b0676d5cb3a9852c0d3452900c2f79a0293
tkharris <tkharris@c60273ff-4a12-0410-8d58-82ceea6d4170>
2008-05-21 20:27:35 +0000

    Added phoenix2corpus.cpp, tying together phoenix2pcfg and pcfg2corpus. Maybe this would be better done in python or something...
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@8348 c60273ff-4a12-0410-8d58-82ceea6d4170

---
+++ logios/Tools/cfg2ngram/src/phoenix2corpus.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "PCFG.h"
+#include "debug.h"
+
+string usage(const char* program_name) {
+  ostringstream s;
+  s << "Usage: " << program_name << " {number of samples} {forms file} {grammar file}";
+  return s.str();
+}
+
+int main(int argc, char* argv[]) {
+  DebugStream::threashold_ = DebugStream::I;
+  srand((unsigned)time(0));
+
+  if(argc-1 < 3) {
+    cerr << usage(argv[0]) << endl;
+    exit(1);
+  }
+
+  unsigned int n; istringstream(argv[1]) >> n;
+  ifstream forms(argv[2]);
+  ifstream grammar(argv[3]);
+  PCFG g(PCFG::readPhoenixGrammarAndForms(grammar, forms));
+  info << "Got Phoenix forms and grammar file: " << endl 
+       << g.stats() << endl;
+  cout << g.generateSamples(n);
+}
