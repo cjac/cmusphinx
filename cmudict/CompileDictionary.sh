4e4e6237139df3dfcd54791c3458e1bd1aaf3408
air <air@c60273ff-4a12-0410-8d58-82ceea6d4170>
2008-08-01 16:14:07 +0000

    some additions and minor corrections.
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@8685 c60273ff-4a12-0410-8d58-82ceea6d4170

--- cmudict/CompileDictionary.sh
+++ cmudict/CompileDictionary.sh
@@ -1,7 +1,1 @@
-#!/usr/bin/sh
-# [20080422] (air) Compile cmudict into SPHINX_40 form
-
-# make_baseforms.pl simply removes stress marks
-perl bin/make_baseform.pl cmudict.0.7a sphinxdict/cmudict.0.7a_SPHINX_40
-
-#
+#!/usr/bin/sh# [20080422] (air) Compile cmudict into SPHINX_40 form# make_baseforms.pl simply removes stress marksperl bin/make_baseform.pl cmudict.0.7a sphinxdict/cmudict.0.7a_SPHINX_40#
