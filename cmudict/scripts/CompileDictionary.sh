d26366f4f8bcf25c7c970ae94e6c422e25913ab6
air <air@c60273ff-4a12-0410-8d58-82ceea6d4170>
2009-11-11 22:13:03 +0000

    added new words to cmudict, made corrections. Put additional scripts under version control
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@9429 c60273ff-4a12-0410-8d58-82ceea6d4170

--- cmudict/scripts/CompileDictionary.sh
+++ cmudict/scripts/CompileDictionary.sh
@@ -2,6 +2,14 @@
 # [20080422] (air) Compile cmudict into SPHINX_40 form
 # make_baseforms.pl removes stress marks and eliminates resulting duplicates
 
-perl scripts/make_baseform.pl cmudict.0.7a sphinxdict/cmudict.0.7a_SPHINX_40
+DICTIONARY=cmudict.0.7a
+
+
+echo -n "Compiling $DICTIONARY..."
+perl ./scripts/make_baseform.pl $DICTIONARY sphinxdict/${DICTIONARY}_SPHINX_40
+
+echo ""
+echo -n "Testing sphinx dictionary... "
+./scripts/test_dict.pl -p sphinxdict/SphinxPhones_40 sphinxdict/${DICTIONARY}_SPHINX_40
 
 #
--- cmudict/scripts/test_cmudict.pl
+++ cmudict/scripts/test_cmudict.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl -w
 
 # ====================================================================
 # Copyright (C) 1999-2008 Carnegie Mellon University and Alexander
@@ -37,77 +37,112 @@
 #
 
 # do a sanity check on a dictionary (# of tabs, duplicates)
+# this should be done after manual editing and before checking in
 #
 # [21oct98] (air) Created
 # [30oct98] (air) expanded functionality: check for phonetic symbols
 # [03feb99] (air) bug fix; added noise symbols to check
 # [20010623] (air) added cmd-line flags; word/pron bound now \s+
 # [20080422] (air) fixed for DOS eol's, also noisefile now properly optional
+# [20090331] (air) *** RENAMED to test_cmudict.pl ***
+#                  added tests specific to the raw cmudict
 
 #
-# correct dictionary format is:   ^WORD\tW ER DD\n$
+# correct dictionary format is:   ^WORD(\(\d\))*\tW ER\d* DD\n$
 # 
 # - "W ER DD" are symbols from the legal phone set(s)
 # - no leading/trailing spaces allowed
 # - no duplicates words allowed
-# - character collating sequence enforced
+# - character collating sequence enforced for baseform
 # 
 # above spec should cover all (current) consumers of the dictionary file.
 # not all conventions checked however (eg, for multiple pronunciations)
 #
 
+use strict;
+
+my ($word, $pron, $tok);
+my (%dict, %phone);
+
 use Getopt::Std; use vars qw/ $opt_p $opt_n /;
-if ($#ARGV<0) { die("usage: test_dict -p <phonefile> [-n <noisefile>] <dictfile>\n"); }
-getopt('p:n:'); $phonefile = $opt_p; $noisefile = $opt_n;
-$dictfile = $ARGV[0];
+if ($#ARGV<0) { die("usage: test_dict -p <phonefile> <dictfile>\n"); }
+getopt('p:n:');
+my $phonefile = $opt_p;
+my $dictfile = $ARGV[0];
 
 # get the legal symbol set
 open(PH,$phonefile) || die("can't open $phonefile!\n");
-while (<PH>) { s/[\r\n]*//g; $phone{$_} = 1; } close(PH);
-if ( defined $noisefile ) {
-  open(PH,$noisefile) || die("can't open $noisefile!\n");
-  while (<PH>) { s/[\r\n]*//g; $phone{$_} = 1; } close(PH);
-}
+while (<PH>) { s/[\r\n]*//g; $phone{$_} = 0; }
+close(PH);
+
 open(DICT,$dictfile) ||die("$dictfile not found!\n");
 
 # go through dict, do tests
-%dict = (); $last = ""; my ($lead, $trail); $word_cnt = 0;
+%dict = (); my $last = ""; my ($lead, $trail); my $word_cnt = 0;
 while (<DICT>) {
     chomp;  #    s/^\s*(.+?)\s*$/$1/;
-    s/[\r\n]+//g;
-    if ( /^;;;/ ) { next; }
-    $line = $_;
-    ($word,$pron) = split (/\s+/,$line,2);  # BIG assumption about no leading junk...
-    $dict{$word}++;
+    s/[\r\n]*$//g;  # deal with dos/unix diffs
+    if ( /^;;;/ ) { next; }  # ignore all comments in this pass
+    my $line = $_;
+    if ( $line  =~ /^\s*$/ ) { next; }  # empty lines allowed
 
+    # general spacing issues
     ($lead = $_) =~ s/^\s*(.+)/$1/;
     ($trail = $_) =~ s/(.+?)\s*$/$1/;
     if ($line ne $trail) { print "ERROR: trailing space in '$line'!\n"; }
     if ($line ne $lead) { print "ERROR: leading space in '$line'!\n"; }
+
+    # examine the head term and the pronunciation
+    ($word,$pron) = split (/\s+/,$line,2);
+    $dict{$word}++;
+
+    # word order
     if ( $last ge $word ) { print "ERROR: collation sequence for $last, $word wrong\n"; }
 
-    # check for legal symbols
-    @sym = split(/\s/,$pron);
-    $errs = "";
-    foreach $s (@sym) {	if ( ! $phone{$s} ) { $errs .= " $s"; } else { $phone{$s}++; } }
+    # check tabbing
+    my @line = split (/  /,$line);
+    if ( ($line[0] ne $word) or (scalar @line ne 2) ) { 
+	print "WARNING: tabbing error (",scalar @line, ") in: $line\n";
+    }
+
+    # check variant suffix
+    # note that other than it being a digit, it only has to be unique
+    if ( $word =~ /^[\(\)]/ ) {  # ignore 1st char if it's a paren
+	$tok = substr $word, 1;
+    } else { $tok = $word; }
+    if ( ($tok =~ /\(/) or ($tok =~ /\)/) ) {
+	if ( not ($tok =~ /^.+?\(\d\)$/) ) {
+	    print "ERROR: malformed variant tag in '$word'\n";
+	}
+    }
+
+    # check for legal phonetic symbols
+    my @sym = split(/\s/,$pron);
+    my $errs = "";
+    my ($ph,$stress,$tail);
+    foreach my $s (@sym) {
+	$ph = $stress = $tail = 0;
+	($ph,$stress) = ($s =~ /([A-Z]+)([012]*)/);
+	if ( # (defined $tail ) or
+	     # ((defined $stress) and not ($stress =~ /[012]/)) or
+	     ( not defined $phone{$ph} )
+	    ) { $errs .= " $s"; } else { $phone{$s}++; }
+    }
     if ($errs ne "") { print "ERROR: $word has illegal symbols: '$errs'\n"; }
 
-    # bad format
-    @line = split (/\t/,$line);
-    if ( $#line != 1 ) { print "WARNING: tabbing error (",$#line, ") in: $line\n"; }
     $word_cnt++;
     $last = $word;
 }
 close(DICT);
 
 # check for duplicates entries
-foreach $x (keys %dict) {
-    if ($dict{$x}>1) { print "ERROR: $x occurs ", $dict{$x}, " times!\n"; }
+foreach my $x (keys %dict) {
+    if ($dict{$x}>1) { print "ERROR: '$x' occurs ", $dict{$x}, " times!\n"; }
 }
 
-print STDERR "processed $word_cnt words.\n";
+print "processed $word_cnt words.\n";
 
 # print out the phone counts
-# foreach (sort keys %phone) { print STDERR "$_\t$phone{$_}\n"; }
+foreach (sort keys %phone) { print "$_\t$phone{$_}\n"; }
 
 #
