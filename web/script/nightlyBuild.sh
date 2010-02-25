8ef8203e47bb6bd4d7f48187f55b87bab80e5381
egouvea <egouvea@c60273ff-4a12-0410-8d58-82ceea6d4170>
2010-02-25 23:00:21 +0000

    Moved location of nightly build tar files away from CMU and into sourceforge.nets file release management system
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@9742 c60273ff-4a12-0410-8d58-82ceea6d4170

--- web/script/nightlyBuild.sh
+++ web/script/nightlyBuild.sh
@@ -4,13 +4,12 @@
 . ${HOME}/.profile
 
 # Check that we have all executables
-if ! SVN=`command -v svn 2>&1`; then exit 1; fi
+if ! RSYNC=`command -v rsync 2>&1`; then exit 1; fi
 if ! SCP=`command -v scp 2>&1`; then exit 1; fi
+if ! SVN=`command -v svn 2>&1`; then exit 1; fi
 if ! TAR=`command -v gtar 2>&1`; then
   if ! TAR=`command -v tar 2>&1`; then exit 1; fi
 fi
-
-PUSH_DEST=/usr1/httpd/html/sphinx/download/nightly/
 
 loopUntilSuccess () {
     cmd=$@
@@ -50,7 +49,7 @@ createNightlyBuild () {
     cd $TMP_DIR
     $TAR -czf /tmp/${name}.nightly.tar.gz $module || exit 1
     chmod 664 /tmp/$name.nightly.tar.gz
-    ${SCP} -o "BatchMode yes" /tmp/$name.nightly.tar.gz www.speech.cs.cmu.edu:${PUSH_DEST}
+    ${RSYNC} -e ssh /tmp/$name.nightly.tar.gz `whoami`,cmusphinx@frs.sourceforge.net:/home/frs/project/c/cm/cmusphinx/NightlyBuild/
 
     cd
     /bin/rm -rf $TMP_DIR
@@ -72,33 +71,33 @@ content=text/html;charset=ISO-8859-1>
 <h1>Sphinx Open Source nightly builds available at SourceForge.net</h1>
 <h2>Applications</h2>
 <ul>
-<li><a href="dictator.tar.gz">Eval Dictator tarball</a> or <a href="dictator.jar">Eval Dictator jar</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/dictator.tar.gz">Eval Dictator tarball</a> or <a href="http://cmusphinx.org/download/nightly/dictator.jar">Eval Dictator jar</a></li>
 </ul>
 <h2>Common Libraries</h2>
 <ul>
-<li><a href="sphinxbase.nightly.tar.gz">sphinxbase</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/sphinxbase.nightly.tar.gz">sphinxbase</a></li>
 </ul>
 <h2>Decoders</h2>
 <ul>
-<li><a href="s3flat.nightly.tar.gz">s3flat</a></li>
-<li><a href="pocketsphinx.nightly.tar.gz">pocketsphinx</a></li>
-<li><a href="sphinx2.nightly.tar.gz">sphinx2</a></li>
-<li><a href="sphinx3.nightly.tar.gz">sphinx3</a></li>
-<li><a href="sphinx4.nightly.tar.gz">sphinx4</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/s3flat.nightly.tar.gz">s3flat</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/pocketsphinx.nightly.tar.gz">pocketsphinx</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/sphinx2.nightly.tar.gz">sphinx2</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/sphinx3.nightly.tar.gz">sphinx3</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/sphinx4.nightly.tar.gz">sphinx4</a></li>
 </ul>
 <h2>Acoustic Model Training</h2>
 <ul>
-<li><a href="SphinxTrain.nightly.tar.gz">SphinxTrain</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/SphinxTrain.nightly.tar.gz">SphinxTrain</a></li>
 </ul>
 <h2>Language Model Training</h2>
 <ul>
-<li><a href="SimpleLM.nightly.tar.gz">SimpleLM</a></li>
-<li><a href="cmuclmtk.nightly.tar.gz">cmuclmtk</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/SimpleLM.nightly.tar.gz">SimpleLM</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/cmuclmtk.nightly.tar.gz">cmuclmtk</a></li>
 </ul>
 <h2>Tools</h2>
 <ul>
-<li><a href="cepview.nightly.tar.gz">cepview</a></li>
-<li><a href="lm3g2dmp.nightly.tar.gz">lm3g2dmp</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/cepview.nightly.tar.gz">cepview</a></li>
+<li><a href="http://cmusphinx.org/download/nightly/lm3g2dmp.nightly.tar.gz">lm3g2dmp</a></li>
 </ul>
 <p>Last update on `date`</p>
 </body>
@@ -107,6 +106,7 @@ END
 
 }
 
+pushd ${HOME}
 createNightlyBuild cmuclmtk cmuclmtk
 createNightlyBuild sphinx2 sphinx2
 createNightlyBuild sphinx3 sphinx3
@@ -120,7 +120,8 @@ createNightlyBuild archive_s3/s3 s3flat
 createNightlyBuild share/cepview cepview
 createNightlyBuild share/lm3g2dmp lm3g2dmp
 createNightlyBuild SimpleLM SimpleLM
+popd
 nightlypage=nightlybuild.html
 nightlyBuildPage $nightlypage
-${SCP} -o "BatchMode yes" $nightlypage www.speech.cs.cmu.edu:${PUSH_DEST}/index.html
+${SCP} -o "BatchMode yes" $nightlypage www.speech.cs.cmu.edu:/usr1/httpd/html/sphinx/download/nightly/index.html
 /bin/rm $nightlypage
