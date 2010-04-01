cfeed56a74c243144ac354e6a720d3077963deeb
nshmyrev <nshmyrev@c60273ff-4a12-0410-8d58-82ceea6d4170>
2010-04-01 05:01:08 +0000

    Merged changes into stable branch
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/branches/pocketsphinx-0.6@9954 c60273ff-4a12-0410-8d58-82ceea6d4170

--- sphinxbase/src/libsphinxbase/lm/lm3g_templates.c
+++ sphinxbase/src/libsphinxbase/lm/lm3g_templates.c
@@ -398,17 +398,26 @@ lm3g_template_successors(ngram_iter_t *bitor)
     itor->ug = from->ug;
     switch (bitor->m) {
     case 0:
-        /* This indicates no successors (FIXME: make sure this is true!) */
-        if (itor->ug->bo_wt1.l == 0 && itor->ug->bigrams == 0)
-            goto done;
+        /* Next itor bigrams is the same as this itor bigram or
+	   itor bigrams is more than total count. This means no successors */
+        if (((itor->ug + 1) - model->lm3g.unigrams < bitor->model->n_counts[0] &&
+	    itor->ug->bigrams == (itor->ug + 1)->bigrams) || 
+	    itor->ug->bigrams == bitor->model->n_counts[1])
+	    goto done;
+	    
         /* Start iterating from first bigram successor of from->ug. */
         itor->bg = model->lm3g.bigrams + itor->ug->bigrams;
         break;
     case 1:
         itor->bg = from->bg;
-        /* This indicates no successors (FIXME: make sure this is true!) */
-        if (model->lm3g.bo_wt2[itor->bg->bo_wt2].l == 0)
-            goto done;
+
+        /* This indicates no successors */
+        if (((itor->bg + 1) - model->lm3g.bigrams < bitor->model->n_counts[1] &&
+    	    FIRST_TG (model, itor->bg - model->lm3g.bigrams) == 
+    	    FIRST_TG (model, (itor->bg + 1) - model->lm3g.bigrams)) ||
+	    FIRST_TG (model, itor->bg - model->lm3g.bigrams) == bitor->model->n_counts[2])
+    	    goto done;
+    	    
         /* Start iterating from first trigram successor of from->bg. */
         itor->tg = (model->lm3g.trigrams 
                     + FIRST_TG(model, (itor->bg - model->lm3g.bigrams)));
