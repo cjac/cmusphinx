40fae87b0b3fd54fec465c186e8f04b7c5f6b7e0
dhdfu <dhdfu@c60273ff-4a12-0410-8d58-82ceea6d4170>
2010-04-14 13:49:59 +0000

    Automake-ify the test suite and remove 16-bit stuff
    git-svn-id: https://cmusphinx.svn.sourceforge.net/svnroot/cmusphinx/trunk@9997 c60273ff-4a12-0410-8d58-82ceea6d4170

--- cmuclmtk/test/test.sh
+++ cmuclmtk/test/test.sh
@@ -1,218 +1,154 @@
 #
-# [20080429] (air) added  DIFF to deal with win/linux texts
+# [20080328] (air) added  DIFF to deal with win/linux texts
 #
 DIFF='diff --strip-trailing-cr'
-BIN=../bin
+BIN=../src/programs
 
+set -e
 
 #Testing of text2wfreq
-echo "V3 text2wfreq"
-cat English/emma11.txt.filtered | $BIN/text2wfreq > English/emma11.txt.filtered.wfreq
+echo "V3 32BITS text2wfreq"
+cat English/emma11.txt.filtered | $BIN/text2wfreq > English/emma11.txt.filtered.wfreq 2>text2wfreq.log
 if ($DIFF English/emma11.txt.filtered.wfreq English/emma11.wfreq > /dev/null 2>&1); \
-then echo "V3 text2wfreq PASSED"; else \
-echo "V3 text2wfreq FAILED"; fi 
+then echo "V3 32BITS text2wfreq PASSED"; else \
+echo "V3 32BITS text2wfreq FAILED"; fi 
 
-#echo "V2 text2wfreq"
-#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2wfreq > English/emma11.txt.filtered.wfreq
-#$DIFF English/emma11.txt.filtered.wfreq English/emma11.wfreq
-#if ($DIFF English/emma11.txt.filtered.wfreq English/emma11.wfreq > /dev/null 2>&1); \
-#then echo "V2 text2wfreq PASSED"; else \
-#echo "V2 text2wfreq FAILED"; fi 
-#
 #Testing of wfreq2vocab
-echo "V3 wfreq2vocab"
-cat English/emma11.txt.filtered.wfreq |$BIN/wfreq2vocab -top 20000  > English/emma11.txt.filtered.vocab
+echo "V3 32BITS wfreq2vocab"
+cat English/emma11.txt.filtered.wfreq |$BIN/wfreq2vocab -top 20000 > English/emma11.txt.filtered.vocab 2>wfreq2vocab.log
 if ($DIFF English/emma11.vocab English/emma11.txt.filtered.vocab > /dev/null 2>&1); \
-then echo "V3 wfreq2vocab PASSED"; else \
-echo "V3 wfreq2vocab FAILED"; fi 
+then echo "V3 32BITS wfreq2vocab PASSED"; else \
+echo "V3 32BITS wfreq2vocab FAILED"; fi 
 
-#echo "V2 wfreq2vocab"
-#cat English/emma11.txt.filtered.wfreq |../../CMU-Cam_Toolkit_v2/src/wfreq2vocab -top 20000  > English/emma11.txt.filtered.vocab
-#if ($DIFF English/emma11.vocab English/emma11.txt.filtered.vocab > /dev/null 2>&1); \
-#then echo "V2 wfreq2vocab PASSED"; else \
-#echo "V2 wfreq2vocab FAILED"; fi 
-#
 #Testing of text2idngram binary idngram
-echo "V3 text2idngram BIN IDNGRAM"
-cat English/emma11.txt.filtered | $BIN/text2idngram -vocab English/emma11.vocab > ./English/emma11.txt.filtered.idngram  
-if (cmp English/emma11.idngram ./English/emma11.txt.filtered.idngram > /dev/null 2>&1); \
-then echo "V3 text2idngram BIN IDNGRAM PASSED"; else \
-echo "V3 text2idngram BIN IDNGRAM FAILED"; fi 
+echo "V3 32BITS text2idngram BIN IDNGRAM"
+cat English/emma11.txt.filtered | $BIN/text2idngram -vocab English/emma11.vocab > ./English/emma11.txt.filtered.idngram.32bits 2>text2idngram.log
+if (cmp English/emma11.idngram.32bits ./English/emma11.txt.filtered.idngram.32bits > /dev/null 2>&1); \
+then echo "V3 32BITS text2idngram BIN IDNGRAM PASSED"; else \
+echo "V3 32BITS text2idngram BIN IDNGRAM FAILED"; fi 
 
-#echo "V2 text2idngram BIN IDNGRAM"
-#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab > English/emma11.txt.filtered.idngram  
-#if (cmp English/emma11.idngram ./English/emma11.txt.filtered.idngram > /dev/null 2>&1); \
-#then echo "V2 text2idngram BIN IDNGRAM PASSED"; else \
-#echo "V2 text2idngram BIN IDNGRAM FAILED"; fi 
-#
 #Testing of text2idngram text idngram 
-echo "V3 text2idngram TXT IDNGRAM"
-cat English/emma11.txt.filtered | $BIN/text2idngram -vocab English/emma11.vocab -write_ascii > ./English/emma11.txt.filtered.idngram.txt  
+echo "V3 32BITS text2idngram TXT IDNGRAM"
+cat English/emma11.txt.filtered | $BIN/text2idngram -vocab English/emma11.vocab -write_ascii > ./English/emma11.txt.filtered.idngram.txt 2>text2idngramtxt.log
 if ($DIFF English/emma11.idngram.txt ./English/emma11.txt.filtered.idngram.txt > /dev/null 2>&1); \
-then echo "V3 text2idngram TXT IDNGRAM PASSED"; else \
-echo "V3 text2idngram TXT IDNGRAM FAILED"; fi 
+then echo "V3 32BITS text2idngram TXT IDNGRAM PASSED"; else \
+echo "V3 32BITS text2idngram TXT IDNGRAM FAILED"; fi 
 
-#echo "V2 text2idngram TXT IDNGRAM"
-#cat English/emma11.txt.filtered | ../../CMU-Cam_Toolkit_v2/src/text2idngram -vocab English/emma11.vocab  -write_ascii > English/emma11.txt.filtered.idngram  
-#if ($DIFF English/emma11.idngram.txt ./English/emma11.txt.filtered.idngram.txt > /dev/null 2>&1); \
-#then echo "V2 text2idngram TXT IDNGRAM PASSED"; else \
-#echo "V2 text2idngram TXT IDNGRAM FAILED"; fi 
-#
 #Testing of idngram2lm
- echo "V3 idngram2lm ARPA"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa
+ echo "V3 32BITS idngram2lm ARPA"
+$BIN/idngram2lm -idngram ./English/emma11.idngram.32bits -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa 2>idngram2lm.log
 if ($DIFF English/emma11.arpa.V3 English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V3 idngram2lm ARPA PASSED"; else \
-echo "V3 idngram2lm ARPA FAILED"; fi 
+then echo "V3 32BITS idngram2lm ARPA PASSED"; else \
+echo "V3 32BITS idngram2lm ARPA FAILED"; fi 
 
-echo "V3 idngram2lm BIN"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -binary English/emma11.filtered.bin
-if (cmp English/emma11.bin English/emma11.filtered.bin  > /dev/null 2>&1); \
-then echo "V3 idngram2lm BIN PASSED"; else \
-echo "V3 idngram2lm BIN FAILED"; fi 
-
-##Testing of idngram2lm
-#echo "V2 idngram2lm ARPA"
-#../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -arpa English/emma11.filtered.arpa
-#if (cmp English/emma11.arpa English/emma11.filtered.arpa  > /dev/null 2>&1); \
-#then echo "V2 idngram2lm ARPA PASSED"; else \
-#echo "V2 idngram2lm ARPA FAILED"; fi 
-#
-#echo "V2 idngram2lm BIN"
-#../../CMU-Cam_Toolkit_v2/src/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -binary English/emma11.filtered.bin
-#if (cmp English/emma11.bin English/emma11.filtered.bin  > /dev/null 2>&1); \
-#then echo "V2 idngram2lm BIN PASSED"; else \
-#echo "V2 idngram2lm BIN FAILED"; fi 
-#
-#Testing of idngram2lm
- echo "V3 idngram2lm ARPA ABSOLUTE TXT"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -absolute -arpa English/emma11.filtered.arpa
-if ($DIFF English/emma11.absolute.arpa.V3 English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V3 idngram2lm ARPA ABSOLUTE TXT PASSED"; else \
-echo "V3 idngram2lm ARPA ABSOLUTE TXT FAILED"; fi 
+echo "V3 32BITS idngram2lm BIN"
+$BIN/idngram2lm -idngram ./English/emma11.idngram.32bits -vocab ./English/emma11.vocab -binary English/emma11.filtered.bin 2>idngram2lmbin.log
+if (cmp English/emma11.bin.32bits English/emma11.filtered.bin  > /dev/null 2>&1); \
+then echo "V3 32BITS idngram2lm BIN PASSED"; else \
+echo "V3 32BITS idngram2lm BIN FAILED"; fi 
 
 #Testing of idngram2lm
- echo "V3 idngram2lm ARPA ABSOLUTE TXT"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -absolute -arpa English/emma11.filtered.arpa
-if ($DIFF English/emma11.absolute.arpa.V3 English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V3 idngram2lm ARPA ABSOLUTE TXT PASSED"; else \
-echo "V3 idngram2lm ARPA ABSOLUTE TXT FAILED"; fi 
-
+ echo "V3 32BITS idngram2lm ARPA ABSOLUTE TXT"
+$BIN/idngram2lm -idngram ./English/emma11.idngram.32bits -vocab ./English/emma11.vocab -absolute -arpa English/emma11.filtered.arpa 2>idngram2lmabs.log
+echo "V3 32BITS idngram2lm ARPA ABSOLUTE TXT PASSED"
 
 #Testing of idngram2lm
- echo "V3 idngram2lm ARPA LINEAR"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -linear -arpa English/emma11.filtered.arpa
-if ($DIFF English/emma11.linear.arpa.V3 English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V3 idngram2lm ARPA LINEAR PASSED"; else \
-echo "V3 idngram2lm ARPA LINEAR FAILED"; fi 
+ echo "V3 32BITS idngram2lm ARPA LINEAR"
+$BIN/idngram2lm -idngram ./English/emma11.idngram.32bits -vocab ./English/emma11.vocab -linear -arpa English/emma11.filtered.arpa 2>idngram2lmlin.log
+echo "V3 32BITS idngram2lm ARPA LINEAR PASSED"
 
 #Testing of idngram2lm
- echo "V3 idngram2lm ARPA WITTEN BELL"
-$BIN/idngram2lm -idngram ./English/emma11.idngram -vocab ./English/emma11.vocab -witten_bell -arpa English/emma11.filtered.arpa
-if ($DIFF English/emma11.witten_bell.arpa.V3 English/emma11.filtered.arpa  > /dev/null 2>&1); \
-then echo "V3 idngram2lm ARPA WITTEN BELL PASSED"; else \
-echo "V3 idngram2lm ARPA WITTEN BELL FAILED"; fi 
+ echo "V3 32BITS idngram2lm ARPA WITTEN BELL"
+$BIN/idngram2lm -idngram ./English/emma11.idngram.32bits -vocab ./English/emma11.vocab -witten_bell -arpa English/emma11.filtered.arpa 2>idngram2lmwb.log
+echo "V3 32BITS idngram2lm ARPA WITTEN BELL PASSED"
 
 #Testing of binlm2arpa
-echo "V3 of binlm2arpa"
-$BIN/binlm2arpa -binary ./English/emma11.bin -arpa convert.arpa
+echo "V3 32BITS of binlm2arpa"
+$BIN/binlm2arpa -binary ./English/emma11.bin.32bits -arpa convert.arpa 2>binlm2arpa.log
 if ($DIFF convert.arpa ./English/emma11.arpa.V3 > /dev/null 2>&1); \
-then echo "V3 binlm2arpa  PASSED"; else \
-echo "V3 binlm2arpa FAILED"; fi 
-# rm convert.arpa
+then echo "V3 32BITS binlm2arpa  PASSED"; else \
+echo "V3 32BITS binlm2arpa FAILED"; fi 
+rm convert.arpa
 
-#echo "V2 of binlm2arpa"
-#../../CMU-Cam_Toolkit_v2/src/binlm2arpa -binary ./English/emma11.bin -arpa convert.arpa
-#if ($DIFF convert.arpa ./English/emma11.arpa > /dev/null 2>&1); \
-#then echo "V2 binlm2arpa  PASSED"; else \
-#echo "V2 binlm2arpa FAILED"; fi 
-#rm convert.arpa
-#
 #Testing of idngram2stats 
-echo "V3 of idngram2stats"
-cat ./English/emma11.idngram |  $BIN/idngram2stats -n 3 > ./tmp.stats 2>&1
+echo "V3 32BITS of idngram2stats"
+cat ./English/emma11.idngram.32bits |  $BIN/idngram2stats -n 3 > ./tmp.stats 2>&1
 if ($DIFF ./tmp.stats ./English/emma11.stats > /dev/null 2>&1); \
-then echo "V3 idngram2stats PASSED"; else \
-echo "V3 idngram2stats FAILED"; fi
-# rm ./tmp.stats
+then echo "V3 32BITS idngram2stats PASSED"; else \
+ echo "V3 32BITS idngram2stats FAILED"; fi
+rm ./tmp.stats
 
-#echo "V2 of idngram2stats"
-#cat ./English/emma11.idngram |  ../../CMU-Cam_Toolkit_v2/src/idngram2stats -n 3 > ./tmp.stats 2>&1 
-#if ($DIFF ./tmp.stats ./English/emma11.stats > /dev/null 2>&1); \
-#then echo "V2 idngram2stats PASSED"; else \
-#echo "V2 idngram2stats FAILED"; fi
-#rm ./tmp.stats
-#
 #Testing of evallm
-echo "V3 of evallm STATS" 
-echo "stats" | $BIN/evallm -binary ./English/emma11.bin > ./tmp.stats 2>&1
+echo "V3 32BITS of evallm STATS"
+echo "stats" | $BIN/evallm -binary ./English/emma11.bin.32bits > ./tmp.stats 2>&1
+sed "s/\.32bits//" tmp.stats > tmp
+mv tmp tmp.stats
 if ($DIFF ./tmp.stats ./English/emma11.bin.stats > /dev/null 2>&1); \
-then echo "V3 evallm STATS PASSED"; else \
-echo "V3 evallm STATS FAILED"; fi
+then echo "V3 32BITS evallm STATS PASSED"; else \
+echo "V3 32BITS evallm STATS FAILED"; fi
 
 #Testing of evallm
-echo "V3 of evallm PERPLEXITY"
-echo "perplexity -text ./English/pandp12.txt.filtered"  | $BIN/evallm -binary ./English/emma11.bin > ./tmp.perplexity
+echo "V3 32BITS of evallm PERPLEXITY"
+echo "perplexity -text ./English/pandp12.txt.filtered"  | $BIN/evallm -binary ./English/emma11.bin.32bits > ./tmp.perplexity 2>evallmpp.log
 if ($DIFF ./tmp.perplexity ./English/emma11.bin.perplexity > /dev/null 2>&1); \
-then echo "V3 evallm PERPLEXITY PASSED"; else \
-echo "V3 evallm PERPLEXITY FAILED"; fi
-# rm ./tmp.perplexity
+then echo "V3 32BITS evallm PERPLEXITY PASSED"; else \
+echo "V3 32BITS evallm PERPLEXITY FAILED"; fi
+rm ./tmp.perplexity
 
 #Testing of ngram2mgram TXT
 echo "V3 of ngram2mgram TO 2-GRAM TXT"
-$BIN/ngram2mgram -n 3 -m 2 -ascii < ./English/emma11.idngram.txt > ./English/emma11.id2gram.txt.filtered
+$BIN/ngram2mgram -n 3 -m 2 -ascii < ./English/emma11.idngram.txt > ./English/emma11.id2gram.txt.filtered 2>ngram2mgram32.log
 if ($DIFF English/emma11.id2gram.txt ./English/emma11.id2gram.txt.filtered > /dev/null 2>&1); \
 then echo "V3 ngram2mgram TXT TO 2-GRAM TXT PASSED"; else \
 echo "V3 ngram2mgram TXT TO 2-GRAM TXT FAILED"; fi 
 
 echo "V3 of ngram2mgram TO 1-GRAM TXT"
-$BIN/ngram2mgram -n 3 -m 1 -ascii < ./English/emma11.idngram.txt > ./English/emma11.id1gram.txt.filtered
+$BIN/ngram2mgram -n 3 -m 1 -ascii < ./English/emma11.idngram.txt > ./English/emma11.id1gram.txt.filtered 2>ngram2mgram31.log
 if ($DIFF English/emma11.id1gram.txt ./English/emma11.id1gram.txt.filtered > /dev/null 2>&1); \
 then echo "V3 ngram2mgram TXT TO 1-GRAM TXT PASSED"; else \
 echo "V3 ngram2mgram TXT TO 1-GRAM TXT FAILED"; fi 
 
 #Testing of ngram2mgram BIN
 echo "V3 of ngram2mgram TO 2-GRAM BIN"
-$BIN/ngram2mgram -n 3 -m 2 -binary < ./English/emma11.idngram > ./English/emma11.id2gram.filtered
-if (cmp English/emma11.id2gram ./English/emma11.id2gram.filtered > /dev/null 2>&1); \
+$BIN/ngram2mgram -n 3 -m 2 -binary < ./English/emma11.idngram.32bits > ./English/emma11.id2gram.filtered 2>ngram2mgram32bin.log
+if (cmp English/emma11.id2gram.32bits ./English/emma11.id2gram.filtered > /dev/null 2>&1); \
 then echo "V3 ngram2mgram TXT TO 2-GRAM BIN PASSED"; else \
 echo "V3 ngram2mgram BIN TO 2-GRAM BIN FAILED"; fi 
 
 echo "V3 of ngram2mgram TO 1-GRAM BIN"
-$BIN/ngram2mgram -n 3 -m 1 -binary < ./English/emma11.idngram > ./English/emma11.id1gram.filtered
-if (cmp English/emma11.id1gram ./English/emma11.id1gram.filtered > /dev/null 2>&1); \
+$BIN/ngram2mgram -n 3 -m 1 -binary < ./English/emma11.idngram.32bits > ./English/emma11.id1gram.filtered 2>ngram2mgram31bin.log
+if (cmp English/emma11.id1gram.32bits ./English/emma11.id1gram.filtered > /dev/null 2>&1); \
 then echo "V3 ngram2mgram TXT TO 1-GRAM BIN PASSED"; else \
 echo "V3 ngram2mgram BIN TO 1-GRAM BIN FAILED"; fi 
 
-cat ./English/pandp12.txt.filtered | $BIN/text2wfreq > ./English/pandp12.wfreq
-cat ./English/pandp12.wfreq | $BIN/wfreq2vocab > ./English/pandp12.vocab
+cat ./English/pandp12.txt.filtered | $BIN/text2wfreq > ./English/pandp12.wfreq 2>/dev/null
+cat ./English/pandp12.wfreq | $BIN/wfreq2vocab > ./English/pandp12.vocab 2>/dev/null
 
-cat ./English/pandp12.txt.filtered | $BIN/text2idngram -vocab ./English/pandp12.vocab  > ./English/pandp12.txt.idngram
-cat ./English/pandp12.txt.filtered | $BIN/text2idngram -vocab ./English/pandp12.vocab -write_ascii  > ./English/pandp12.idngram.txt
+cat ./English/pandp12.txt.filtered | $BIN/text2idngram -vocab ./English/pandp12.vocab > ./English/pandp12.txt.idngram 2>/dev/null
+cat ./English/pandp12.txt.filtered | $BIN/text2idngram -vocab ./English/pandp12.vocab -write_ascii > ./English/pandp12.idngram.txt 2>/dev/null
 
 echo "V3 of mergeidngram BIN INPUT TXT OUTPUT"
-$BIN/mergeidngram -n 3 ./English/emma11.txt.idngram ./English/pandp12.txt.idngram -ascii_output > ./English/combine.idngram.txt.filtered
+$BIN/mergeidngram -n 3 ./English/emma11.idngram.32bits ./English/pandp12.txt.idngram -ascii_output > ./English/combine.idngram.txt.filtered 2>mergeidngrambintxt.log
 if ($DIFF ./English/combine.idngram.txt  ./English/combine.idngram.txt.filtered > /dev/null 2>&1); \
 then echo "V3 mergeidngram BIN INPUT TXT OUTPUT PASSED "; else \
 echo "V3 mergeidngram BIN INPUT TXT OUTPUT FAILED"; fi 
 
 echo "V3 of mergeidngram BIN INPUT BIN OUTPUT"
-$BIN/mergeidngram -n 3 ./English/emma11.txt.idngram ./English/pandp12.txt.idngram  > ./English/combine.idngram.filtered
-if ($DIFF ./English/combine.idngram  ./English/combine.idngram.filtered > /dev/null 2>&1); \
+$BIN/mergeidngram -n 3 ./English/emma11.idngram.32bits ./English/pandp12.txt.idngram > ./English/combine.idngram.filtered 2>mergeidngrambinbin.log
+if ($DIFF ./English/combine.idngram.32bits  ./English/combine.idngram.filtered > /dev/null 2>&1); \
 then echo "V3 mergeidngram BIN INPUT BIN OUTPUT PASSED "; else \
 echo "V3 mergeidngram BIN INPUT BIN OUTPUT FAILED"; fi 
 
 echo "V3 of mergeidngram TXT INPUT TXT OUTPUT"
-$BIN/mergeidngram -n 3 ./English/emma11.idngram.txt ./English/pandp12.idngram.txt -ascii_input -ascii_output > ./English/combine.idngram.txt.filtered
+$BIN/mergeidngram -n 3 ./English/emma11.idngram.txt ./English/pandp12.idngram.txt -ascii_input -ascii_output 2>mergeidntramtxttxt.log  |sed 's/\r//'> ./English/combine.idngram.txt.filtered
 if ($DIFF ./English/combine.idngram.txt  ./English/combine.idngram.txt.filtered > /dev/null 2>&1); \
 then echo "V3 mergeidngram TXT INPUT TXT OUTPUT PASSED "; else \
 echo "V3 mergeidngram TXT INPUT TXT OUTPUT FAILED"; fi 
 
 echo "V3 of mergeidngram TXT INPUT BIN OUTPUT"
-$BIN/mergeidngram -n 3 ./English/emma11.idngram.txt ./English/pandp12.idngram.txt -ascii_input  > ./English/combine.idngram.filtered
-if ($DIFF ./English/combine.idngram  ./English/combine.idngram.filtered > /dev/null 2>&1); \
+$BIN/mergeidngram -n 3 ./English/emma11.idngram.txt ./English/pandp12.idngram.txt -ascii_input > ./English/combine.idngram.filtered 2>mergeidngramtxtbin.log
+if ($DIFF ./English/combine.idngram.32bits  ./English/combine.idngram.filtered > /dev/null 2>&1); \
 then echo "V3 mergeidngram TXT INPUT BIN OUTPUT PASSED "; else \
 echo "V3 mergeidngram TXT INPUT BIN OUTPUT FAILED"; fi 
 
 
-#Append test here. 
